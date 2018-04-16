cfa0ddaa6c64ec2ce7a60981744a686b3b182bb2
Juraj Marcin <juraj.marcin123@gmail.com>
2018-04-16 09:30:03 +0200

    #25798 - react-location-redux - translation for login page

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -2,13 +2,14 @@ import React from 'react';
 
 import Form from './components/Form'
 import './login.css'
+import {Translate} from 'react-localize-redux'
 
 
 const Login = props => {
     return <div className="Login">
         <div className="form-wr">
             <div className="form-place">
-                <h1 className="form-header">Login to PRODEX</h1>
+                <h1 className="form-header"><Translate id="login.loginToPRODEX"/></h1>
                 <Form {...props} />
             </div>
         </div>
