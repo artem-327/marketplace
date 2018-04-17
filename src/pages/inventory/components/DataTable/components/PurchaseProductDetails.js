f130f3fc21e64543aa71e821bc1264b965e70f0e
Juraj Marcin <juraj.marcin123@gmail.com>
2018-04-17 12:48:33 +0200

    #25818 - purchase form basic finished

--- src/pages/inventory/components/DataTable/components/PurchaseProductDetails.js
+++ src/pages/inventory/components/DataTable/components/PurchaseProductDetails.js
@@ -6,50 +6,38 @@ class PurchaseProductDetails extends Component {
     render() {
         return (
             <div className="purchase-product-detail">
-                <div className="detail-item left-border-line">
-                    <span className="detail-item-header"><Translate id="inventoryTable.body.vendor"/></span>
-                    <span className="detail-item-text"><Translate id="inventoryTable.body.vendor"/></span>
-                </div>
-                <div className="detail-item left-border-line">
-                    <span className="detail-item-header"><Translate id="inventoryTable.body.mfr"/></span>
-                    <span className="detail-item-text"><Translate id="inventoryTable.body.vendor"/></span>
-                </div>
-                <div className="detail-item left-border-line">
-                    <span className="detail-item-header"><Translate id="inventoryTable.body.origin"/></span>
-                    <span className="detail-item-text"><Translate id="inventoryTable.body.vendor"/></span>
-                </div>
-                <div className="detail-item left-border-line">
-                    <span className="detail-item-header"><Translate id="inventoryTable.body.packing"/></span>
-                    <span className="detail-item-text"><Translate id="inventoryTable.body.vendor"/></span>
-                </div>
-                <div className="detail-item left-border-line">
-                    <span className="detail-item-header"><Translate id="inventoryTable.body.location"/></span>
-                    <span className="detail-item-text"><Translate id="inventoryTable.body.vendor"/></span>
-                </div>
-                <div className="detail-item left-border-line">
-                    <span className="detail-item-header"><Translate id="inventoryTable.body.condition"/></span>
-                    <span className="detail-item-text"><Translate id="inventoryTable.body.vendor"/></span>
-                </div>
-                <div className="detail-item left-border-line">
-                    <span className="detail-item-header"><Translate id="inventoryTable.body.form"/></span>
-                    <span className="detail-item-text"><Translate id="inventoryTable.body.vendor"/></span>
-                </div>
-                <div className="detail-item left-border-line">
-                    <span className="detail-item-header"><Translate id="inventoryTable.body.assay"/></span>
-                    <span className="detail-item-text"><Translate id="inventoryTable.body.vendor"/></span>
-                </div>
-                <div className="detail-item left-border-line">
-                    <span className="detail-item-header"><Translate id="inventoryTable.body.zip"/></span>
-                    <span className="detail-item-text"><Translate id="inventoryTable.body.vendor"/></span>
-                </div>
-                <div className="detail-item-long left-border-line">
-                    <span className="detail-item-header"><Translate id="inventoryTable.body.packagingWeight"/></span>
-                    <span className="detail-item-text"><Translate id="inventoryTable.body.vendor"/></span>
-                </div>
-                <div className="detail-item-long">
-                    <span className="detail-item-header"><Translate id="inventoryTable.body.totalProductAvailable"/></span>
-                    <span className="detail-item-text"><Translate id="inventoryTable.body.vendor"/></span>
-                </div>
+                <table className="table">
+                    <thead>
+                    <tr>
+                        <th scope="col"><Translate id="inventoryTable.body.vendor"/></th>
+                        <th scope="col"><Translate id="inventoryTable.body.mfr"/></th>
+                        <th scope="col"><Translate id="inventoryTable.body.origin"/></th>
+                        <th scope="col"><Translate id="inventoryTable.body.packing"/></th>
+                        <th scope="col"><Translate id="inventoryTable.body.location"/></th>
+                        <th scope="col"><Translate id="inventoryTable.body.condition"/></th>
+                        <th scope="col"><Translate id="inventoryTable.body.form"/></th>
+                        <th scope="col"><Translate id="inventoryTable.body.assay"/></th>
+                        <th scope="col"><Translate id="inventoryTable.body.zip"/></th>
+                        <th scope="col"><Translate id="inventoryTable.body.packagingWeight"/></th>
+                        <th scope="col"><Translate id="inventoryTable.body.totalProductAvailable"/></th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <th>Mark</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                        <td>Mark</td>
+                        <td>Mark</td>
+                        <td>Mark</td>
+                        <td>Mark</td>
+                        <td>Mark</td>
+                        <td>Mark</td>
+                        <td>Mark</td>
+                    </tr>
+                    </tbody>
+                </table>
             </div>
         );
     }
