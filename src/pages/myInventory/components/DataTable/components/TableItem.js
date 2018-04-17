10635fc5b6024cf9d4a71d6b2bb55560c8c0f07b
Juraj Marcin <juraj.marcin123@gmail.com>
2018-04-17 14:43:07 +0200

    #25836 - My invetory page - table creation

---
+++ src/pages/myInventory/components/DataTable/components/TableItem.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+
+
+class TableItem extends Component {
+
+    render() {
+        return (
+            <tr>
+                <td>test</td>
+                <td>test</td>
+                <td>test</td>
+                <td>test</td>
+                <td>test</td>
+                <td>test</td>
+                <td>test</td>
+                <td>test</td>
+                <td>test</td>
+                <td>test</td>
+                <td>test</td>
+                <td>test</td>
+                <td>test</td>
+                <td>test</td>
+                <td>test</td>
+                <td>test</td>
+                <td>test</td>
+                <td>test</td>
+                <td>YES</td>
+                <td>NO</td>
+            </tr>
+        );
+    }
+}
+
+export default TableItem
