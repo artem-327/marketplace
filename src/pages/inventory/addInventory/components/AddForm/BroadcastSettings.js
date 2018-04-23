90e8df6d246d910775bb773f923c214c3b4acaac
Juraj Marcin <juraj.marcin123@gmail.com>
2018-04-23 09:53:13 +0200

    #25836 - Add Inventory Page - whole form created

--- src/pages/inventory/addInventory/components/AddForm/BroadcastSettings.js
+++ src/pages/inventory/addInventory/components/AddForm/BroadcastSettings.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {Translate} from 'react-localize-redux'
 import './addForm.css'
 import { Control} from 'react-redux-form';
+import DocumentsImage from "../../../../../images/documentsButton.png"
 
 
 class BroadcastSettings extends Component {
@@ -16,45 +17,73 @@ class BroadcastSettings extends Component {
         return (
             <div  className="broadcast-settings">
 
-                <div className="rules-header col-lg-12 col-md-12 col-sm-12">
-                    <Translate id="inventoryPage.addInventory.form.rules"/>
+                <div className="broadcast-header col-lg-12 col-md-12 col-sm-12">
+                    <Translate id="inventoryPage.addInventory.form.broadcastSettings"/>
                 </div>
                 <table className="table">
                     <thead>
                     <tr>
-                        <th scope="col"><Translate id="inventoryPage.addInventory.form.splitPackages"/></th>
-                        <th scope="col"><Translate id="inventoryPage.addInventory.form.minimumPackages"/></th>
-                        <th scope="col"><Translate id="inventoryPage.addInventory.form.incrementalPricing"/></th>
+                        <th scope="col"><Translate id="inventoryPage.addInventory.form.listAnonymously"/></th>
+                        <th scope="col"><Translate id="inventoryPage.addInventory.form.restrictedStates"/></th>
+                        <th scope="col"><Translate id="inventoryPage.addInventory.form.restrictedCompanies"/></th>
                     </tr>
                     </thead>
                     <tbody>
                     <tr>
-                        <td><Control model="forms.purchaseForm.reqShipDate" type="number" placeholder="10"/></td>
-                        <td><Control model="forms.purchaseForm.reqShipDate" type="number" placeholder="10"/></td>
                         <td>
                             <Control.select model="simple.favoriteColor">
-                                <option><Translate id="inventoryPage.addInventory.form.yes"/></option>
-                                <option><Translate id="inventoryPage.addInventory.form.no"/></option>
+                                <option>Select</option>
+                                <option value="2">2</option>
+                            </Control.select>
+                        </td>
+                        <td>
+                            <Control.select model="simple.favoriteColor">
+                                <option>Select</option>
+                                <option value="2">2</option>
+                            </Control.select>
+                        </td>
+                        <td>
+                            <Control.select model="simple.favoriteColor">
+                                <option>Select</option>
+                                <option value="2">2</option>
                             </Control.select>
                         </td>
                     </tr>
                     </tbody>
                 </table>
-                {/*<table className="table">*/}
-                    {/*<thead>*/}
-                    {/*<tr>*/}
-                        {/*<th scope="col"/>*/}
-                        {/*<th scope="col"><Translate id="inventoryPage.addInventory.form.fromPackages"/></th>*/}
-                        {/*<th scope="col"><Translate id="inventoryPage.addInventory.form.toPackages"/></th>*/}
-                        {/*<th scope="col"><Translate id="inventoryPage.addInventory.form.priceLBS"/></th>*/}
-                    {/*</tr>*/}
-                    {/*</thead>*/}
-                    {/*<tbody>*/}
-                    {/*<RulesRow id={1} from={10} price={1.86}/>*/}
-                    {/*<RulesRow id={2} from={50} price={1.86}/>*/}
 
-                    {/*</tbody>*/}
-                {/*</table>*/}
+
+                <div className="broadcast-header col-lg-12 col-md-12 col-sm-12">
+                    <Translate id="inventoryPage.addInventory.form.documents"/>
+                </div>
+                <div className="row documents-buttons">
+                    <div className="col-lg-4 col-md-4 col-sm-4">
+                        <button>
+                            <img alt="documentsImage" src={DocumentsImage}/>
+                            <Translate id="inventoryPage.addInventory.form.attachCofA"/>
+                        </button>
+                    </div>
+                    <div className="col-lg-4 col-md-4 col-sm-4">
+                        <button>
+                            <img alt="documentsImage" src={DocumentsImage}/>
+                            <Translate id="inventoryPage.addInventory.form.attachSDS"/>
+                        </button>
+                    </div>
+                    <div className="col-lg-4 col-md-4 col-sm-4">
+                        <button>
+                            <img alt="documentsImage" src={DocumentsImage}/>
+                            <Translate id="inventoryPage.addInventory.form.attachSpecSheet"/>
+                        </button>
+                    </div>
+                </div>
+                <div className="row add-inventory">
+                    <div className="col-lg-12 col-md-12 col-sm-12">
+                        <button className="add-inventory-button">
+                            <img alt="documentsImage" src={DocumentsImage}/>
+                            <Translate id="inventoryPage.addInventory.form.addInventoryButton"/>
+                        </button>
+                    </div>
+                </div>
             </div>
         );
     }
