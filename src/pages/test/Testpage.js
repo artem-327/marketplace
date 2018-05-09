0b921ccb979020048b64d2195897e040b6147293
Jonyberry <jonyberry1@gmail.cim>
2018-05-09 13:29:54 +0200

    added testing page

--- src/pages/test/Testpage.js
+++ src/pages/test/Testpage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const TestPage = props => {
-    console.log(props);
+    console.log(props.identity);
     return<div>
         <div className="form-place">
             <h1 className="form-header">I exist because of testing</h1>
