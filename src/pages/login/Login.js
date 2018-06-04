aee20ba7edc9ba2a3df34b25366cbf31a40f12ba
Marek Lhotecký <nerxeus@gmail.com>
2018-06-04 14:25:12 +0200

    working on styles

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -3,13 +3,14 @@ import logoLogin from '../../images/login/logo_echo.png';
 
 import Form from './components/Form'
 import {Translate} from 'react-localize-redux'
+import './login.css'
 
 
 const Login = props => {
-    return <div className="registration">
-        <div className="registration-wr">
+    return <div className="login">
+        <div className="login-wr">
             <div className="form-place">
-                <div className="login">
+                <div className="logForm">
                 <img className="logoLogin" src={logoLogin}  alt='Login'/>
                 {/*<h1 className="form-header"><Translate id="login.loginToPRODEX"/></h1>*/}
                 <Form {...props} />
--- src/pages/login/components/Form.js
+++ src/pages/login/components/Form.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import classnames from 'classnames'
+import classnames from 'classnames';
 import {Control, Form} from 'react-redux-form';
 import {Link} from 'react-router-dom';
-import {Translate} from 'react-localize-redux'
+import {Translate} from 'react-localize-redux';
+import '../login.css';
+
 
 export default class LoginForm extends React.Component {
 
     handleSubmit(input) {
+        let {email, password} = this.props.loginInputs;
+        if(!email || !password) return;
         this.props.login(input.email, input.password).then(() => {
             this.props.getIdentity().then(()=>{
                 this.props.history.push("/");
@@ -16,11 +20,10 @@ export default class LoginForm extends React.Component {
 
     render() {
         let {isFetching, hasError} = this.props.formStatus;
-        let butLogin = isFetching ?
-            <Translate id="login.logging"/>
-            :
-            <Translate id="login.login"/>;
-        console.log();
+        let {email, password} = this.props.loginInputs;
+        let butLogin = isFetching ? <Translate id="login.logging"/> : <Translate id="login.login"/>;
+        //TODO:: react to all types of errors
+        let loginErr = /*hasError ?*/ <div className="login-err">Incorrect username or password</div>; /*: null;*/
         return (
             <Form model="forms.loginForm" onSubmit={(val) => this.handleSubmit(val)}>
                 <div>
@@ -31,8 +34,9 @@ export default class LoginForm extends React.Component {
                     {/*<label htmlFor="forms.loginForm.password"><Translate id="login.password"/></label>*/}
                     <Control.password model="forms.loginForm.password" placeholder="Password"/>
                 </div>
+                {loginErr}
                 <div className="form-middle">
-                    <button className={classnames({"not-valid": hasError}, {"loading": isFetching})}>{butLogin}</button>
+                    <button className={classnames({"loading": isFetching}, {"disabled": (!email || !password)})}>{butLogin}</button>
                 </div>
                 {/*<Link className="form-link" to="#"><Translate id="login.forgottenPassword"/></Link>*/}
             </Form>
