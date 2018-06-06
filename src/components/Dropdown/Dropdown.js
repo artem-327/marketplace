553facd1448ff710f3f303dbe9ecdf5f931e411b
Marek Lhotecký <nerxeus@gmail.com>
2018-06-06 14:25:24 +0200

    final Dropdown without styling

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -1,98 +1,52 @@
-
-// import {Translate} from 'react-localize-redux';
 import React, {Component} from 'react';
-import './dropdown.css';
-
+import PropTypes from "prop-types";
 
 class Dropdown extends Component {
     constructor(props) {
         super(props);
-        this.toggleDropdown = this.toggleDropdown.bind(this);
+        this.setCurrentValue = this.setCurrentValue.bind(this);
         this.state = {
-            options: this.groupOptionValues(this.props.optionValues)
-        }
+            isOpen: false,
+            currentValue: this.props.currentValue,
+        };
+    }
+
+    setCurrentValue(val){
+        this.setState({currentValue: val, isOpen: false})
     }
 
     componentWillReceiveProps(nextProps){
-        this.setState({
-            options: this.groupOptionValues(nextProps.optionValues)
+        this.setState({currentValue: nextProps.currentValue, isOpen: false})
+    }
+
+    render() {
+        let {currentValue} = this.state;
+        let opt = this.props.options.map((option, index)=>{
+            return <li key={index + 'dropdown'} onClick={()=>{this.setCurrentValue(option.value)}}>{option.value}</li>
         });
-    }
-
-    groupOptionValues(optionValues) {
-        return optionValues.reduce((carry, option) => {
-            (carry[option.option.id] = carry[option.option.id] || {...option.option, visible: true, optionValues: []}).optionValues.push(option);
-            return carry;
-        }, {});
-    }
-
-    toggleDropdown(optionId){
-        this.setState({
-            options: {
-                ...this.state.options,
-                [optionId]:{
-                    ...this.state.options[optionId],
-                    visible: !this.state.options[optionId].visible
-                }
-            }
-        })
-    }
-
-    // handleSubmit(inputs){
-    //     console.log(inputs);
-    // }
-
-    render(){
-        // let currentState = this.props.currentState || null ;
-        // let test = [];
-        // let options = test.map((option, index)=>{
-        //     <div>{option.name}</div>
-        // });
-    //     return (
-    //         <div> {currentState || this.props.placeholder || 'zadejte hodnotu'}</div>
-    //
-    //     )
-    // }
-        console.log(this.state.options);
+        let options = this.state.isOpen ?
+            <ul className='dropdown-options'>
+                {opt}
+            </ul> : null;
         return (
-            <div className="App">
-                <table className="option-values">
-                    <thead>
-                    <tr>
-                        <th><input type="dropdown" /></th>
-                        {/*<th>{currentState || this.props.placeholder || 'zadejte hodnotu'}</th>*/}
-                        <th>'zadejte hodnotu'</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {Object.values(this.state.options).reduce((rows, option) => {
-                        rows.push(
-                            <tr className="option" key={'m' + option.name} onClick={() => {this.toggleDropdown(option.id)}}>
-                                <td>
-                                    <span><a href="#">{option.id}</a></span>
-                                    <span className="product-name">{option.name}</span>
-                                </td>
-                                <td colSpan="3">
-                                    {option.visible ? <i className="icon fas fa-angle-down"/> : <i className="icon fas fa-angle-up"/>}
-                                </td>
-                            </tr>
-                        );
-                        option.visible ?
-                            option.optionValues.forEach((option) => {
-                                rows.push(
-                                    <tr className="option-values" key={'m' + option.id}>
-                                        <td>{option.name}</td>
-                                    </tr>
-                                )
-                            }) : null;
-                        return rows;
-                    }, [])}
-                    </tbody>
-                </table>
+            <div className='dropdown-wr'>
+                <div className='dropdown-trigger' onClick={()=>{this.setState({isOpen: !this.state.isOpen})}}>
+                    <span>{currentValue || this.props.placeholder || 'Select Option'}</span><img src={}
+                </div>
+                {options}
             </div>
         );
     }
+};
 
+Dropdown.propTypes = {
+    options: PropTypes.arrayOf(
+        PropTypes.shape({
+            value: PropTypes.string,
+        })
+    ).isRequired,
+    currentValue: PropTypes.string,
+    placeholder: PropTypes.string
 };
 
 
