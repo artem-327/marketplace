ee70009ce9d872933a306c35a295345e1c6aa035
Marek Lhotecký <nerxeus@gmail.com>
2018-06-06 14:25:24 +0200

    final Dropdown without styling

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,14 +1,24 @@
 import React, {Component} from 'react';
 import './dashboard.css'
 import AddInventory from './components/AddInventory';
+import Dropdown from '../../components/Dropdown/Dropdown';
 
 
 class Dashboard extends Component {
 
     render() {
+        let options = [
+            {
+                value: 'val1'
+            },
+            {
+                value: 'val2'
+            }
+        ]
         return (
             <div className="dashboard">
-                <AddInventory {...this.props}/>
+                {/*<AddInventory {...this.props}/>*/}
+                <Dropdown options={options} placeholder='Select our option' currentValue='val2'/>
             </div>
         );
     }
