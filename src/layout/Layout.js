68247138f5d0baddf2434e472848d83188b6f958
Jonyberry <jonyberry1@gmail.cim>
2018-06-15 14:38:50 +0200

    added auth to layout

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -2,19 +2,19 @@ import React, { Component } from 'react';
 import Header from "./Header";
 import Footer from "./Footer";
 import PopUp from '../components/PopUp';
+import {withAuth} from "../utils/auth";
+import { withRouter } from "react-router-dom";
 
 
 class Layout extends Component {
     render() {
-        return (
-            <div>
-                <Header/>
-                <PopUp />
-                {this.props.children}
-                <Footer/>
-            </div>
-        );
+        return <div>
+            <Header/>
+            <PopUp />
+            {this.props.children}
+            <Footer/>
+        </div>
     }
 }
 
-export default Layout
+export default withRouter(withAuth(Layout))
