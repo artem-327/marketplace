c81880b7890732e5b516ebb83142ae1f8a448781
Marek Lhotecký <nerxeus@gmail.com>
2018-06-19 18:41:51 +0200

    finish radio button

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -18,17 +18,17 @@ class Dashboard extends Component {
                     <button className='button small'>Submit</button><br/><br/>
                     <button className='button disabled'>Submit</button><br/><br/>
                     <div>
-                        <label className="rbutton">One
-                            <input type="radio" checked="checked" name="radio" />
+                        <label className="rbutton"><p>One</p>
+                            <input type="radio" name="radio" value="One" />
                                 <span className="radiomark"></span>
                         </label>
-                        <label className="rbutton">Two
-                            <input type="radio" name="radio" />
+                        <label className="rbutton"><p>Two</p>
+                            <input type="radio" name="radio" value="Two" />
                                 <span className="radiomark"></span>
                         </label>
                         <label className="rbutton">Three
-                            <input type="radio" name="radio" />
-                                <span className="radiomark"></span>
+                            <input type="radio" name="radio" value="Three" />
+                                <span className="radiomark small"></span>
                         </label>
                     </div>
                     <Spinner/>
