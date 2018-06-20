3caa530b2a82ef34691d900d437bf20bc2647dfb
Marek Lhotecký <nerxeus@gmail.com>
2018-06-20 17:40:24 +0200

    working on general components

--- src/components/Spinner/Spinner.js
+++ src/components/Spinner/Spinner.js
@@ -3,10 +3,10 @@ import './spinner.css';
 class Spinner extends Component {
     render () {
         return (
-            <div class="spinner">
-                <div class="bounce1"></div>
-                <div class="bounce2"></div>
-                <div class="bounce3"></div>
+            <div className="spinner">
+                <div className="bounce1"></div>
+                <div className="bounce2"></div>
+                <div className="bounce3"></div>
             </div>
         )
     }
