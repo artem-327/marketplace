4dc5e0b4bad690b298b114710c045dd1c9cf0ba8
Marek Lhotecký <nerxeus@gmail.com>
2018-06-21 13:03:37 +0200

    rework radio button and finish checkbox component

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -3,6 +3,7 @@ import './dashboard.css'
 import ConMess from '../../components/constructionMessage';
 import Spinner from '../../components/Spinner/Spinner';
 import Radio from "../../components/Radio/Radio";
+import Checkbox from "../../components/Checkbox/Checkbox";
 
 class Dashboard extends Component {
 
@@ -19,29 +20,23 @@ class Dashboard extends Component {
                     <button className='button small'>Submit</button><br/><br/>
                     <button className='button disabled'>Submit</button><br/><br/>
                     <Spinner/>
-                    <div className="input-checkbox">
-                        <label htmlFor="test">
-                            checkbox small
-                            <input type="checkbox" id="test"/>
-                            <span className="checkmark small">  </span>
-                        </label>
-                    </div>
-                    <div className="input-checkbox">
-                        <label htmlFor="test2">
-                            checkbox default
-                            <input type="checkbox" id="test2"/>
-                            <span className="checkmark">  </span>
-                        </label>
-                    </div>
-                    <div className="input-checkbox">
-                        <label htmlFor="test3">
-                            checkbox big
-                            <input type="checkbox" id="test3"/>
-                            <span className="checkmark big">  </span>
-                        </label>
-                    </div>
-                    <Radio css="checkmark" name='foo' opns={[{value:23, label:'vysocina'}, {value:24, label:'Brno'}]} checked={24}/>
-                    <Radio css="checkmark small" name='foo' opns={[{value:25, label:'jihocesky'}, {value:26, label:'Jihlava'}]} checked={25}/>
+                    <Radio name='foo' opns={[{value:23, label:'vysocina'}, {value:24, label:'Brno'}]} checked={24}/>
+                    <Radio style="small" name='foo2' opns={[{value:25, label:'jihocesky'}, {value:26, label:'Jihlava'}]} checked={25}/>
+                    <Checkbox name='fee3' label='foo' onChange={(value) => {console.log(value)}} />
+                    {/*<div className="input-checkbox">*/}
+                    {/*<label htmlFor="test2">*/}
+                    {/*checkbox default*/}
+                    {/*<input type="checkbox" id="test2"/>*/}
+                    {/*<span className="checkmark">  </span>*/}
+                    {/*</label>*/}
+                    {/*</div>*/}
+                    {/*<div className="input-checkbox">*/}
+                    {/*<label htmlFor="test3">*/}
+                    {/*checkbox big*/}
+                    {/*<input type="checkbox" id="test3"/>*/}
+                    {/*<span className="checkmark big">  </span>*/}
+                    {/*</label>*/}
+                    {/*</div>*/}
             </div>
             </div>
         );
