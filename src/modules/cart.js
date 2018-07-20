ca751046dd6e8b88bb4846dcb595251890197904
Michal Hornak <michalhornak@MMH.local>
2018-07-20 14:06:20 +0200

    styled addCart component

--- src/modules/cart.js
+++ src/modules/cart.js
@@ -1,34 +1,23 @@
 const CURRENT_ADDED = "CURRENT_ADDED";
+const CURRENT_ADDED_FULFILLED = "CURRENT_ADDED_FULFILLED";
 
 export const initialState = {
     addCart:{
-        name: 'Test product',
-        merchant: 'Test Merchant',
-        availableProducts: '30pck / 3000lbs',
-        packageSize: '100 lbs',
-        quantity: [
-            {
-                name: '20pcks / $3200',
-                count: 20,
-                price: 3200,
-                id: 1,
-            },
-            {
-                name: '30pcks / $2500',
-                count: 30,
-                price: 2500,
-                id: 2,
-            }
-        ]
+        name: '',
+        merchant: '',
+        availableProducts: '',
+        packageSize: '',
+        quantity: []
     }
 
 };
 
 export default function reducer(state = initialState, action) {
     switch (action.type) {
-        case CURRENT_ADDED: {
+        case CURRENT_ADDED_FULFILLED: {
             return {
                 ...state,
+                addCart: action.payload
             }
         }
         default: {
@@ -37,9 +26,30 @@ export default function reducer(state = initialState, action) {
     }
 }
 
-export function getCurrentAdded() {
+export function getCurrentAdded(id) {
     return {
         type: CURRENT_ADDED,
+        payload: Promise.resolve({
+            
+                name: 'Isopropyl Alcohol',
+                merchant: 'ABC Chemical',
+                availableProducts: '60 pck / 3000 lbs',
+                packageSize: '50 lbs',
+                quantity: [
+                    {
+                        name: '20pcks / $3200',
+                        count: 20,
+                        price: 3200,
+                        id: 1,
+                    },
+                    {
+                        name: '30pcks / $2500',
+                        count: 30,
+                        price: 2500,
+                        id: 2,
+                    }
+                ]
+        })
     }
 }
 
