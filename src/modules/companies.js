befc6436a263647f3e7a31b8119d35f430a198e9
Jonyberry <bartl@reinto.cz>
2018-07-22 17:04:03 +0200

    repair all BrR

--- src/modules/companies.js
+++ src/modules/companies.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+// import axios from 'axios';
 
 const GET_COMPANIES = 'GET_COMPANIES';
 const GET_COMPANIES_FULFILLED = 'GET_COMPANIES_FULFILLED';
@@ -33,50 +33,50 @@ export default function reducer(state = initialState, action) {
 export function fetchAll() {
     return {
         type: GET_COMPANIES,
-        payload: axios.get('/api/v1/companies/').then(response => response.data.data.companies)
-        // payload: Promise.resolve(
-        //     [{
-        //         "id": 1,
-        //         "name": "Company A",
-        //         "offices": [
-        //             {
-        //                 "id": 1,
-        //                 "location": {
-        //                     "id": 1,
-        //                     "country": "United States",
-        //                     "state": "Alabama"
-        //                 }
-        //             }
-        //         ]
-        //     },
-        //     {
-        //         "id": 2,
-        //         "name": "Company B",
-        //         "offices": [
-        //             {
-        //                 "id": 2,
-        //                 "location": {
-        //                     "id": 1,
-        //                     "country": "United States",
-        //                     "state": "Alabama"
-        //                 }
-        //             }
-        //         ]
-        //     },
-        //     {
-        //         "id": 3,
-        //         "name": "Company C",
-        //         "offices": [
-        //             {
-        //                 "id": 3,
-        //                 "location": {
-        //                     "id": 2,
-        //                     "country": "CZ",
-        //                     "state": "Vysocina"
-        //                 }
-        //             }
-        //         ]
-        //     }]
-        // )
+        // payload: axios.get('/api/v1/companies/').then(response => response.data.data.companies)
+        payload: Promise.resolve(
+            [{
+                "id": 1,
+                "name": "Company A",
+                "offices": [
+                    {
+                        "id": 1,
+                        "location": {
+                            "id": 1,
+                            "country": "United States",
+                            "state": "Alabama"
+                        }
+                    }
+                ]
+            },
+            {
+                "id": 2,
+                "name": "Company B",
+                "offices": [
+                    {
+                        "id": 2,
+                        "location": {
+                            "id": 1,
+                            "country": "United States",
+                            "state": "Alabama"
+                        }
+                    }
+                ]
+            },
+            {
+                "id": 3,
+                "name": "Company C",
+                "offices": [
+                    {
+                        "id": 3,
+                        "location": {
+                            "id": 2,
+                            "country": "CZ",
+                            "state": "Vysocina"
+                        }
+                    }
+                ]
+            }]
+        )
     }
 }
--- src/pages/inventory/myInventory/components/BroadcastTargetGroup.js
+++ src/pages/inventory/myInventory/components/BroadcastTargetGroup.js
@@ -10,14 +10,46 @@ class BroadcastTargetGroup extends Component {
             isOpen: false,
             target: [],
             groupSelected: 'include',
+            groupChangeType: 'priceMultiplication',
             filter: this.props.filter,
+            groupAmount: 0
         }
     }
 
     componentWillReceiveProps(nextProps){
         if(nextProps.filter !== this.state.filter){
-            this.setState({filter: nextProps.filter, target: [], groupSelected: 'include', isOpen: false,})
+            this.props.updateTargets(this.props.index, []);
+            this.setState({filter: nextProps.filter, target: [], groupSelected: 'include', groupChangeType: 'priceMultiplication', isOpen: false,})
         }
+    }
+
+    handleChangeAmountGroup(id, value){
+        let newTarget = [];
+        for(let i = 0; i < this.props.items.length; i++){
+            let itemValues = this.checkItemValue(this.props.items[i].company);
+            newTarget.push({visibility: itemValues[0] === 'include', 'company': this.props.items[i].company, updateType: itemValues[1],  amount: value})
+        }
+        this.setState({
+            groupAmount: value,
+            target: newTarget
+        }, ()=> this.props.updateTargets(this.props.index, this.state.target))
+    }
+
+    handleChangeUpdateTypeGroup(id, value){
+        let newTarget = [];
+        for(let i = 0; i < this.props.items.length; i++){
+            let itemValues = this.checkItemValue(this.props.items[i].company);
+            newTarget.push({visibility: itemValues[0] === 'include', 'company': this.props.items[i].company, updateType: value,  amount: itemValues[2]})
+        }
+        // if(this.props.type !== 'company') {
+        //     this.props.updateTargets(this.props.index, [{[this.props.type]: this.props.id, updateType: value, amount: this.state.groupAmount, visibility: this.state.groupSelected}])
+        // }else{
+        //     this.props.updateTargets(this.props.index, newTarget)
+        // }
+        this.setState({
+            groupChangeType: value,
+            target: newTarget
+        }, ()=> this.props.updateTargets(this.props.index, this.state.target))
     }
 
     handleChangeGroup(id, value){
@@ -25,17 +57,52 @@ class BroadcastTargetGroup extends Component {
         if(value === 'include' || value === 'exclude'){
             newTarget = [];
             for(let i = 0; i < this.props.items.length; i++){
-                newTarget.push({visibility: value === 'include', 'company': this.props.items[i].company})
+                let itemValues = this.checkItemValue(this.props.items[i].company);
+                newTarget.push({visibility: value === 'include', 'company': this.props.items[i].company, amount: itemValues[2], updateType: itemValues[1]})
             }
-            if(this.props.type !== 'company') {
-                this.props.updateTargets(this.props.index, [{[this.props.type]: this.props.id, visibility: value === 'include'}])
-            }else{
-                this.props.updateTargets(this.props.index, newTarget)
-            }
+            // if(this.props.type !== 'company') {
+            //     this.props.updateTargets(this.props.index, [{[this.props.type]: this.props.id, visibility: value === 'include', updateType: this.state.groupChangeType, amount: this.state.groupAmount}])
+            // }else{
+            //     this.props.updateTargets(this.props.index, newTarget)
+            // }
         }
         this.setState({
             groupSelected: value,
             target: newTarget
+        }, ()=> this.props.updateTargets(this.props.index, this.state.target))
+    }
+
+    handleChangeAmountItem(id, value){
+        for(let i = 0; i < this.state.target.length; i++){
+            if(this.state.target[i].company === id){
+                let newTarget = this.state.target.slice();
+                newTarget[i].amount = value;
+                this.setState({target: newTarget, groupAmount: ""},()=>{
+                    this.props.updateTargets(this.props.index, this.state.target)
+                });
+                return;
+            }
+        }
+        this.setState({target: [...this.state.target, {visibility: true, company: id, updateType: 'priceMultiplication', amount: value}], groupAmount: ""},()=>{
+            this.props.updateTargets(this.props.index, this.state.target)
+        })
+    }
+
+
+    handleChangeUpdateTypeItem(id, value){
+        for(let i = 0; i < this.state.target.length; i++){
+            if(this.state.target[i].company === id){
+                let newTarget = this.state.target.slice();
+                newTarget[i].updateType = value;
+                newTarget[i].amount = newTarget[i].amount || 0;
+                this.setState({target: newTarget, groupChangeType: null},()=>{
+                    this.props.updateTargets(this.props.index, this.state.target)
+                });
+                return;
+            }
+        }
+        this.setState({target: [...this.state.target, {visibility: true, company: id, updateType: value, amount: 0}], groupChangeType: null},()=>{
+            this.props.updateTargets(this.props.index, this.state.target)
         })
     }
 
@@ -51,7 +118,7 @@ class BroadcastTargetGroup extends Component {
                 return;
             }
         }
-        this.setState({target: [...this.state.target, {visibility: value === 'include', company: id}], groupSelected: newGroupState},()=>{
+        this.setState({target: [...this.state.target, {visibility: value === 'include', company: id, updateType: 'priceMultiplication', amount: 0}], groupSelected: newGroupState},()=>{
             this.props.updateTargets(this.props.index, this.state.target)
         })
     }
@@ -60,25 +127,30 @@ class BroadcastTargetGroup extends Component {
         for(let i = 0; i < this.state.target.length; i++){
             if(this.state.target[i].company === id){
                 if(this.state.target[i].visibility){
-                    return 'include'
+                    return ['include', this.state.target[i].updateType, this.state.target[i].amount]
                 }
-                return 'exclude'
+                return ['exclude', this.state.target[i].updateType, this.state.target[i].amount]
             }
         }
-        return 'include'
+        return ['include', 'priceMultiplication', 0]
     }
 
     renderItems(){
         return this.props.items.map((item, index) => {
-            let value = this.state.groupSelected;
-            if(value === 'custom'){
-                value = this.checkItemValue(item.company)
-            }
+            let itemValues = this.checkItemValue(item.company);
             return (<div key={index} className='br-item-header'>
                 <div className='left-group'>
                     {item.name}
                 </div>
-                <BroadcastConfig item name={item.name + index} value={value} id={item.company} changeBrConfig={(id, value)=>this.handleChangeItem(id, value)}/>
+                <BroadcastConfig item
+                                 name={item.name + index}
+                                 value={itemValues[0]}
+                                 updateType={itemValues[1]}
+                                 id={item.company}
+                                 amount={itemValues[2]}
+                                 changeUpdateType={(id, value)=>this.handleChangeUpdateTypeItem(id, value)}
+                                 changeAmount={(id, value)=>this.handleChangeAmountItem(id, value)}
+                                 changeBrConfig={(id, value)=>this.handleChangeItem(id, value)}/>
                 <div className='clearfix' > </div>
             </div>)
         })
@@ -103,6 +175,10 @@ class BroadcastTargetGroup extends Component {
                            name={this.props.name}
                            id={this.props.id}
                            value={this.state.groupSelected}
+                           updateType={this.state.groupChangeType}
+                           amount={this.state.groupAmount}
+                           changeUpdateType={(id, value)=>this.handleChangeUpdateTypeGroup(id, value)}
+                           changeAmount={(id, value)=>this.handleChangeAmountGroup(id, value)}
                            changeBrConfig={(id, value)=>this.handleChangeGroup(id, value)}/>
                        <div className='clearfix' > </div>
                    </span>
