0deb6b2f1b6ba5c86eddd1205ce6c17c5ccc0a91
adam kovac <kovac@reinto.cz>
2018-07-24 16:27:14 +0200

    DataTables - component table with header and body groups

--- src/pages/test/Testpage.js
+++ src/pages/test/Testpage.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import Spinner from "../../components/Spinner/Spinner";
-import Radio from "../../components/Radio/Radio";
-import Checkbox from "../../components/Checkbox/Checkbox";
-import FilterTag from "../../components/Filter/components/FilterTag/FilterTag";
-import ThreeDots from "../../components/ThreeDots/ThreeDots";
+import DataTable from "../../components/DataTable/DataTable";
 
 const TestPage = props => {
     return<div>
         <div>
+            <DataTable
+                name="table"
+                header={[
+                    1,2,3
+                ]}
+                bodyGroups={[
+                    {
+                        id: 1,
+                        name: 'product',
+                        data: [
+                            [
+                                'neco',
+                                'neco ine',
+                                'neco dalsie',
+                            ],
+                            [
+                                '255',
+                                '1024',
+                                '42',
+                            ]
+                        ],
+                    },
+                ]}
+            />
             <br/>
-            <button className='button big'>Submit</button><br/><br/>
-            <button className='button'>Submit</button><br/><br/>
-            <button className='button small'>Submit</button><br/><br/>
-            <button className='button disabled'>Submit</button><br/><br/>
-            <Radio name='foo' opns={[{value:23, label:'vysocina'}, {value:24, label:'Brno'}]} checked={24}/>
-            <Radio className='small' name='foo2' opns={[{value:25, label:'jihocesky'}, {value:26, label:'Jihlava'}]} checked={25}/>
-            <Checkbox name='fee3' label='foo' onChange={(value) => {console.log(value)}} />
-            <ThreeDots/>
-            <br/>
-            <ThreeDots className="small"/>
-            <Spinner/>
-            <FilterTag/>
         </div>
     </div>;
 };
