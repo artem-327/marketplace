6966f7fe993cdd76e568747d306c2bcb419a3b58
Marek Lhotecký <lhotecky@reinto.cz>
2018-08-10 13:10:31 +0200

    add validation to Location and Pricing component

--- src/pages/inventory/addInventory/components/AddForm/Pricing.js
+++ src/pages/inventory/addInventory/components/AddForm/Pricing.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
-import {Control} from 'react-redux-form';
+import {Control, Errors} from 'react-redux-form';
 import DropdownRedux from "../../../../../components/Dropdown/DropdownRedux";
 // import Checkbox from "../../../../../components/Checkbox/Checkbox";
 import IncrementalPricing from './IncrementalPricing';
 import CheckboxRedux from "../../../../../components/Checkbox/CheckboxRedux";
+import {required, isNumber, min, isCasNumber, messages} from "../../../../../utils/validation";
 
 export default class Pricing extends Component {
     constructor(props){
@@ -19,26 +20,66 @@ export default class Pricing extends Component {
             </div>
             : null;
         return (
-            <div>
 
+            <div>
                     <h6>SET PRICE & RULES</h6>
                 <div>
+                    <Errors
+                        className="form-error"
+                        model=".pricing.price"
+                        show="touched"
+                        messages={{
+                            required: messages.required,
+                            isNumber: messages.isNumber
+                        }}
+                    />
                     <div className='group-item-wr'>
                         <label htmlFor=".pricePr">Price pr (lb)</label>
                         <Control.text model=".pricing.price"
                                       id=".pricePr"
+                                      validators={{
+                                          isNumber,
+                                          required,
+
+                                      }}
                                       placeholder="$"/>
                     </div>
+                    <Errors
+                        className="form-error"
+                        model=".pricing.cost"
+                        show="touched"
+                        messages={{
+                            required: messages.required,
+                            isNumber: messages.isNumber
+                        }}
+                    />
                     <div className='group-item-wr'>
                         <label htmlFor=".costPr">Cost pr (lb)</label>
                         <Control.text model=".pricing.cost"
                                       id=".costPr"
+                                      validators={{
+                                          isNumber,
+                                          required,
+                                      }}
                                       placeholder="$"/>
                     </div>
+                    <Errors
+                        className="form-error"
+                        model=".pricing.margin"
+                        show="touched"
+                        messages={{
+                            required: messages.required,
+                            isNumber: messages.isNumber
+                        }}
+                    />
                     <div className='group-item-wr'>
                         <label htmlFor=".grossMargin">Gross Margin %</label>
                         <Control.text model=".pricing.margin"
                                       id=".grossMargin"
+                                      validators={{
+                                          isNumber,
+                                          required,
+                                      }}
                                       placeholder="$"/>
                     </div>
                     <div className='group-item-wr'>
@@ -54,17 +95,40 @@ export default class Pricing extends Component {
                     {/*</div>*/}
                 {/*</div>*/}
                 <div>
+                    <Errors
+                        className="form-error"
+                        model=".splits"
+                        show="touched"
+                        messages={{
+                            required: messages.required,
+                            isNumber: messages.isNumber
+                        }}
+                    />
                 <div className='group-item-wr'>
                 <label htmlFor=".splits">Splits</label>
                 <Control.text model=".splits"
                               id=".splits"
+                              validators={{
+                                  isNumber,
+                                  required,
+                              }}
                               placeholder="Packages"/>
                 </div>
+                    <Errors
+                        className="form-error"
+                        model="forms.addProductOffer.addProductOffer.minimum"
+                        show="touched"
+                        messages={{
+                            required: messages.required,
+                        }}
+                    />
                 <div className='group-item-wr'>
                 <label htmlFor=".minimum">Minimum</label>
-                <DropdownRedux opns={[{id: 10, name: '10'}]} placeholder='Packages'
-                model="forms.addProductOffer.addProductOffer.minimum"
-                dispatch={this.props.dispatch}/>
+                <DropdownRedux opns={[{id: 10, name: '10'}]}
+                               placeholder='Packages'
+                               model="forms.addProductOffer.addProductOffer.minimum"
+                               validators={{required}}
+                               dispatch={this.props.dispatch}/>
                 </div>
                 </div>
                 <div className='group-item-wr'>
