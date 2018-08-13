1e9c9a7fff637ed04a508aaf68fd3bf2a8badc0c
Marek Lhotecký <lhotecky@reinto.cz>
2018-08-13 15:53:49 +0200

    remove comment of all inventory components

--- src/pages/inventory/allInventory/components/ProductOffers.js
+++ src/pages/inventory/allInventory/components/ProductOffers.js
@@ -92,11 +92,11 @@ class ProductOffers extends Component {
                                     <tr className="product-offer" key={offer.id}>
                                         <td><Checkbox onChange={(value) => {console.log(value)}}/></td>
                                         <td>{offer.merchant.email}</td>
-                                        {/*<td>{offer.packaging.amount}</td>*/}
-                                        {/*<td>{offer.packaging.container.name}</td>*/}
-                                        {/*<td>{offer.packaging.capacity}</td>*/}
-                                        {/*<td>{parseInt(offer.packaging.amount, 10) * parseInt(offer.packaging.capacity, 10)}</td>*/}
-                                        {/*<td>{offer.pricing.price}</td>*/}
+                                        <td>{offer.packaging.amount}</td>
+                                        <td>{offer.packaging.container.name}</td>
+                                        <td>{offer.packaging.capacity}</td>
+                                        <td>{parseInt(offer.packaging.amount, 10) * parseInt(offer.packaging.capacity, 10)}</td>
+                                        <td>{offer.pricing.price}</td>
                                         <td>{offer.name}</td>
                                         <td>{offer.manufacturer}</td>
                                         <td>{offer.origin}</td>
