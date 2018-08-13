21d50d9e77dbe3245ecdc9bf5b48969b5db586fe
Marek Lhotecký <lhotecky@reinto.cz>
2018-08-13 12:12:10 +0200

    change error messages

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,11 +1,9 @@
 export const required = (val) => val && val !== "";
 export const isNumber = (val) => !isNaN(parseFloat(val)) && isFinite(val);
 export const min = (val, min) => val > min;
-export const isCasNumber = (val) => /^[0-9]{2}[-][0-9]{2}[-][0-9]$/.test(val);
 
 export const messages = {
-  required: "Input is required.",
-  isNumber: "Input must be number.",
-  min: "Value must be grater then 0.",
-  isCasNumber: "Must be in format 'xx-xx-x'.",
+  required: "Required",
+  isNumber: "Must be number",
+  min: "Must be grater then 0",
 };
