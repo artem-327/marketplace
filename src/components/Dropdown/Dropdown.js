6c7eb303c0f6c70c911c2055b919032fca1f11db
Marek Lhotecký <lhotecky@reinto.cz>
2018-08-24 12:35:06 +0200

    update css for dropdown - disabled

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -53,10 +53,15 @@ class Dropdown extends Component {
     }
 
     renderDropdown(opt){
-        if(this.props.disable) return;
+        if(this.props.disabled) return;
         return opt.map((option, index)=>{
             return <li key={index + 'dropdown'} onClick={()=>{this.setCurrentValue(option.id, option.name)}}>{option.name}</li>
         });
+    }
+
+    toggleDropdown(){
+        if(this.props.disabled) return;
+        this.setState({isOpen: !this.state.isOpen})
     }
 
     render() {
@@ -67,7 +72,7 @@ class Dropdown extends Component {
             </ul> : null;
         return (
             <div className='dropdown-wr' ref={this.dropdownRef} >
-                <div className={'dropdown-trigger ' + classnames({'disable' : this.props.disable, 'open' : isOpen})} onClick={()=>{this.setState({isOpen: !this.state.isOpen})}}>
+                <div className={'dropdown-trigger ' + classnames({'disabled' : this.props.disabled, 'open' : isOpen})} onClick={()=>this.toggleDropdown()}>
                     <div>{currentValue || this.props.placeholder || 'Select'}<img alt="up" src={ArrowUp} /></div>
                 </div>
                 {options}
@@ -85,7 +90,7 @@ Dropdown.propTypes = {
     currentValue: PropTypes.string,
     placeholder: PropTypes.string,
     onChange: PropTypes.func,
-    disable: PropTypes.bool,
+    disabled: PropTypes.bool,
 };
 
 
