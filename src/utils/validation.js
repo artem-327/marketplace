7afb8bc665fb7e2b4294c6b20b13a63b8aabea68
Marek Lhotecký <lhotecky@reinto.cz>
2018-08-29 15:59:18 +0200

    add validation for Assay min and Assay max: max 100%, must be bigger than min

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,9 +1,17 @@
 export const required = (val) => val && val !== "";
 export const isNumber = (val) => !isNaN(parseFloat(val)) && isFinite(val);
 export const min = (val, min) => val > min;
+export const maxPercent = (val) => val < 101;
+export const bigger = (val, min) => {
+    console.log(val, min);
+    return val > min;
+}
 
 export const messages = {
-  required: "Required",
-  isNumber: "Must be number",
-  min: "Must be grater then 0",
+    required: "Required",
+    isNumber: "Must be number",
+    min: "Must be grater then 0",
+    maxPercent: "Maximum is 100%",
+    bigger: "Must be bigger than min"
+
 };
