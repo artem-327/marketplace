4417b36f3e2bc0d779c77435ffd3a2241e088240
Marek Lhotecký <lhotecky@reinto.cz>
2018-09-07 13:56:15 +0200

    continue on Attachments styling

--- src/pages/inventory/addInventory/components/Upload/UploadLot.js
+++ src/pages/inventory/addInventory/components/Upload/UploadLot.js
@@ -2,6 +2,9 @@ import React, {Component} from 'react';
 import './uploadLot.css'
 import upload from '../../../../../images/upload/upload.png';
 import uploaded from '../../../../../images/upload/uploaded.png';
+import PropTypes from "prop-types";
+import File from "./components/File";
+import ReactDropzone from "react-dropzone";
 
 class UploadLot extends Component {
     constructor(props) {
@@ -19,23 +22,48 @@ class UploadLot extends Component {
         this.setState({files});
     };
 
-    isUploaded() {
-        if (this.state.files === []) return false;
-        if (this.state.files !== []) return true;
-    }
-
     render() {
+        let files = this.state.files.map((file, index) => (
+            <File onRemove={() => this.removeFile(index)} className="file lot" name={file.name}/>));
+        let hasFile = this.state.files.length !== 0;
         return (
-            <div>
-                <input type="file" id="lot" className="upload-item"/>
-                <div className='uploadLot'>
-                    {this.state.isUploaded ? <img className="upload" src={uploaded} alt='drop'/> :
-                        <img className="uploaded" src={upload} alt='drop-close'/>}
-                    <label>Click to upload C of A</label>
-                </div>
+            <div className="uploadLot">
+                <ReactDropzone className="dropzoneLot" activeClassName="active" onDrop={this.onPreviewDrop}>
+                    {this.props.header}
+                    {hasFile ? <img className="uploaded" src={uploaded} alt='drop'/> :
+                        <React.Fragment>
+                            <img className="upload" src={upload} alt='drop-close'/>
+                            <div>
+                                <label>Click to upload C of A</label>
+                            </div>
+                        </React.Fragment>
+                    }
+                </ReactDropzone>
+                <div className="file-space">{files}</div>
             </div>
         )
     }
 }
 
+UploadLot.propTypes = {
+    className: PropTypes.string,
+    content: PropTypes.string,
+    files: PropTypes.array
+};
+
 export default UploadLot;
+
+// render() {
+//     let files = this.state.files.map((file, index) => (<File onRemove={()=>this.removeFile(index)} name={file.name} />));
+//     return (
+//         <div>
+//             <input type="file" id="lot" className="upload-item"/>
+//             <div className='uploadLot'>
+//                 {this.state.isUploaded ? <img className="upload" src={uploaded} alt='drop'/> :
+//                     <img className="uploaded" src={upload} alt='drop-close'/>}
+//                 <label>Click to upload C of A</label>
+//             </div>
+//         </div>
+//     )
+// }
+// }
--- src/pages/inventory/addInventory/components/Upload/components/File.js
+++ src/pages/inventory/addInventory/components/Upload/components/File.js
@@ -6,7 +6,7 @@ class File extends Component {
 
     render () {
         return (
-            <div id={this.props.name} className="file">
+            <div id={this.props.name} className={this.props.className}>
                 <i className="close" onClick={()=>this.props.onRemove()} />
                 {this.props.name}
             </div>
@@ -17,6 +17,7 @@ File.propTypes = {
     name: PropTypes.string,
     close: PropTypes.func,
     onRemove: PropTypes.func,
+    className: PropTypes.string,
 };
 
 export default File;
