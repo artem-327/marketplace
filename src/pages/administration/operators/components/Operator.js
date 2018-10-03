738576fa7465c47b298e2289eef938259146da43
lehu <hubackova@reinto.cz>
2018-10-03 14:51:20 +0200

    refactor Operator to stateless component

--- src/pages/administration/operators/components/Operator.js
+++ src/pages/administration/operators/components/Operator.js
@@ -1,23 +1,28 @@
-import React, {Component} from 'react';
+import React from 'react';
+import PropTypes from "prop-types";
 
-class Operator extends Component {
-    
-    render() {
-        let {firstname, middlename, lastname, email, login} = this.props;
-        return (
-            <tr className="admin-operators-new-item">
-                    <td>{firstname}</td>
-                    <td>{middlename}</td>
-                    <td>{lastname}</td>
-                    <td>{email}</td>
-                    <td>{login}</td>
-                    <td>
-                        <button className="button small red" onClick={()=>this.props.removeOperator(this.props.id)}>Delete</button>
-                        <button className="button small" onClick={()=>this.props.editOperator({...this.props})}>Edit</button>
-                    </td>
-            </tr>
-        )
-    }
-}
+const Operator = ({ firstname, middlename, lastname, email, login }) => {
+    return (
+        <tr className="admin-operators-new-item">
+            <td>{firstname}</td>
+            <td>{middlename}</td>
+            <td>{lastname}</td>
+            <td>{email}</td>
+            <td>{login}</td>
+            <td>
+                <button className="button small red" onClick={() => this.props.removeOperator(this.props.id)}>Delete</button>
+                <button className="button small" onClick={() => this.props.editOperator({ ...this.props })}>Edit</button>
+            </td>
+        </tr>
+    );
+};
 
 export default Operator;
+
+Operator.propTypes = {
+    firstname: PropTypes.string,
+    middlename: PropTypes.string,
+    lastname: PropTypes.string,
+    email: PropTypes.string,
+    login: PropTypes.string,
+};
