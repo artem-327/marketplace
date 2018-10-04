a4c095f07bb710c487a2985d624a11f7d6897cac
Jonyberry <bartl@reinto.cz>
2018-10-04 15:08:24 +0200

    added dataTable init

---
+++ src/pages/testDataTable/TestDataTable.js
@@ -0,0 +1,15 @@
+import React,{Component} from 'react';
+
+
+class TestPage extends Component {
+
+    render(){
+        return (
+            <div>
+                <h1 className='header inv-header'>Test Table</h1>
+            </div>
+        )
+    }
+}
+
+export default TestPage;
