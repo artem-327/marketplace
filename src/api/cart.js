59840b05be52c6202848ecf631b3424228580a11
hubackova <hubackova@reinto.cz>
2018-10-16 14:22:09 +0200

    cart fix

--- src/api/cart.js
+++ src/api/cart.js
@@ -1,11 +1,7 @@
 import axios from 'axios';
 
 const api = {
-<<<<<<< HEAD
-    getProductOffer: (id) =>  axios.get(`/api/ux92h9/product-offers/${id}/`).then(response => response.data.data.productOffer),
-=======
     getCurrentAdded: (id) =>  axios.get(`/api/ux92h9/product-offers/${id}/`).then(response => response.data.data.productOffer),
->>>>>>> origin/master
     fetchCartItems: () => axios.get('/api/25tjry/cart/').then(response => response.data.data.cart),
     fetchDeliveryAddresses: () => axios.get('/api/51zjrk/delivery-addresses/').then(response => response.data.data),
     removeProductFromCart: (id) => axios.delete(`api/jdr9gj/cart/${id}/`),
