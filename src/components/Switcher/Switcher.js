836df80cf7de517370c4c6ee2fd2459635bca908
hubackova <hubackova@reinto.cz>
2018-10-26 13:28:37 +0200

    refactor switcher to functional component

--- src/components/Switcher/Switcher.js
+++ src/components/Switcher/Switcher.js
@@ -1,27 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Switcher.css';
 
-class Switcher extends Component {
-  render() {
-    const sliderType = this.props.isRounded ? "slider round" : "slider"
-    return (
-      <div className="switch-container">
-        <div className="switch-container">
-          <label className="switch">
-            <input 
-              type="checkbox"
-              {...this.props}
-            />
-            <span className={sliderType}></span>
-          </label>
-        </div>
-      </div>
-    )
-  }
-}
+const Switcher = (props) => {
+  const sliderType = props.isrounded ? "slider round" : "slider"
+  return (
+    <div className="switch-container">
+    <div className="switch-container">
+      <label className="switch">
+        <input 
+          type="checkbox"
+          {...props}
+        />
+        <span className={sliderType}></span>
+      </label>
+    </div>
+  </div>
+  );
+};
 
 export default Switcher;
-
-
-
-
--- src/components/Switcher/SwitcherRedux.js
+++ src/components/Switcher/SwitcherRedux.js
@@ -1,27 +1,23 @@
-import React, { Component } from 'react';
-import { Control  } from 'react-redux-form';
+
+import React from 'react';
+import { Control } from 'react-redux-form';
 import Switcher from './Switcher';
 
-class SwitcherRedux extends Component {
-  render() {
-    const { isRounded, model } = this.props;
-    return <Control.checkbox
-    model={model}
-    component={Switcher}
-    mapProps={{
-      value: (props) => props.viewValue,
-    }}
-    {...this.props}
-    controlProps={{
-      isRounded: {isRounded}
-    }}
-  />
-  }
-
-}
+const SwitcherRedux = (props) => {
+  const { isRounded, model } = props;
+  return (
+    <Control.checkbox
+      model={model}
+      component={Switcher}
+      mapProps={{
+        value: (props) => props.viewValue,
+      }}
+      controlProps={{
+        isrounded: { isRounded }
+      }}
+      {...this.props}
+    />
+  );
+};
 
 export default SwitcherRedux;
-
-
-
-
