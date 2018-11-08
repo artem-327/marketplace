f399bd1b0bc74928574ff7f72fb1fdab70ca5544
hubackova <hubackova@reinto.cz>
2018-11-08 15:33:47 +0100

    logout after 30 minutes of inactivity

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import React, { Component } from 'react';
-
 import './app.css';
-
-import Main   from './layout/Main'
+import Main from './layout/Main'
 import Header from './layout/Header'
 import PopUp from './components/PopUp'
 import ErrorsHandler from './utils/errorsHandler'
+import IdleTimer from 'react-idle-timer'
+
 class App extends Component {
-//router paths - Main
+  constructor(props) {
+    super(props)
+    this.idleTimer = null
+  }
+
+  onIdle = (e) => {
+    this.props.history.push('/login/')
+    localStorage.clear()
+    alert("You were logout due to inactivity")
+  }
+
+  //router paths - Main
   render() {
     return (
+      <IdleTimer
+        ref={ref => { this.idleTimer = ref }}
+        element={document}
+        onActive={this.onActive}
+        onIdle={this.onIdle}
+        timeout={1000 * 60 * 30} //logout after 30minutes
+      >
         <div className="App" >
-            <PopUp/>
-            <Header/>
-            <Main/>
-            <ErrorsHandler/>
+          <PopUp />
+          <Header />
+          <Main />
+          <ErrorsHandler />
         </div>
+      </IdleTimer>
     );
   }
-}
 
-// need commit
+}
 
 export default App;
