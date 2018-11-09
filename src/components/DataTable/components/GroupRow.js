125dc9bd07d97258e59a8bff04ac570b3245e19f
Marek Juroško <marek.jurosko@artio.cz>
2018-11-09 18:37:50 +0100

    Design fixes (My/All Inventory)

--- src/components/DataTable/components/GroupRow.js
+++ src/components/DataTable/components/GroupRow.js
@@ -28,9 +28,8 @@ class GroupRow extends Component {
             <React.Fragment>
                 <tr className="data-table-group-header" onClick={(e) => this.toggleGroup(e)}>
                     {this.props.selectable ? <td className="data-table-select" ref={this.checkbox}><CheckboxControlled value={this.isSelected()} onChange={(value) => this.selectGroup(value)}/></td> : null}
-                    {this.props.contextMenu ? <React.Fragment><td/><td/></React.Fragment> : null}
-                    <td  className="group-header" colSpan={(this.props.headers.length)} >{this.props.rows.group}
-                    <span className="data-table-group-count">{this.props.rows.countLabel ? this.props.rows.countLabel + this.props.rows.rows.length : null} {this.state.open ? <i className="icon fas fa-angle-down"/> : <i className="icon fas fa-angle-up"/>}</span></td></tr>
+                    <td  className="group-header" colSpan={(this.props.headers.length+(this.props.contextMenu ? 2 : 0))} >{this.props.rows.group}
+                    <span className="data-table-group-count">{this.props.rows.countLabel ? this.props.rows.countLabel + this.props.rows.rows.length : null} {this.state.open ? <i className="icon angle-down"/> : <i className="icon angle-up"/>}</span></td></tr>
                 {this.state.open ? this.props.rowsOpns.rows.map((opns, index) => (
                     <Row selectable={this.props.selectable} id={opns.id} rowOpns={opns} rowComponent={this.props.rowComponent} contextMenu={this.props.contextMenu} headers={this.props.headers} data={this.props.rows.rows[opns.index].data} selectFunc={this.props.selectFunc} groupId={this.props.rowsOpns.index} key={index}/>
                 )) : null}
