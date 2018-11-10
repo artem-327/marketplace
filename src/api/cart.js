79c4d26d203ce26bc5a3717386b33074d1e92538
Lenka <hubackova.lenka@gmail.com>
2018-11-10 08:51:57 +0100

    add slashes to endpoint calls

--- src/api/cart.js
+++ src/api/cart.js
@@ -2,8 +2,9 @@ import axios from 'axios';
 
 const api = {
     getProductOffer: (id) =>  axios.get(`/api/ux92h9/product-offers/${id}/`).then(response => response.data.data.productOffer),
+    //     getProductOffer: (id) =>  axios.get(`/api/product-offers/${id}/applyrules/`).then(response => response.data.data.productOffer), TODO: call this in the future
     createNewOrder: (order) => axios.post('/api/eXe66/orders/', order),
-    editOrder: (order) => axios.post(`/api/jul096/orders/${order.id}`, {...order}),
+    editOrder: (order) => axios.post(`/api/jul096/orders/${order.id}/`, {...order}),
     getOrderDetail: (id) =>  axios.get(`/api/m600tl/orders/${id}/`).then(response => response.data.data.order),
     fetchCart: () => axios.get('/api/mnU0lp/cart/').then(response => response.data.data.cart),
     removeProductFromCart: (id) => axios.delete(`/api/4eijtx/cart/${id}/`),
