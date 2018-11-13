2c672f1f8377ddd69943c01b19210862ff8516b9
Marek Juroško <marek.jurosko@artio.cz>
2018-11-13 14:05:20 +0100

    Task #28524 - Design modifications for .header-top + Filters

--- src/pages/inventory/allInventory/AllInventory.js
+++ src/pages/inventory/allInventory/AllInventory.js
@@ -3,6 +3,7 @@ import ProductOffers from "./components/ProductOffers";
 import Filter from '../../../components/Filter';
 import Spinner from '../../../components/Spinner/Spinner';
 import FilterTag from "../../../components/Filter/components/FilterTag";
+import SubMenu from '../../../components/SubMenu';
 
 class AllInventory extends Component {
 
@@ -22,8 +23,11 @@ class AllInventory extends Component {
             <ProductOffers {...this.props}/>;
         return (
             <div>
-                <h1 className='header inv-header'>MARKETPLACE</h1>
-                <FilterTag dispatch={this.props.dispatch} closeFunc={(filter) => {this.props.fetchAllProductOffers({...filter})}}/>
+                <div className='header-top'>
+                    <h1 className='header inv-header'>MARKETPLACE</h1>
+                    <SubMenu/>
+                    <FilterTag dispatch={this.props.dispatch} closeFunc={(filter) => {this.props.fetchAllProductOffers({...filter})}}/>
+                </div>
                 <Filter chemicalName quantity date price assay condition form package filterFunc={(inputs) => this.props.fetchAllProductOffers(inputs)} />
                 {content}
             </div>
--- src/pages/inventory/myInventory/MyInventory.js
+++ src/pages/inventory/myInventory/MyInventory.js
@@ -5,6 +5,7 @@ import './myInventory.css';
 import Spinner from "../../../components/Spinner/Spinner";
 import FilterTag from "../../../components/Filter/components/FilterTag";
 import {getSelectedDataTable} from "../../../utils/functions";
+import SubMenu from '../../../components/SubMenu';
 
 const GROUP_BY_ALL_COMPANIES = 1;
 const GROUP_BY_REGIONS = 2;
@@ -97,9 +98,12 @@ class MyInventory extends Component {
                 broadcastActive={this.state.brActive}/>;
         return (
             <div className='my-inventory'>
-                <h1 className='header inv-header'>MY INVENTORY</h1>
-                <FilterTag dispatch={this.props.dispatch} closeFunc={(filter) => {this.props.fetchMyProductOffers({...filter})}}/>
-                <h3 className='header small'>{getSelectedDataTable(this.props.productOffersTable)} product offerings selected</h3>
+                <div className='header-top'>
+                    <h1 className='header inv-header'>MY INVENTORY</h1>
+                    <SubMenu/>
+                    <FilterTag dispatch={this.props.dispatch} closeFunc={(filter) => {this.props.fetchMyProductOffers({...filter})}}/>
+                    <h3 className='header small'>{getSelectedDataTable(this.props.productOffersTable)} product offerings selected</h3>
+                </div>
                 <Filter chemicalName productAgeFilter date assay quantity price condition form filterFunc={(filter) => {this.props.fetchMyProductOffers({...filter})}} />
                 {content}
             </div>
