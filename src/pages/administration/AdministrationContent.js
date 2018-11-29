8f19dd18a3327bd65dd306032b6463a75a756b76
hubackova <hubackova@reinto.cz>
2018-11-29 10:01:39 +0100

    add users data to data table

--- src/pages/administration/AdministrationContent.js
+++ src/pages/administration/AdministrationContent.js
@@ -3,9 +3,7 @@ import React from "react";
 const AdministrationContent = (props) => {
   return (
     <div className="administration-content">
-      <div className="add-group">
         {props.administrComponent}
-      </div>
     </div>
   );
 };
