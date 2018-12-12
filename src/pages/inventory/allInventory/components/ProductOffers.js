4a7032bc8fcde745135f326645a78c33492c2cf0
Marek Lhotecký <lhotecky@reinto.cz>
2018-12-12 15:10:24 +0100

    price fix

--- src/pages/inventory/allInventory/components/ProductOffers.js
+++ src/pages/inventory/allInventory/components/ProductOffers.js
@@ -34,6 +34,7 @@ class ProductOffers extends Component {
                 const location = countryException.includes(countryName)
                     ? `${offer.warehouse.address.city}, ${offer.warehouse.address.province.name}`
                     : `${offer.warehouse.address.city}, ${countryName}`
+                const price = offer.pricing.tiers.length > 1 ? "$" + offer.pricing.tiers[0].price.formatMoney(3) + '-' + offer.pricing.tiers[offer.pricing.tiers.length - 1].price.formatMoney(3) : "$" + offer.pricing.price.formatMoney(3),
 
                 return{
                     id: offer.id,
@@ -41,7 +42,7 @@ class ProductOffers extends Component {
                         offer.pkgAmount.formatNumber(),
                         `${packageSize} ${unit} ${packageUnit}`,
                         `${(parseInt(offer.pkgAmount, 10) * parseInt(offer.packaging.size, 10)).formatNumber()} ${unit}`,
-                        offer.pricing.tiers.length > 1 ? offer.pricing.tiers[0].price.formatMoney(3) + '-' + offer.pricing.tiers[offer.pricing.tiers.length - 1].price.formatMoney(3) : "$" + offer.pricing.price.formatMoney(3),
+                        price,
                         offer.name,
                         offer.manufacturer.name,
                         offer.origin.name,
