a92dde6a1db72d6649e727661d73dc392c46de1e
reintopc <lhotecky@reinto.cz>
2018-12-27 17:34:00 +0100

    change props structure in Product Offering

--- src/pages/inventory/addInventory/AddInventory.js
+++ src/pages/inventory/addInventory/AddInventory.js
@@ -43,7 +43,6 @@ export default class AddInventory extends Component {
 
     render() {
 
-        //console.log(this.props)
         return(
         <div>
             <h1 className='header'>{!this.props.edit ? 'ADD INVENTORY' : 'EDIT PRODUCT OFFER - ' + this.props.productOffer.name}</h1>
