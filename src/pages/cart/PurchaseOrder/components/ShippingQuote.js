8ac49781e42c4afc09d31e2c32a18b8c9637cbe1
Marek Juroško <marek.jurosko@artio.cz>
2019-01-11 11:04:07 +0100

    Task #28463 - Design modifications for Freight Selection (scrollbar, floated header and small design issues fixed)

--- src/pages/cart/PurchaseOrder/components/ShippingQuote.js
+++ src/pages/cart/PurchaseOrder/components/ShippingQuote.js
@@ -4,6 +4,7 @@ import NumberFormat from 'react-number-format'
 import moment from 'moment'
 import Spinner from '../../../../components/Spinner/Spinner'
 import Radio from '../../../../components/Radio/Radio'
+import PerfectScrollbar from 'react-perfect-scrollbar'
 
 class ShippingQuote extends Component {
     state = {}
@@ -21,6 +22,29 @@ class ShippingQuote extends Component {
         this.setState({selectedItem: value});
     }
 
+    handleScrollY() {
+        // get table header height
+        const tableHeaderHeight = document.querySelector('.scrollbar-container.freight-selection-wrapper thead tr th:first-child').clientHeight;
+
+        // get position of scrollbar (0 - 1)
+        let freightWrapper = document.querySelector('.scrollbar-container.freight-selection-wrapper');
+        let scrollPosition = freightWrapper.scrollTop / (freightWrapper.scrollHeight - freightWrapper.clientHeight);
+
+        // get real scroll height (minus table header height)
+        let scrollHeight = (freightWrapper.scrollHeight - freightWrapper.clientHeight - tableHeaderHeight);
+
+        // floated header (move header together with scrolling)
+        let topPosition = document.querySelector('.scrollbar-container.freight-selection-wrapper').scrollTop;
+        let fixHeader = document.querySelectorAll('.freight-selection-wrapper th > .fix-header');
+        for (let i = 0; i < fixHeader.length; i++) {
+            fixHeader[i].style.top = topPosition + 'px';
+        }
+
+        // calculate scrollbar position
+        let yScrollbar = document.querySelector('.scrollbar-container.freight-selection-wrapper > .ps__rail-y');
+        yScrollbar.style.marginTop = scrollPosition * scrollHeight + 'px';
+    }
+
     renderShippingQuotes() {
         if (typeof this.props.shippingQuotes.length === 'undefined' || this.props.shippingQuotes.length < 1 || typeof this.props.selectedAddress.id == 'undefined') {
             return (
@@ -29,16 +53,33 @@ class ShippingQuote extends Component {
         }
 
         return (
-            <>
+            <PerfectScrollbar className="freight-selection-wrapper" onScrollY={this.handleScrollY}>
                 <table className="freight-selection">
                     <thead>
                         <tr>
-                            <th></th>
-                            <th>Carrier</th>
-                            <th className="a-right">Cost</th>
-                            <th>Estimated Delivery</th>
-                            <th>ETD</th>
-                            <th>Service Type</th>
+                            <th>
+                                <div className={'fix-header'}></div>
+                            </th>
+                            <th>
+                                Carrier
+                                <div className={'fix-header'}>Carrier</div>
+                            </th>
+                            <th className="a-right">
+                                Cost
+                                <div className={'fix-header'}>Cost</div>
+                            </th>
+                            <th>
+                                Estimated Delivery
+                                <div className={'fix-header'}>Estimated Delivery</div>
+                            </th>
+                            <th>
+                                ETD
+                                <div className={'fix-header'}>ETD</div>
+                            </th>
+                            <th>
+                                Service Type
+                                <div className={'fix-header'}>Service Type</div>
+                            </th>
                         </tr>
                     </thead>
                     <tbody>
@@ -74,7 +115,7 @@ class ShippingQuote extends Component {
                         })}
                     </tbody>
                 </table>
-            </>
+            </PerfectScrollbar>
         );
     }
 
