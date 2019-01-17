a706fe1cff6a292a3c3aba7c21fecb255840ba30
C:\Program Files\Git <lhotecky@reinto.cz>
2019-01-17 19:34:13 +0100

    Analyzing a bug when after deleting product offer in My Inventory, data is undefined

--- src/components/DataTable/components/GroupRow.js
+++ src/components/DataTable/components/GroupRow.js
@@ -52,17 +52,58 @@ class GroupRow extends Component {
     }
 
     render() {
+        console.log(this.props);
         const {tableType} = this.props
         const isAllInventory = tableType ==="allInventoryTable"
         return (
             <React.Fragment>
                 <tr className="data-table-group-header" onClick={(e) => this.toggleGroup(e)}>
-                    {this.props.selectable ? <td className="data-table-select" ref={this.checkbox}><CheckboxControlled value={this.isSelected()} onClick={isAllInventory ? event => this.handleSelect(event) : () => {}} onChange={(value) => this.selectGroup(value)}/></td> : null}
-                    <td  className="group-header" colSpan={(this.props.headers.length+(this.props.contextMenu ? 2 : 0))} >{this.props.rows.group}
-                    <span className="data-table-group-count">{this.props.rows.countLabel ? this.props.rows.countLabel + this.props.rows.rows.length : null} {this.state.open ? <i className="icon angle-down"/> : <i className="icon angle-up"/>}</span></td></tr>
-                {this.state.open ? this.props.rowsOpns.rows.map((opns, index) => (
-                    <Row selectable={this.props.selectable} history={this.props.history} tableType={this.props.tableType} addPopup={this.props.addPopup} removePopup={this.props.removePopup} id={opns.id} rowOpns={opns} rowComponent={this.props.rowComponent} contextMenu={this.props.contextMenu} headers={this.props.headers} data={this.props.rows.rows[opns.index].data} selectFunc={this.props.selectFunc} groupId={this.props.rowsOpns.index} key={index}/>
-                )) : null}
+                    {
+                        this.props.selectable ?
+                            <td className="data-table-select"
+                                ref={this.checkbox}>
+                                <CheckboxControlled
+                                    value={this.isSelected()}
+                                    onClick={
+                                        isAllInventory ?
+                                            event => this.handleSelect(event)
+                                            : () => {}} onChange={(value) => this.selectGroup(value)}
+                                />
+                            </td>
+                            : null
+                    }
+                    <td  className="group-header"
+                         colSpan={(this.props.headers.length+(this.props.contextMenu ? 2 : 0))} >{this.props.rows.group}
+                        <span
+                            className="data-table-group-count">
+                            {this.props.rows.countLabel ? this.props.rows.countLabel + this.props.rows.rows.length : null}
+                            {this.state.open ? <i className="icon angle-down"/> : <i className="icon angle-up"/>}
+                        </span>
+                    </td>
+                </tr>
+                {this.state.open ?
+                    this.props.rowsOpns.rows.map((opns, index) => {
+                        if (this.props.rows.rows[opns.index]){
+                            return (
+                                <Row selectable={this.props.selectable}
+                                     history={this.props.history}
+                                     tableType={this.props.tableType}
+                                     addPopup={this.props.addPopup}
+                                     removePopup={this.props.removePopup}
+                                     id={opns.id}
+                                     rowOpns={opns}
+                                     rowComponent={this.props.rowComponent}
+                                     contextMenu={this.props.contextMenu}
+                                     headers={this.props.headers}
+                                     data={this.props.rows.rows[opns.index].data}
+                                     selectFunc={this.props.selectFunc}
+                                     groupId={this.props.rowsOpns.index}
+                                     key={index}/>
+                            )
+                        }
+                    }
+                    )
+                    : null}
             </React.Fragment>
         )
 
