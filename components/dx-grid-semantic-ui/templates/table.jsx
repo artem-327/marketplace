5d5a98dbc19afe21e5bf1ee2cd8ca18f4a002a6d
Miroslav Volovec <miroslav.volovec@gmail.com>
2019-03-22 14:30:20 +0100

    Removed unnecessary props from SUI Table

--- components/dx-grid-semantic-ui/templates/table.jsx
+++ components/dx-grid-semantic-ui/templates/table.jsx
@@ -3,6 +3,6 @@ import { Table as TableSUI, Ref } from "semantic-ui-react";
 
 export const Table = ({ tableRef, ...restProps }) => (
   <Ref innerRef={tableRef}>
-    <TableSUI fixed unstackable {...restProps} />
+    <TableSUI {...restProps} />
   </Ref>
 );
