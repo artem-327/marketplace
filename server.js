7cead6f053e4f9c4519030d992bb07df211e14f5
Miroslav Volovec <miroslav.volovec@gmail.com>
2019-03-22 14:14:36 +0100

    fixed PORT name in env

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const routes = require('./routes')
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = routes.getRequestHandler(app)
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 
 app.prepare()
 .then(() => {
