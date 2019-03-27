15883d78915409e3bfa540d9c5c345ba0a57d410
Miroslav Volovec <miroslav.volovec@gmail.com>
2019-03-27 11:14:33 +0100

    Move Add warehouse form into modal

--- src/pages/settings/components/WarehouseTable/AddNewWarehousePopup.js
+++ src/pages/settings/components/WarehouseTable/AddNewWarehousePopup.js
@@ -15,34 +15,35 @@ class EditWarehousePopup extends React.Component {
     } = this.props
 
     return (
-      <Segment padded raised style={{width: '70%', margin: 'auto'}}>
-        <Header>Add new warehouse</Header>
-        <Form
-          model="forms.settingsPopup.addNewWarehouse"
-          onSubmit={(value) => postNewWarehouseRequest(value)}
-          component={SForm}
-        >
-          <FormGroup widths="equal">
-            <FormField label="Warehouse name" control={Control.text} model=".warehouseName" defaultValue={''}/>
-            <FormField label="Contact Name" control={Control.text} model=".contactName" defaultValue={''} />
-          </FormGroup>
-          <FormGroup widths="equal">
-            <FormField label="Address" control={Control.text} model=".address" defaultValue={''} />
-            <FormField label="City" control={Control.text} model=".city" defaultValue={''} />
-            <FormField label="State" control={Control.text} model=".state" defaultValue={''} />
-            <FormField label="Zipcode" control={Control.text} model=".zipCode" defaultValue={''} />
-          </FormGroup>
-          <FormGroup widths="equal">
-            <FormField label="Phone" control={Control.text} model=".phone" defaultValue={''} />
-            <FormField label="e-mail" control={Control.text} model=".email" defaultValue={''} />
-          </FormGroup>
-          
-          <div style={{textAlign: 'right'}}>
-            <Button onClick={closeAddPopup}>Cancel</Button>
-            <Button primary>Save</Button>
-          </div>
-        </Form>
-      </Segment>
+      <Modal open centered={false}>
+        <Modal.Header>Add new warehouse</Modal.Header>
+        <Modal.Content>
+          <Form
+            model="forms.settingsPopup.addNewWarehouse"
+            onSubmit={(value) => postNewWarehouseRequest(value)}
+            component={SForm}
+          >
+            <FormGroup widths="equal">
+              <FormField label="Warehouse name" control={Control.text} model=".warehouseName" defaultValue={''} />
+              <FormField label="Contact Name" control={Control.text} model=".contactName" defaultValue={''} />
+            </FormGroup>
+            <FormGroup widths="equal">
+              <FormField label="Address" control={Control.text} model=".address" defaultValue={''} />
+              <FormField label="City" control={Control.text} model=".city" defaultValue={''} />
+              <FormField label="State" control={Control.text} model=".state" defaultValue={''} />
+              <FormField label="Zipcode" control={Control.text} model=".zipCode" defaultValue={''} />
+            </FormGroup>
+            <FormGroup widths="equal">
+              <FormField label="Phone" control={Control.text} model=".phone" defaultValue={''} />
+              <FormField label="e-mail" control={Control.text} model=".email" defaultValue={''} />
+            </FormGroup>
+          </Form>
+        </Modal.Content>
+        <Modal.Actions>
+          <Button onClick={closeAddPopup}>Cancel</Button>
+          <Button primary>Save</Button>
+        </Modal.Actions>
+      </Modal>
     )
   }
 }
