2943db1c95dd2b0f1e445f27c92ba0fb2a762507
Miroslav Volovec <miroslav.volovec@gmail.com>
2019-04-05 10:58:23 +0200

    Fixed body style at login page

--- pages/_document.js
+++ pages/_document.js
@@ -4,31 +4,21 @@ import { ServerStyleSheet } from 'styled-components'
 export default class MyDocument extends Document {
   static getInitialProps({ renderPage }) {
     const sheet = new ServerStyleSheet()
-    let bodyClassName
-    const page = renderPage((App) => {
-      bodyClassName = App.bodyClassName
-      return (props) => sheet.collectStyles(<App {...props} bodyClassName={App.bodyClassName} />)
-    })
+    const page = renderPage((App) => (props) => sheet.collectStyles(<App {...props} />),)
     const styleTags = sheet.getStyleElement()
 
-    return {
-      ...page,
-      styleTags,
-      bodyClassName
-    }
+    return { ...page, styleTags }
   }
 
   render() {
-    const {styleTags, bodyClassName} = this.props
-
     return (
       <html>
         <Head>
-          {styleTags}
+          {this.props.styleTags}
           <meta charSet='utf-8' />
           <meta name='viewport' content='initial-scale=1.0, width=device-width' />
         </Head>
-        <body className={bodyClassName}>
+        <body>
           <Main />
           <NextScript />
         </body>
