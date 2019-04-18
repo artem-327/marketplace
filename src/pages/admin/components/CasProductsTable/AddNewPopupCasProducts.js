0839d8446322d529e3fd300d9e32d057b5d70b2c
Miroslav Volovec <miroslav.volovec@gmail.com>
2019-04-18 09:36:13 +0200

    Optimized unNumbers searching

--- src/pages/admin/components/CasProductsTable/AddNewPopupCasProducts.js
+++ src/pages/admin/components/CasProductsTable/AddNewPopupCasProducts.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
-import { Modal, FormGroup, Header } from 'semantic-ui-react'
+import { Modal, FormGroup, Header, Dropdown as SDropdown, FormField } from 'semantic-ui-react'
 
 import { closeAddPopup, postNewCasProductRequest } from '../../actions'
-import { Form, Input, Button, Dropdown, Field  } from 'formik-semantic-ui'
+import { Form, Input, Button, Dropdown, Field } from 'formik-semantic-ui'
 import * as Yup from 'yup'
 
 const initialFormValues = {
-  'casIndexName':   '',
-  'casNumber':      '',
-  'chemicalName':   '',
-  'unNumber':       '',
-  'hazardClasses':  [],
+  'casIndexName': '',
+  'casNumber': '',
+  'chemicalName': '',
+  'unNumber': '',
+  'hazardClasses': [],
   'packagingGroup': '',
 }
 
@@ -23,26 +23,26 @@ const formValidation = Yup.object().shape({
 })
 
 class AddNewPopupCasProducts extends React.Component {
-  render() {
-    const {
-      closeAddPopup,
-      currentTab,
-      config,
-      postNewCasProductRequest
-    } = this.props
 
+  state = {
+    unNumbers: [],
+    unNumbersReduced: [],
+    packagingGroups: [],
+    hazardClasses: []
+  }
+
+  initCollections = () => {
     const unNumbers = this.props.unNumbers.map(d => {
-      //console.log('xxxxxxxxxx - map unNumbers');
       return {
         key: d.id,
         text: d.unNumberCode,
         value: d.id,
+        dataSearch: d.unNumberCode + ' ' + d.description,
         content: <Header content={d.unNumberCode} subheader={d.description} />,
       }
     })
 
     const packagingGroups = this.props.packagingGroups.map(d => {
-      console.log('xxxxxxxxxx - map packagingGroups');
       return {
         key: d.id,
         text: d.groupCode,
@@ -52,7 +52,6 @@ class AddNewPopupCasProducts extends React.Component {
     })
 
     const hazardClasses = this.props.hazardClasses.map(d => {
-      console.log('xxxxxxxxxx - map hazardClasses');
       return {
         key: d.id,
         text: d.classCode,
@@ -61,74 +60,118 @@ class AddNewPopupCasProducts extends React.Component {
       }
     })
 
+    
+    this.setState({
+      unNumbers,
+      packagingGroups,
+      hazardClasses
+    })
+  }
+
+  filterUnNumbers = (list, query) => {
+    const unNumbersReduced = this.state.unNumbers.filter(({dataSearch}) => dataSearch.indexOf(query) > 1).map(({dataSearch, ...item}) => item)
+    setTimeout(() => this.setState({unNumbersReduced}), 1)
+    return unNumbersReduced
+  }
+
+  componentWillMount() {
+    this.initCollections()
+  }
+
+  render() {
+    const {
+      closeAddPopup,
+      currentTab,
+      config,
+      postNewCasProductRequest
+    } = this.props
+
+    const {
+      unNumbers,
+      hazardClasses,
+      packagingGroups
+    } = this.state
+
     return (
       <Modal open centered={false}>
-        <Modal.Header>Add { config.addEditText }</Modal.Header>
+        <Modal.Header>Add {config.addEditText}</Modal.Header>
         <Modal.Content>
           <Form
+            enableReinitialize
             initialValues={initialFormValues}
-            validationSchema={formValidation}
+            // validationSchema={formValidation}
+            validateOnBlur={false}
+            validateOnChange={false}
             onReset={closeAddPopup}
             onSubmit={(values, actions) => {
               const data = {
                 casIndexName: values.casIndexName,
-                casNumber:    values.casNumber,
+                casNumber: values.casNumber,
                 chemicalName: values.chemicalName,
-                ...(values.unNumber !== '' && {unNumber: values.unNumber}),
-                ...(values.packagingGroup !== '' && {packagingGroup: values.packagingGroup}),
-                ...(values.hazardClasses.length && {hazardClasses: values.hazardClasses}),
+                ...(values.unNumber !== '' && { unNumber: values.unNumber }),
+                ...(values.packagingGroup !== '' && { packagingGroup: values.packagingGroup }),
+                ...(values.hazardClasses.length && { hazardClasses: values.hazardClasses }),
               }
-              console.log('xxxxxxxxxxx AddNewPopupCasProducts - submit values - ', values);
-              console.log('xxxxxxxxxxx AddNewPopupCasProducts - submit data - ', data);
-              postNewCasProductRequest(data);
+              console.log('xxxxxxxxxxx AddNewPopupCasProducts - submit values - ', values)
+              console.log('xxxxxxxxxxx AddNewPopupCasProducts - submit data - ', data)
+              postNewCasProductRequest(data)
             }}
           >
-            <FormGroup widths="equal">
-              <Input type='text' label={config.display.columns[0].title} name="casIndexName" />
-            </FormGroup>
-            <FormGroup widths="equal">
-              <Input type='text' label={config.display.columns[1].title} name="casNumber" />
-              <Input type='text' label={config.display.columns[2].title} name="chemicalName" />
-            </FormGroup>
-            <FormGroup widths="equal">
-              <Dropdown
-                name="unNumber"
-                label={config.display.columns[3].title} options={unNumbers}
-                inputProps={{
-                  selection: true,
-                  search: true,
-                  placeholder: 'Choose an option',
-                }}
-              />
-            </FormGroup>
-            <FormGroup widths="equal">
-              <Dropdown
-                name="packagingGroup"
-                label={config.display.columns[4].title} options={packagingGroups}
-                inputProps={{
-                  selection: true,
-                  search: true,
-                  placeholder: 'Choose an option',
-                }}
-              />
-            </FormGroup>
-            <FormGroup widths="equal">
-              <Dropdown
-                name="hazardClasses"
-                label={config.display.columns[5].title}
-                options={hazardClasses}
-                inputProps={{
-                  placeholder: 'Choose an option',
-                  multiple: true,
-                  selection: true,
-                  search: true,
-                }}
-              />
-            </FormGroup>
-            <div style={{ textAlign: 'right' }}>
-              <Button.Reset>Cancel</Button.Reset>
-              <Button.Submit>Save</Button.Submit>
-            </div>
+            {(props) => { return (
+              <>
+                <FormGroup widths="equal">
+                  <Input type='text' label={config.display.columns[0].title} name="casIndexName" />
+                </FormGroup>
+                <FormGroup widths="equal">
+                  <Input type='text' label={config.display.columns[1].title} name="casNumber" />
+                  <Input type='text' label={config.display.columns[2].title} name="chemicalName" />
+                </FormGroup>
+                <FormGroup widths="equal">
+                  <Dropdown
+                    name="unNumber"
+                    fast
+                    label={config.display.columns[3].title} 
+                    options={this.state.unNumbersReduced}
+                    inputProps={{
+                      selection: true,
+                      search: this.filterUnNumbers,
+                      placeholder: 'Search for UN Number (type at least 3 characters)',
+                      minCharacters: 3,
+                      clearable: true
+                    }}
+                  />
+                </FormGroup>
+                <FormGroup widths="equal">
+                  <Dropdown
+                    name="packagingGroup"
+                    label={config.display.columns[4].title} options={packagingGroups}
+                    inputProps={{
+                      selection: true,
+                      search: true,
+                      placeholder: 'Choose an option',
+                    }}
+                  />
+                </FormGroup>
+                <FormGroup widths="equal">
+                  <Dropdown
+                    name="hazardClasses"
+                    label={config.display.columns[5].title}
+                    options={hazardClasses}
+                    inputProps={{
+                      placeholder: 'Choose an option',
+                      multiple: true,
+                      selection: true,
+                      search: true,
+                    }}
+                  />
+                </FormGroup>
+                <div style={{ textAlign: 'right' }}>
+                  <Button.Reset>Cancel</Button.Reset>
+                  <Button.Submit>Save</Button.Submit>
+                </div>
+              </>
+            )}}
+
           </Form>
         </Modal.Content>
       </Modal>
@@ -139,10 +182,10 @@ class AddNewPopupCasProducts extends React.Component {
 const mapDispatchToProps = {
   closeAddPopup,
   postNewCasProductRequest,
-};
+}
 
 const mapStateToProps = state => {
-  let cfg = state.admin.config[state.admin.currentTab];
+  let cfg = state.admin.config[state.admin.currentTab]
   return {
     config: cfg,
     currentTab: state.admin.currentTab,
@@ -150,6 +193,6 @@ const mapStateToProps = state => {
     unNumbers: state.admin.unNumbers,
     hazardClasses: state.admin.hazardClasses,
   }
-};
+}
 
 export default connect(mapStateToProps, mapDispatchToProps)(AddNewPopupCasProducts)
