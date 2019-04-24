e30d6fa79d1c5bee929489bd81d7f8304e781ba1
David Gomola <dave.gomola13@gmail.com>
2019-04-24 15:09:00 +0200

    #30030 Error message display

--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,13 @@
 import Head from 'next/head'
 import Link from 'next/link'
-import {withRouter} from 'next/router'
-import {Container, Menu, Image, Dropdown} from 'semantic-ui-react'
+import { withRouter } from 'next/router'
+import { Container, Menu, Image, Dropdown } from 'semantic-ui-react'
 import styled from 'styled-components'
 import Logo from '~/assets/images/nav/inventory.png'
 import ErrorsHandler from '~/src/utils/errorsHandler'
 import NavigationMenu from './NavigationMenu'
 import PopUp from '~/src/components/PopUp'
-import cn from "classnames"
+import ErrorMessage from './message/ErrorMessage'
 
 const TopMenu = styled(Menu)`
   background-color: #33373e !important;
@@ -26,38 +26,40 @@ const LogoImage = styled(Image)`
   height: 23.78px;
 `
 
-const MenuLink = withRouter(({router: {pathname}, to, children}) => (
+const MenuLink = withRouter(({ router: { pathname }, to, children }) => (
   <Link prefetch href={to}>
-    <Menu.Item as="a" active={pathname === to}>{children}</Menu.Item>
+    <Menu.Item as='a' active={pathname === to}>{children}</Menu.Item>
   </Link>
 ))
 
-const Layout = ({children, router: {pathname}, title = "Echo exchange"}) => (
+const Layout = ({ children, router: { pathname }, title = 'Echo exchange' }) => (
   <MainContainer fluid>
     <PopUp />
-    <ErrorsHandler />
     <Head>
       <title>Echo exchange / {title}</title>
     </Head>
-    <TopMenu fixed="top" inverted size="large" borderless>
-      
+    <TopMenu fixed='top' inverted size='large' borderless>
+
       <TopMenuContainer fluid>
         <LogoImage src={Logo} />
 
         <NavigationMenu />
-        
-        <Menu.Menu position="right" className='black'>
+
+        <Menu.Menu position='right' className='black'>
           <Dropdown item icon={{ name: 'user circle outline', size: 'large' }}>
             <Dropdown.Menu>
-              <Dropdown.Item as={MenuLink} to="/auth/logout">Logout</Dropdown.Item>
+              <Dropdown.Item as={MenuLink} to='/auth/logout'>Logout</Dropdown.Item>
             </Dropdown.Menu>
           </Dropdown>
         </Menu.Menu>
       </TopMenuContainer>
+
+
     </TopMenu>
-    
+    <ErrorMessage />
+
     <ContentContainer fluid className='page-wrapper'>
-      {children} 
+      {children}
     </ContentContainer>
 
   </MainContainer>
