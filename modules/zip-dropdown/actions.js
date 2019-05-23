c7edfebd7862b4b1912f7839234c40c43f1c0086
David Gomola <dave.gomola13@gmail.com>
2019-05-23 11:26:21 +0200

    Zip dropdown bugs fixes

--- modules/zip-dropdown/actions.js
+++ modules/zip-dropdown/actions.js
@@ -2,5 +2,5 @@ import * as AT from './action-types'
 import * as api from './api'
 
 
-export const addZip = (zip) => ({ type: AT.ADD_ZIP, payload: api.addZip(zip) })
+export const addZip = (zip) => ({ type: AT.ADD_ZIP, payload: zip })
 export const getZipCodes = (params = {}) => ({ type: AT.GET_ZIP_CODES, payload: api.getZipCodes(params) })
--- modules/zip-dropdown/api.js
+++ modules/zip-dropdown/api.js
@@ -1,6 +1,5 @@
 import api from '~/api'
 
-export const addZip = (zip) => new Promise((resolve) => setTimeout(() => resolve({ zip, id: Math.floor(Math.random() * 1234) }), 2000)) // Probably won't be used...
 export const getZipCodes = (params) => {
   let queryString = '?'
   let keys = Object.keys(params)
