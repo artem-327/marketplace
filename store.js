f52b205085df35dd0214b521cec1186021539f57
Miroslav Volovec <miroslav.volovec@gmail.com>
2019-05-29 13:05:53 +0200

    Cleanup store, moved settings into proper location

--- store.js
+++ store.js
@@ -1,44 +1,162 @@
-// import { createStore, applyMiddleware, combineReducers } from 'redux'
-// import { composeWithDevTools } from 'redux-devtools-extension'
-// import { createLogger } from 'redux-logger'
-// import thunkMiddleware from 'redux-thunk'
-// import promiseMiddleware from 'redux-promise-middleware'
-// import { combineForms } from 'react-redux-form'
-// import createSagaMiddleware from 'redux-saga'
+import { createStore, applyMiddleware } from 'redux'
+import { createLogger } from 'redux-logger'
+import thunk from 'redux-thunk'
+import promise from 'redux-promise-middleware'
+import { combineReducers, compose } from 'redux'
+import { combineForms } from 'react-redux-form'
+import createSagaMiddleware from 'redux-saga'
+import { loadState, saveState } from '~/utils/storePersist'
+import { throttle } from 'lodash'
 
-// const logger = createLogger({
-//   predicate: (getState, action) => process.env.NODE_ENV === "development"
-// })
-// const sagaMiddleware = createSagaMiddleware()
+// import jwtDecode from 'jwt-decode'
+// import moment from "moment"
 
-// // Orders
-// import ordersReducers from './modules/orders/reducers'
-// import ordersSaga from './modules/orders/saga'
+// import identity, {initialState as identityFormInit, logout} from './modules/identity'
+import identity, { initialState as identityFormInit } from '~/src/modules/identity'
+import users from '~/src/modules/users'
 
-// const reducers = combineReducers({
-//   orders: ordersReducers
-// })
+import companies from '~/src/modules/companies'
+import productOffers, { initialState as addProductsInit } from '~/src/modules/productOffers'
+import shippingQuotes, { initialState as shippingQuotesInit } from '~/src/modules/shippingQuotes'
+import popup from '~/src/modules/popup'
+import filter, { initialState as filterInit } from '~/src/modules/filter'
+import packageTypes from '~/src/modules/packageTypes'
+import brcRules, { initialState as broadcastInit } from "~/src/modules/broadcast"
+// import cart, { initialState as cartInit } from "./modules/cart"
+import merchants, { initialState as merchantsInit } from "~/src/modules/merchants"
+import products, { initialState as productsInit } from '~/src/modules/products'
+import location from '~/src/modules/location'
+import errors from "~/src/modules/errors"
+import dataTables from "~/src/modules/dataTables"
+import settings from '~/modules/settings/reducers'
+import admin from '~/src/pages/admin/reducers'
 
-// const middleware = applyMiddleware(
-//   thunkMiddleware, 
-//   sagaMiddleware, 
-//   promiseMiddleware,
-//   logger, 
-// )
+import { show as saveFilterItem } from '~/src/components/Filter/components/SavedFilters/reducers/SaveFilterItem.reducers'
+import companiesSaga from "~/src/saga/companies"
+import officesSaga from "~/src/saga/offices"
+import merchantsSaga from "~/src/saga/merchants"
+import usersSaga from "~/src/pages/administration/users/saga/users"
+import operatorsSaga from "~/src/pages/administration/operators/saga/operators"
+import cartSaga from "~/src/pages/cart/saga/cart"
+import locationsSaga from "~/src/saga/locations"
+import broadcastSaga from "~/src/saga/broadcast"
+import productOffersSaga from "~/src/saga/productOffers"
+import shippingQuotesSaga from "~/src/saga/shippingQuotes"
 
-// export function initializeStore (initialState = {}) {
-//   const store = createStore(
-//     reducers,
-//     initialState,
-//     composeWithDevTools(middleware)
-//   )
+// Simple Add/Edit Inventory
+import simpleAdd from '~/modules/inventory/reducer'
+import shiping from '~/modules/shipping/reducer'
+import cart, { initialState as cartInit } from '~/modules/purchase-order/reducer'
+// Orders
+import ordersReducers from '~/src/pages/orders/reducers'
+import ordersSaga from '~/src/pages/orders/saga'
 
-//   sagaMiddleware.run(
-//     ordersSaga
-//   )
+import zip from '~/modules/zip-dropdown/reducer'
+import businessTypes from '~/modules/company-form/reducer'
 
-//   return store
-// }
+import auth from '~/modules/auth/reducer'
+import broadcast from '~/modules/broadcast/reducer'
 
-import store from './src/store'
-export default store
+const reducer = combineReducers({
+  auth,
+  admin,
+  businessTypes,
+  identity,
+  brcRules,
+  companies,
+  users,
+  location,
+  productOffers,
+  shippingQuotes,
+  shiping,
+  products,
+  packageTypes,
+  cart,
+  popup,
+  merchants,
+  filter,
+  errors,
+  dataTables,
+  saveFilterItem,
+  simpleAdd,
+  orders: ordersReducers,
+  zip,
+  forms: combineForms({
+    filter: filterInit.data,
+    brcRules: broadcastInit.broadcastData,
+    addProductOffer: addProductsInit.addProductOffer,
+    shippingQuotes: shippingQuotesInit.shippingQuotes,
+    productMapping: productsInit.productsMapping,
+    productOffering: productsInit.productOffering,
+    loginForm: identityFormInit.loginForm.data,
+    registrationForm: identityFormInit.registrationForm.data,
+    merchants: merchantsInit,
+    cart: cartInit,
+    shippingEdit: {},
+    settingsPopup: {
+      editWarehouse: {},
+      addNewWarehouse: {},
+      newProduct: {}
+    }
+  }, 'forms'),
+  settings,
+  broadcast,
+})
+
+const logger = createLogger({
+  predicate: (getState, action) => process.env.NODE_ENV === "development"
+})
+
+// Middleware to check token expiration and potentially redirect user to login package
+// const checkTokenExpirationMiddleware = store => next => action => {
+//     const token = localStorage.getItem('jwtoken')
+//     if (token) {
+//         const expirationTime = moment(jwtDecode(token).exp)
+//         const nowTime = moment(Date.now() / 1000)
+//       if (expirationTime < nowTime) {
+//         next(action)
+//         store.dispatch(logout())
+//       }
+//     }
+//     next(action)
+//   }
+
+const composeEnhancers =
+  typeof window === 'object' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose
+
+
+export const makeStore = (preloadedState) => {
+  // create the saga middleware
+  const sagaMiddleware = createSagaMiddleware()
+
+  const middleware = composeEnhancers(applyMiddleware(thunk, promise(), sagaMiddleware, logger))
+  // const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+
+  const { auth } = loadState() || {}
+  let store = createStore(reducer, { auth }, middleware)
+  // let store = createStore(reducer, middleware)
+
+  store.subscribe(throttle(() => {
+    const { auth } = store.getState()
+    saveState({ auth })
+  }, 1000))
+
+  sagaMiddleware.run(companiesSaga)
+  sagaMiddleware.run(officesSaga)
+  sagaMiddleware.run(usersSaga)
+  sagaMiddleware.run(operatorsSaga)
+  sagaMiddleware.run(merchantsSaga)
+  sagaMiddleware.run(cartSaga)
+  sagaMiddleware.run(locationsSaga)
+  sagaMiddleware.run(broadcastSaga)
+  sagaMiddleware.run(productOffersSaga)
+  sagaMiddleware.run(shippingQuotesSaga)
+  //sagaMiddleware.run(simpleAddSaga)
+  sagaMiddleware.run(ordersSaga)
+
+  return store
+}
