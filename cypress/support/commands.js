31713d24cddd90c4c6e4699b465e4b6f3b8d2d7f
Karel Snajdr <karel.snajdr@artio.cz>
2019-05-30 15:13:01 +0200

    #30282 ADD: login and logout commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,33 @@
 //
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add("login", (email, password) => {
+    cy.server();
+    //This is the post call we are interested in capturing
+    cy.route('POST', '/prodex/oauth/token').as('login');
+    cy.visit("");
+    cy.url().should("include","login");
+    cy.get("input[name=username]")
+        .type(email);
+    cy.get("input[name=password]")
+        .type(password);
+    cy.get("button[type=submit]").click();
+
+    cy.wait('@login');
+
+    //Assert on XHR
+    cy.get('@login').then(function (xhr) {
+        expect(xhr.status).to.eq(200);
+        expect(xhr.responseBody).to.have.property('access_token');
+    });
+});
+
+Cypress.Commands.add("logout", () => {
+    cy.get(".right.menu .user.circle").click("center");
+    cy.get(".right.menu .item.dropdown").should("have.class","visible");
+    cy.get(".right.menu .item.dropdown").contains("Logout").click("center");
+    cy.url().should("include","/login");
+    cy.visit("admin");
+    cy.url().should("include","/login");
+});
