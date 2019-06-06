c1093b9c6703dc685e7c80f05f4c14a104af4752
David Gomola <dave.gomola13@gmail.com>
2019-06-06 09:58:30 +0200

    Checkout page design adjustments

--- modules/purchase-order/components/Payment.js
+++ modules/purchase-order/components/Payment.js
@@ -2,23 +2,38 @@ import React, { Component } from 'react'
 import { FormattedMessage } from 'react-intl'
 import { Dropdown, GridRow, GridColumn, Divider } from 'semantic-ui-react'
 import ShippingAddress from './ShippingAddress'
-
+import Link from 'next/link'
 
 export default class Payment extends Component {
 
   render() {
     let { payments, selectedAddress } = this.props
-
+    let columns = payments.length === 0
+      ? { computer: 16 }
+      : { computer: 8 }
     return (
       <>
         <GridRow>
-          <GridColumn computer={8}>
-            <Dropdown
-              options={payments}
-              fluid
-              selection
-              placeholder={<FormattedMessage id='cart.selectBankAccount' />}
-            />
+          <GridColumn {...columns}>
+            {
+              payments.length === 0
+                ? <FormattedMessage
+                  id='payments.notAvailableAddOne'
+                  values={{
+                    addBankAccount: <FormattedMessage id='payments.addBankAccount'>
+                      {(text) => (
+                        <Link href='/settings?type=bank-accounts'>{text}</Link>
+                      )}
+                    </FormattedMessage>
+                  }} />
+                : <Dropdown
+                  options={payments}
+                  fluid
+                  selection
+                  placeholder={<FormattedMessage id='cart.selectBankAccount' />}
+                />
+            }
+
           </GridColumn>
         </GridRow>
 
