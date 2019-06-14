7ddccf1c95f3800e7d8bc6748e8de8811aa0cb39
Miroslav Volovec <miroslav.volovec@gmail.com>
2019-06-14 13:32:19 +0200

    Fixed profile page/module imports

--- modules/profile/components/Profile.js
+++ modules/profile/components/Profile.js
@@ -1,31 +1,22 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Container, Grid, Divider } from 'semantic-ui-react'
-import Layout from "../../../pages/dashboard";
-
-
-
 
 class Profile extends Component {
 
-    render() {
+  render() {
 
-        return (
-            <Layout title="Dashboard">
-                <h1>My Profile 123</h1>
-            </Layout>
-
-
-
-        )
-    }
+    return (
+      <h1>My Profile 123</h1>
+    )
+  }
 }
 
 
 const mapStateToProps = state => {
-    return {
-        ...state.profile,
-    }
+  return {
+    ...state.profile,
+  }
 }
 
 export default connect(mapStateToProps, null)(Profile)
