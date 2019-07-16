cbce430388d8a5966f0e5b2211ed3dc1c5332199
Tomas Drlicek <tomas.drlicek@ataccama.com>
2019-07-16 15:47:07 +0200

    Tests template, create itam test

---
+++ cypress/integration/inventory.broadcast.cspec.js
@@ -0,0 +1,18 @@
+context("Inventory Broadcasting", () => {
+
+    beforeEach(function () {
+        cy.login("user1@example.com", "echopass123")
+    })
+
+    it('Start item broadcasting', () => {
+
+    })
+
+    it('Stop item broadcasting', () => {
+
+    })
+
+    it('Set custom broadcasting', () => {
+
+    })
+})
---
+++ cypress/integration/inventory.crud.cpec.js
@@ -0,0 +1,67 @@
+context("Inventory CRUD",() => {
+	
+	beforeEach(function () {
+		cy.login("user1@example.com","echopass123")
+	})
+
+    it('Create item',() => {
+		cy.server()
+		cy.route('/prodex/api/products/own/search?pattern=iso&onlyMapped=false').as('search')
+
+        cy.url().should("include","inventory")
+
+		cy.contains("Inventory").click()
+		cy.contains("Add Inventory").click()
+
+		cy.wait(500)
+		cy.url().should("include","add")
+
+		cy.get("#field_dropdown_product")
+			.children("input")
+			.type("iso")
+			.should("have.value","iso")
+
+		cy.wait('@search')
+		cy.wait(1000)
+		cy.contains("IP550 Isopropanol").click({force: true})
+
+		cy.get("#field_dropdown_warehouse").click()
+		cy.get("div[name='Test 2']").first().click()
+		cy.get("#field_input_pkgAmount")
+			.type("5")
+			.should("have.value","05")
+
+		cy.get("input[name='pricingTiers[0].price']")
+			.scrollIntoView()
+			.type("5")
+			.should("have.value","5")
+
+		cy.contains("Add Product Offer").click()
+		cy.contains("Go to My Inventory").click()
+
+		cy.contains("Isopropanol").should('be.visible')
+		cy.contains("IP550").should('be.visible')
+		cy.contains("Test 2").should('be.visible')
+		cy.contains("5").should('be.visible')
+    })
+
+    it('See item details',() => {
+       
+    })
+
+	it('Update item',() => {
+       
+    })
+	
+	it('Delete item',() => {
+       
+    })
+
+    it('Create item validation',() => {
+
+    })
+
+    it('Create item with optional info',() => {
+
+    })
+})
