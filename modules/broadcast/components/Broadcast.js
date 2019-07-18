25df64fbd26ca156d0adfa778b175be8eec1f708
David Gomola <dave.gomola13@gmail.com>
2019-07-18 13:33:47 +0200

    Implemented broadcast templates

--- modules/broadcast/components/Broadcast.js
+++ modules/broadcast/components/Broadcast.js
@@ -3,19 +3,46 @@ import pt from 'prop-types'
 import { connect } from 'react-redux'
 import _ from 'lodash'
 import * as Actions from '../actions'
-import { Modal, Grid, Icon, Button, Form, Input, Dropdown, Dimmer, Loader, Message, Menu } from 'semantic-ui-react'
+import { Modal, Grid, Icon, Button, Form, Input, Dropdown, Dimmer, Loader, Message, Menu, Divider, Header, GridRow, GridColumn } from 'semantic-ui-react'
+import { Formik } from 'formik'
+import { Input as FormikInput, Button as FormikButton, Dropdown as FormikDropdown } from 'formik-semantic-ui'
 import { withToastManager } from 'react-toast-notifications'
 import { generateToastMarkup } from '~/utils/functions'
 import TreeModel from 'tree-model'
 import { Rule } from './Broadcast.style'
 import RuleItem from './RuleItem'
+import { FormattedMessage, injectIntl } from 'react-intl'
+import * as Yup from 'yup'
+
+import styled from 'styled-components'
+
+import confirm from '~/src/components/Confirmable/confirm'
+import { errorMessages } from '~/constants/yupValidation'
+
+
+const BottomUnpaddedRow = styled(GridRow)`
+  padding-bottom: 0px !important;
+`
+
+
+const templateInitialValues = {
+  name: ''
+}
+
+const templateValidation = () => (
+  Yup.object().shape({
+    name: Yup.string().required(errorMessages.requiredMessage)
+  })
+)
+
 
 
 class Broadcast extends Component {
 
   state = {
     filterSearch: '',
-    tree: new TreeModel().parse({model: {rule:{}} })
+    tree: new TreeModel().parse({ model: { rule: {} } }),
+    selectedTemplate: null
   }
 
   constructor(props) {
@@ -24,26 +51,30 @@ class Broadcast extends Component {
     this.handleFilterChange = _.debounce(this.handleFilterChange, 250)
   }
 
+  componentDidMount() {
+    this.props.getTemplates()
+  }
+
   componentWillReceiveProps(props) {
-    this.setState({ 
+    this.setState({
       filterSearch: props.filter.search,
       tree: this.getFilteredTree(props.treeData, props.filter)
     })
   }
 
   handlePriceChange = () => {
-    this.setState({tree: this.state.tree})
+    this.setState({ tree: this.state.tree })
   }
 
   handleChange = (node) => {
-    this.setState({tree: this.state.tree})
+    this.setState({ tree: this.state.tree })
   }
 
   handleRowClick = (node) => {
     node.model.expanded = !node.model.expanded
     if (!node.model.expanded) node.all(n => n.model.expanded = false)
-    
-    this.setState({tree: this.state.tree})
+
+    this.setState({ tree: this.state.tree })
   }
 
   handleSearchChange = (e, { name, value }) => {
@@ -83,7 +114,7 @@ class Broadcast extends Component {
 
     const presets = {
       region: () => new TreeModel().parse({
-        name: "By region",
+        name: 'By region',
         rule: treeData.model,
         depth: 1,
         children: treeData.children.filter(n => searchParentFn(n)).map(n1 => ({
@@ -104,7 +135,7 @@ class Broadcast extends Component {
         }))
       }),
       branch: () => new TreeModel().parse({
-        name: "By company",
+        name: 'By company',
         rule: treeData.model,
         depth: 1,
         children: _.uniqBy(treeData.all(n => n.model.type === 'company'), n => n.model.id).filter(searchFn).map(n1 => ({
@@ -131,16 +162,51 @@ class Broadcast extends Component {
     return tree
   }
 
+
+  onTemplateSelected = (_, data, setFieldValue) => {
+    let name = data.options.find((opt) => opt.value === data.value).text
+    setFieldValue('name', name)
+    this.setState({ selectedTemplate: { name, id: data.value } })
+
+    this.props.getTemplate(data.value)
+  }
+
+  handleTemplateDelete = async () => {
+    const { deleteTemplate, toastManager, intl } = this.props
+    const { formatMessage } = intl
+    let { name, id } = this.state.selectedTemplate
+    await confirm(
+      formatMessage({ id: 'confirm.deleteTemplate.title' }),
+      formatMessage({ id: 'confirm.deleteTemplate.content' }, { name })
+    )
+
+    await deleteTemplate(id)
+
+    toastManager.add(generateToastMarkup(
+      <FormattedMessage id='notifications.templateDelete.header' />,
+      <FormattedMessage id='notifications.templateDelete.content' values={{ name }} />,
+    ), { appearance: 'success' })
+  }
+
   render() {
-    const { open, loading, treeData, filter, closeBroadcast, saveRules, id, mode, switchMode, toastManager, offer } = this.props
+    const {
+      open, loading, treeData,
+      filter, closeBroadcast, saveRules,
+      id, mode, switchMode,
+      saveTemplate, toastManager, offer,
+      templates, intl, updateTemplate,
+      deleteTemplate } = this.props
+
     const broadcastToBranches = treeData && `${treeData.all(n => n.model.type === 'branch' && n.getPath().filter(_n => _n.model.broadcast === 1).length > 0).length}/${treeData.all(n => n.model.type === 'branch').length}`
 
+    const { formatMessage } = intl
+
     return (
-      <Modal open={open} onClose={closeBroadcast} centered={false} size="large">
+      <Modal open={open} onClose={closeBroadcast} centered={false} size='large'>
         <Modal.Header>Broadcast Controls</Modal.Header>
-        <Modal.Content scrolling style={{ minHeight: '70vh' }} className="flex stretched">
-          <Grid className="flex stretched">
-            <Grid.Row divided className="flex stretched">
+        <Modal.Content scrolling style={{ minHeight: '70vh' }} className='flex stretched'>
+          <Grid className='flex stretched'>
+            <Grid.Row divided className='flex stretched'>
               <Grid.Column width={6}>
                 <div>
                   <Message info size='large' style={{ padding: '6px 15px' }}>
@@ -152,7 +218,7 @@ class Broadcast extends Component {
                       <label>Category filter</label>
                       <Dropdown
                         selection
-                        name="category"
+                        name='category'
                         value={filter.category}
                         onChange={this.handleFilterChange}
                         options={[
@@ -163,9 +229,103 @@ class Broadcast extends Component {
                     </Form.Field>
                     <Form.Field>
                       <label>Filter</label>
-                      <Input name="search" icon='search' iconPosition='left' value={this.state.filterSearch} onChange={this.handleSearchChange} />
+                      <Input name='search' icon='search' iconPosition='left' value={this.state.filterSearch} onChange={this.handleSearchChange} />
                     </Form.Field>
                   </Form>
+                  <Divider />
+                  <Formik
+                    initialValues={templateInitialValues}
+                    validationSchema={templateValidation()}
+                    validateOnChange={false}
+                    enableReinitialize
+                    onSubmit={async (values, { setSubmitting }) => {
+                      let { name, id } = this.state.selectedTemplate
+                      let payload = {
+                        mappedBroadcastRules: {
+                          ...treeData.model
+                        },
+                        ...values
+                      }
+
+
+                      // TODO When BE is ready; recode it so it is if found in dropdown(templates) not if is same as active dropdown item
+
+                      if (values.name === name) {
+                        await confirm(
+                          formatMessage({ id: 'broadcast.overwriteTemplate.header' }, { name }),
+                          formatMessage({ id: 'broadcast.overwriteTemplate.content' })
+                        )
+
+                        await updateTemplate(id, payload)
+
+
+                      } else {
+                        await saveTemplate(payload)
+                      }
+
+
+                      let status = values.name === name ? 'Updated' : 'Saved'
+
+                      toastManager.add(generateToastMarkup(
+                        <FormattedMessage id={`notifications.template${status}.header`} />,
+                        <FormattedMessage id={`notifications.template${status}.content`} values={{ name }} />
+                      ), { appearance: 'success' })
+
+
+
+                      setSubmitting(false)
+                    }}
+                    render={props =>
+                      <Form onSubmit={props.handleSubmit}>
+                        <Grid>
+                          <BottomUnpaddedRow>
+                            <GridColumn computer={16}>
+                              <Header as='h4'><FormattedMessage id='broadcast.templates' defaultMessage='Templates' /></Header>
+                            </GridColumn>
+                          </BottomUnpaddedRow>
+
+                          <GridRow>
+                            <GridColumn computer={12}>
+                              <Dropdown
+                                fluid selection
+                                onChange={(e, data) => this.onTemplateSelected(e, data, props.setFieldValue)}
+                                options={[
+                                  { key: 1, value: 1, text: 'Johanka Z Parku' },
+                                  { key: 2, value: 2, text: 'Sam Doma' }
+                                ]
+                                  // templates.map((template) => ({
+                                  //   key: template.id,
+                                  //   text: template.name,
+                                  //   value: template.id
+                                  // }))
+                                } />
+                            </GridColumn>
+                            <GridColumn computer={4}>
+                              <Button
+                                onClick={this.handleTemplateDelete}
+                                disabled={!this.state.selectedTemplate}
+                                loading={this.props.templateDeleting}
+                                type='button' basic fluid negative><FormattedMessage id='global.delete' /></Button>
+                            </GridColumn>
+                          </GridRow>
+
+                          <GridRow>
+                            <GridColumn computer={12}>
+                              <FormikInput
+                                inputProps={{
+                                  fluid: true,
+                                  placeholder: formatMessage({ id: 'broadcast.templateName', defaultMessage: 'Template Name' })
+                                }} name='name' />
+                            </GridColumn>
+
+                            <GridColumn computer={4}>
+                              <FormikButton.Submit loading={this.props.templateSaving} fluid positive basic><FormattedMessage id='global.save' defaultMessage='Save' /></FormikButton.Submit>
+                            </GridColumn>
+                          </GridRow>
+                        </Grid>
+                      </Form>
+                    }>
+                  </Formik>
                 </div>
               </Grid.Column>
               <Grid.Column width={10} stretched>
@@ -202,35 +362,35 @@ class Broadcast extends Component {
         </Modal.Content>
         <Modal.Actions>
           <Button onClick={() => closeBroadcast()}>Cancel</Button>
-          <Button primary 
-            onClick={async () => { 
+          <Button primary
+            onClick={async () => {
               console.log(treeData.model)
               await saveRules(id, treeData.model)
               toastManager.add(generateToastMarkup(
-                "Saved successfully!",
-                "New broadcast rules have been saved."
+                'Saved successfully!',
+                'New broadcast rules have been saved.'
               ), {
-                appearance: 'success'
-              })
+                  appearance: 'success'
+                })
             }}
           >
             Save
           </Button>
         </Modal.Actions>
-      </Modal>
+      </Modal >
     )
   }
 }
 
-export default withToastManager(connect(({ broadcast: { data, filter, ...rest } }) => {
-  const treeData = data 
-    ? new TreeModel({ childrenPropertyName: 'elements' }).parse(data) 
-    : new TreeModel().parse({model: {rule:{}} })
+export default injectIntl(withToastManager(connect(({ broadcast: { data, filter, ...rest } }) => {
+  const treeData = data
+    ? new TreeModel({ childrenPropertyName: 'elements' }).parse(data)
+    : new TreeModel().parse({ model: { rule: {} } })
 
   return {
     treeData,
     filter,
     ...rest,
   }
-}, Actions)(Broadcast))
+}, { ...Actions })(Broadcast)))
 
--- modules/broadcast/reducer.js
+++ modules/broadcast/reducer.js
@@ -1,15 +1,19 @@
 import typeToReducer from 'type-to-reducer'
-import { openBroadcast, closeBroadcast, updateFilter, saveRules, switchMode } from './actions'
+import { openBroadcast, closeBroadcast, updateFilter, saveRules, switchMode, saveTemplate, getTemplates, getTemplate, updateTemplate, deleteTemplate } from './actions'
 
 const initialState = {
   id: null,
   offer: {
-    pricingTiers: [{price: 0}],
+    pricingTiers: [{ price: 0 }],
     price: {}
   },
   open: false,
   loading: false,
   data: null,
+  templateSaving: false,
+  loadingTemplates: false,
+  templateDeleting: false,
+  templates: [],
   filter: {
     search: '',
     category: 'region'
@@ -69,6 +73,91 @@ export default typeToReducer({
   [saveRules.rejected]: (state) => ({
     ...state,
     loading: false
-  })
+  }),
+
+  [saveTemplate.pending]: (state) => ({
+    ...state,
+    templateSaving: true
+  }),
+
+  [saveTemplate.fulfilled]: (state) => ({
+    ...state,
+    templateSaving: false,
+    // TODO - add to templates array
+  }),
+
+  [saveTemplate.rejeted]: (state) => ({
+    ...state,
+    templateSaving: false
+  }),
+
+  [getTemplates.pending]: (state) => ({
+    ...state,
+    loadingTemplates: true
+  }),
+
+  [getTemplates.fulfilled]: (state, { payload }) => ({
+    ...state,
+    loadingTemplates: false,
+    templates: payload // not tested; BE has some issue at get templates endpoint
+  }),
+
+  [getTemplate.pending]: (state) => ({
+    ...state,
+    loading: true
+  }),
+
+  [getTemplate.fulfilled]: (state, { payload }) => {
+    let { id, mappedBroadcastRules } = payload
+
+    return ({
+      ...state,
+      id,
+      data: mappedBroadcastRules,
+      loading: false
+    })
+  },
+
+  [getTemplate.rejected]: (state, ) => ({
+    ...state,
+    loading: false
+  }),
+
+  [getTemplates.rejected]: (state) => ({
+    ...state,
+    loadingTemplates: false
+  }),
+
+  
+  [updateTemplate.pending]: (state) => ({
+    ...state,
+    templateSaving: true
+  }),
+
+  [updateTemplate.fulfilled]: (state) => ({
+    ...state,
+    templateSaving: false
+  }),
+
+  [updateTemplate.rejected]: (state) => ({
+    ...state,
+    templateSaving: false
+  }),
+
+  [deleteTemplate.pending]: (state) => ({
+    ...state,
+    templateDeleting: true
+  }),
+
+  [deleteTemplate.fulfilled]: (state) => ({
+    ...state,
+    // TODO - remove from templates array
+    templateDeleting: false
+  }),
+
+  [deleteTemplate.rejected]: (state) => ({
+    ...state,
+    templateDeleting: false
+  }),
 
 }, initialState)
