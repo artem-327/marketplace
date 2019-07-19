7f37fa2624ba6b88e7ee4723ee4b2b736cecafd3
Tomas Drlicek <tomas.drlicek@ataccama.com>
2019-07-19 09:33:51 +0200

    Propagating first item id

--- cypress/integration/inventory.broadcast.cspec.js
+++ cypress/integration/inventory.broadcast.cspec.js
@@ -10,49 +10,46 @@ context("Inventory Broadcasting", () => {
         cy.get(".table-responsive").scrollTo("right")
         cy.wait(1000)
 
-        cy.get("[data-test=my_inventory_broadcast]").eq(0).should("not.have.class","checked")
+        cy.get("[data-test=my_inventory_broadcast]").eq(0).should("not.have.class", "checked")
         cy.get("[data-test=my_inventory_broadcast]").eq(0).click()
         cy.wait(1000)
-        cy.get("[data-test=my_inventory_broadcast]").eq(0).should("have.class","checked")
+        cy.get("[data-test=my_inventory_broadcast]").eq(0).should("have.class", "checked")
     })
 
     xit('Stop item broadcasting', () => {
         cy.get(".table-responsive").scrollTo("right")
         cy.wait(1000)
 
-        cy.get("[data-test=my_inventory_broadcast]").eq(0).should("have.class","checked")
+        cy.get("[data-test=my_inventory_broadcast]").eq(0).should("have.class", "checked")
         cy.get("[data-test=my_inventory_broadcast]").eq(0).click()
         cy.wait(1000)
-        cy.get("[data-test=my_inventory_broadcast]").eq(0).should("not.have.class","checked")
+        cy.get("[data-test=my_inventory_broadcast]").eq(0).should("not.have.class", "checked")
     })
 
     it('Set custom broadcasting', () => {
-        let itemId = 0
+       cy.getToken().then(token => {
+            cy.getFirstItemId(token).then(itemId => {
+                cy.get('[data-test=action_' + itemId + ']').click()
+                cy.get('[data-test=action_' + itemId + '_1]').click()
 
-        cy.getToken().then(token =>{
-            cy.getFirstItemId(token).then(id => {
-                itemId = id
+                cy.get('div[type=region]').eq(0).within(() => {
+                    cy.get("input[type=checkbox]").click({force: true})
+                    cy.get("input[type=number]").type("5")
+                })
+
+                cy.contains("Save").click()
+
+                cy.contains("Saved successfully!")
+
+                cy.get('[data-test=action_' + itemId + ']').click()
+                cy.get('[data-test=action_' + itemId + '_1]').click()
+
+                cy.get('div[type=region]').eq(0).within(($region) => {
+                    cy.get("input[type=checkbox]").should("have.class", "checked")
+                    cy.get("input[type=number]").should("have.value", "5")
+                })
             })
-        })
 
-        cy.get('[data-test=action_'+itemId+']').click()
-        cy.get('[data-test=action_'+itemId+'_1]').click()
-
-        cy.get('div[type=region]').eq(0).within(() => {
-            cy.get("input[type=checkbox]").click({force: true})
-            cy.get("input[type=number]").type("5")
-        })
-
-        cy.contains("Save").click()
-
-        cy.contains("Saved successfully!")
-
-        cy.get('[data-test=action_'+itemId+']').click()
-        cy.get('[data-test=action_'+itemId+'_1]').click()
-
-        cy.get('div[type=region]').eq(0).within(($region) => {
-            cy.get("input[type=checkbox]").should("have.class","checked")
-            cy.get("input[type=number]").should("have.value","5")
         })
     })
 })
