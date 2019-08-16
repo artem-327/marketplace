811ea9fa900326b5f7e807979678cf839b24698d
David Gomola <dave.gomola13@gmail.com>
2019-08-16 09:55:13 +0200

    Broadcast changes (#30919 + #30974)

--- modules/broadcast/components/RuleItem.js
+++ modules/broadcast/components/RuleItem.js
@@ -5,7 +5,6 @@ import { Rule } from './Broadcast.style'
 const EmptyIconSpace = () => <span style={{ width: '1.18em', display: 'inline-block', marginRight: '0.25rem' }}>&nbsp;</span>
 
 const RuleItem = (props) => {
-
   const {
     onChange,
     onPriceChange,
@@ -22,6 +21,12 @@ const RuleItem = (props) => {
     const value = rule[propertyName]
     const newValue = value === 1 ? 0 : 1
 
+    if (item.hasChildren()) {
+      item.walk((node) => {
+        node.model.rule.broadcast = newValue
+      })
+    } 
+    
     rule[propertyName] = newValue
 
     onChange(rule)
@@ -32,14 +37,29 @@ const RuleItem = (props) => {
   }
 
   const nodePath = item.getPath()
-  const allChildrenBroadcasting = item.hasChildren() && item.all(n => !n.hasChildren()).length === item.all(n => !n.hasChildren() && n.model.rule.broadcast === 1).length
+  let allChildrenBroadcasting = false, anyChildBroadcasting = false
+
+
+  if (item.hasChildren()) {
+    var all = item.all(n => !n.hasChildren()).length
+    var broadcasted = item.all(n => !n.hasChildren() && n.model.rule.broadcast === 1).length
+
+    anyChildBroadcasting = broadcasted > 0
+    allChildrenBroadcasting = all !== 0 && broadcasted !== 0 && all === broadcasted
+
+    if (allChildrenBroadcasting) item.model.rule.broadcast = 1
+    else if (!anyChildBroadcasting) item.model.rule.broadcast = 0
+    else item.model.rule.broadcast = 2
+  }
+
   const broadcastedParents = nodePath.reverse().slice(1).filter(n => n.model.rule.broadcast === 1)
   const parentBroadcasted = broadcastedParents.reverse()[0]
+  const nodeBroadcast = rule.broadcast === 1 || allChildrenBroadcasting ? 1 : 0
 
-  const nodeBroadcast = !!parentBroadcasted || rule.broadcast === 1 || allChildrenBroadcasting ? 1 : item.first(n => n.model.rule.broadcast === 1) ? 2 : 0
-  
-  const toggleDisabled = !!parentBroadcasted
+
+  // const toggleDisabled = !!parentBroadcasted
   const priceDisabled = !(rule.broadcast === 1 && !parentBroadcasted) //allChildrenBroadcasting || rule.broadcast !== 1 || toggleDisabled
+
 
   return (
     <>
@@ -54,19 +74,19 @@ const RuleItem = (props) => {
             data-test='broadcast_rule_toggle_chckb'
             toggle
             fitted
-            indeterminate={nodeBroadcast === 2}
+            indeterminate={anyChildBroadcasting && !allChildrenBroadcasting}
             checked={nodeBroadcast === 1}
-            disabled={toggleDisabled}
-            onClick={(e) => !toggleDisabled && handleChange('broadcast', e)}
+            // disabled={toggleDisabled}
+            onClick={(e) => handleChange('broadcast', e)}
           />
         </Rule.Toggle>
         <PriceControl
           data-test='broadcast_price_control'
-          offer={offer} 
-          disabled={priceDisabled} 
-          rootRule={parentBroadcasted ? parentBroadcasted.model.rule : null} 
-          rule={rule} 
-          onChange={onPriceChange} 
+          offer={offer}
+          disabled={priceDisabled}
+          rootRule={parentBroadcasted ? parentBroadcasted.model.rule : null}
+          rule={rule}
+          onChange={onPriceChange}
         />
       </Rule.Row>
 
