79b0910825627850dd63748658fb1b7b30029f01
Marek Juroško <marek.jurosko@artio.cz>
2019-08-29 12:48:46 +0200

    Task #31118 - static GTM code if not defined (problem to reach environment variables)

--- pages/_app.js
+++ pages/_app.js
@@ -16,7 +16,7 @@ import shortid from 'shortid'
 import { ToastProvider } from 'react-toast-notifications'
 import TagManager from 'react-gtm-module'
 
-const gtmId = process.env.REACT_APP_GTM_ID
+const gtmId = process.env.REACT_APP_GTM_ID || 'GTM-MKSVRW4'
 
 const tagManagerArgs = { gtmId: gtmId }
 
