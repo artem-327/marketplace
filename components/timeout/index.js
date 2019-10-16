2a520b8b90cbd320cac40ab7d8eb55c1165ac9c7
David Gomola <dave.gomola13@gmail.com>
2019-10-16 12:05:48 +0200

    Idle Timer fixes

--- components/timeout/index.js
+++ components/timeout/index.js
@@ -2,7 +2,7 @@ import { Component } from 'react'
 import { Modal, Button, Form } from 'semantic-ui-react'
 import Router from 'next/router'
 import IdleTimer from 'react-idle-timer'
-import { IDLE_TIMEOUT, refreshToken } from '~/utils/auth'
+import { refreshToken } from '~/utils/auth'
 import moment from 'moment'
 import styled from 'styled-components'
 import { FormattedMessage } from 'react-intl'
@@ -18,7 +18,9 @@ export default class TimeoutWarning extends Component {
 
   state = {
     warningOpen: false,
-    remainingTime: WARNING_OFFSET
+    remainingTime: WARNING_OFFSET,
+    timeout: null,
+    loading: false
   }
 
   componentWillUnmount() {
@@ -51,32 +53,51 @@ export default class TimeoutWarning extends Component {
     refreshToken()
   }
 
-  resetIdleTimer = () => {
-    refreshToken()
+  resetIdleTimer = async () => {
+    this.setState({ loading: true })
+    await refreshToken()
     this.idleTimer.reset()
     clearInterval(this.checkTimeInterval)
     this.setState({
       remainingTime: WARNING_OFFSET,
       warningOpen: false
     })
+    this.setIdleTimeout()
+
+    this.setState({ loading: false })
+  }
+
+  setIdleTimeout = () => {
+    let ttl = window.localStorage.getItem('ttl')
+    let date = new Date(parseInt(ttl, 10))
+
+    this.setState({ timeout: moment(date).diff(moment()) })
+  }
+
+  componentDidMount() {
+    this.setIdleTimeout()
   }
 
   render() {
-    const { warningOpen } = this.state
+    const { warningOpen, timeout } = this.state
+
+    if (!timeout) return null
+
+
 
     return (
       <>
         <IdleTimer
           ref={r => this.idleTimer = r}
-          timeout={IDLE_TIMEOUT - WARNING_OFFSET}
+          timeout={timeout - WARNING_OFFSET}
           onIdle={this.handleIdle}
-          onAction={this.handleAction}
+          // onAction={this.handleAction}
           // debounce={10000}
-          // throttle={15 * (60 * 1000)}
-          debounce={15 * (60 * 1000)}
+          // throttle={(10 * 1000)}
+          // debounce={(1000)}
           stopOnIdle={true}
         />
-        <Modal open={warningOpen} closeIcon onClose={() => Router.push(`/auth/logout`)} size='tiny' style={{ width: 400 }} centered={false}>
+        <Modal open={warningOpen} closeIcon onClose={this.resetIdleTimer} size='tiny' style={{ width: 400 }} centered={false}>
           <Modal.Header>
             <FormattedMessage id='auth.sessionTimeout.modalHeader' defaultMessage='SESSION TIMEOUT' />
           </Modal.Header>
@@ -98,7 +119,7 @@ export default class TimeoutWarning extends Component {
             <Button basic data-test='logout_timeout_logout_btn' color='blue' onClick={() => Router.push(`/auth/logout`)}>
               <FormattedMessage id='auth.sessionTimeout.buttonLogOut' defaultMessage='Log Out' />
             </Button>
-            <Button primary data-test='logout_timeout_keep_working_btn' onClick={this.resetIdleTimer}>
+            <Button loading={this.state.loading} primary data-test='logout_timeout_keep_working_btn' onClick={this.resetIdleTimer}>
               <FormattedMessage id='auth.sessionTimeout.buttonKeepWorking' defaultMessage='Keep Working'>{text => text}</FormattedMessage>
             </Button>
           </Modal.Actions>
