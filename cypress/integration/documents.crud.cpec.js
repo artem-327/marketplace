7d7195e5764a73f39ce3ab84b209bf8e4e0c0568
Tomas Drlicek <tomas.drlicek@ataccama.com>
2019-10-20 18:46:14 +0200

    Fixed user tests

--- cypress/integration/documents.crud.cpec.js
+++ cypress/integration/documents.crud.cpec.js
@@ -7,7 +7,7 @@ context("Company Documents CRUD", () => {
         cy.route("POST", "/prodex/api/product-offers/own/datagrid*").as("inventoryLoading")
         cy.route("POST", "/prodex/api/attachments/datagrid/").as("documentsLoading")
 
-        cy.login("user1@example.com", "echopass123")
+        cy.FElogin("user1@example.com", "echopass123")
 
         cy.url().should("include", "inventory")
 
@@ -21,54 +21,49 @@ context("Company Documents CRUD", () => {
     })
 
     it("Adds a document", () => {
-        cy.clickAdd()
+        cy.get("[data-test='settings_open_popup_btn']").click()
 
-        //TODO Fix after FE adjustments
         cy.fixture("cy.png", "base64").then(fileContent => {
-            cy.get("input[type='file']").upload({ fileContent, fileName: "cy.png", mimeType: "image/png" });
-        });
+            cy.get("input[type='file']").upload({fileContent, fileName: "cy.png", mimeType: "image/png"})
+        })
 
         cy.enterText("#field_input_customName", "Cypress")
-        cy.selectFromDropdown("div[id='field_dropdown_documentType.id']","Certificate of Origin")
+        cy.get("div[id='field_dropdown_documentType.id']").click()
+        cy.contains("Certificate of Origin").click()
 
         cy.clickSave()
 
         cy.contains("Document Added")
 
-        let filter = [{"operator":"LIKE","path":"Attachment.name","values":["%cy%"]}]
+        let filter = [{"operator": "LIKE", "path": "Attachment.name", "values": ["%cy%"]}]
 
         cy.getToken().then(token => {
             cy.getFirstAttachmentWithFilter(token, filter).then(itemId => {
-                cy.get('[data-test=action_' + itemId + ']').click()
-
-                cy.get('[data-test=action_' + itemId + '_0]').click()
+                cy.openElement(itemId, 0)
 
                 attachmentId = itemId
             })
         })
 
         cy.get("#field_input_customName")
-            .should("have.value","Cypress")
+            .should("have.value", "Cypress")
 
         cy.contains("Certificate of Origin")
     })
 
     it("Edits a document", () => {
-        cy.get('[data-test=action_' + attachmentId + ']').click({force: true})
-        cy.get('[data-test=action_' + attachmentId + '_0]').click({force: true})
+        cy.openElement(attachmentId, 0)
 
         cy.get("#field_input_customName")
             .clear()
             .type("Cypress")
-            .should("have.value","Cypress")
+            .should("have.value", "Cypress")
 
         cy.clickSave()
 
-        cy.get('[data-test=action_' + attachmentId + ']').click({force: true})
-        cy.get('[data-test=action_' + attachmentId + '_0]').click({force: true})
-
+        cy.openElement(attachmentId, 0)
         cy.get("#field_input_customName")
-            .should("have.value","Cypress")
+            .should("have.value", "Cypress")
     })
 
     it("Checks error messages", () => {
@@ -77,15 +72,14 @@ context("Company Documents CRUD", () => {
         cy.clickSave()
 
         cy.get(".error")
-            .should("have.length",2)
+            .should("have.length", 2)
             .find(".sui-error-message").each((element) => {
             expect(element.text()).to.match(/(Required)/i)
         })
     })
 
     it("Deletes a document", () => {
-        cy.get('[data-test=action_' + attachmentId + ']').click({force: true})
-        cy.get('[data-test=action_' + attachmentId + '_1]').click({force: true})
+        cy.openElement(attachmentId, 1)
 
         cy.clickSave()
 
