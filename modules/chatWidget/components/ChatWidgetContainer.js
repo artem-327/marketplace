d4d59ee3c839376cd27215a0d791cf257f345697
David Gomola <dave.gomola13@gmail.com>
2019-10-23 10:46:41 +0200

    Typo fix

--- modules/chatWidget/components/ChatWidgetContainer.js
+++ modules/chatWidget/components/ChatWidgetContainer.js
@@ -1,7 +1,7 @@
 import { connect } from 'react-redux'
-import ChatWidget from './chatWidget'
+import ChatWidget from './ChatWidget'
 import * as Actions from '../actions'
-import {injectIntl} from "react-intl";
+import { injectIntl } from 'react-intl'
 import { getSafe } from '~/utils/functions'
 
 function mapStateToProps({ chatWidget }) {  // currently not used (usd only actions & reducer)
