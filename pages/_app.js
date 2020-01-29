ace47da4d0e809643cf0207ebfe79d6a67a90bec
Marek Juroško <marek.jurosko@artio.cz>
2020-01-29 10:19:24 +0100

    Task #32487 - Toasts redesign

--- pages/_app.js
+++ pages/_app.js
@@ -16,6 +16,7 @@ import 'nprogress/nprogress.css'
 import shortid from 'shortid'
 import { ToastProvider } from 'react-toast-notifications'
 import TagManager from 'react-gtm-module'
+import { AlertCircle, AlertTriangle, CheckCircle, X } from 'react-feather'
 
 const gtmId = process.env.REACT_APP_GTM_ID || 'GTM-MKSVRW4'
 
@@ -24,6 +25,38 @@ const tagManagerArgs = { gtmId: gtmId }
 Router.events.on('routeChangeStart', () => NProgress.start())
 Router.events.on('routeChangeComplete', () => NProgress.done())
 Router.events.on('routeChangeError', () => NProgress.done())
+
+const ProdexToast = (toast) => {
+  const { appearance, autoDismiss, children, isRunning, transitionState } = toast
+  let icon = null
+  switch (appearance) {
+    case 'error':
+      icon = (<AlertTriangle />)
+      break
+    case 'success':
+      icon = (<CheckCircle />)
+      break
+    case 'info':
+    case 'warning':
+      icon = (<AlertCircle />)
+      break
+  }
+
+  return (
+    <div className={`prodex-toast ${appearance} ${transitionState} ${isRunning ? 'running' : 'stopped'}`} onMouseEnter={() => toast.onMouseEnter()} onMouseLeave={() => toast.onMouseLeave()}>
+      <div role='button' onClick={() => toast.onDismiss()}>
+        <X />
+      </div>
+      {autoDismiss ? (
+        <div className='dismiss-bar'></div>
+      ) : null}
+      {icon}
+      <div className='toast-content'>
+        {children}
+      </div>
+    </div>
+  )
+}
 
 class ProdexApp extends App {
   componentDidMount() {
@@ -38,7 +71,7 @@ class ProdexApp extends App {
     return (
       <Container>
         <IntlProvider locale='en' messages={EN} textComponent={({ children }) => <>{children}</>}>
-          <ToastProvider pauseOnHover autoDismiss autoDismissTimeout={10 * 1000}>
+          <ToastProvider pauseOnHover autoDismiss autoDismissTimeout={10 * 1000} components={{ Toast: ProdexToast }}>
             <Provider store={store}>
               <Component {...pageProps} />
             </Provider>
