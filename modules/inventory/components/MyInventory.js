19a0a99aeacdac6b6d7081fe16c09d9d72274d5d
Miroslav Veis <miroslav.veis@artio.cz>
2020-03-11 14:58:33 +0100

    tutorial based on BE property, adjusted request body /api/users/me according new swagger, remove cookies after finished tutorial, new logic for tabChange in settings

--- modules/inventory/components/MyInventory.js
+++ modules/inventory/components/MyInventory.js
@@ -17,6 +17,7 @@ import moment from 'moment/moment'
 import { getSafe } from '~/utils/functions'
 import { Datagrid } from '~/modules/datagrid'
 import styled from 'styled-components'
+import Tutorial from '~/modules/tutorial/Tutorial'
 
 const defaultHiddenColumns = [
   'minOrderQuantity',
@@ -514,7 +515,8 @@ class MyInventory extends Component {
       sidebarValues,
       openPopup,
       editedId,
-      closeSidebarDetail
+      closeSidebarDetail,
+      tutorialCompleted
     } = this.props
     const { columns, selectedRows, clientMessage, request } = this.state
 
@@ -549,7 +551,7 @@ class MyInventory extends Component {
           </Modal.Actions>
         </Modal>
         {isOpenImportPopup && <ProductImportPopup productOffer={true} />}
-
+        {!tutorialCompleted && <Tutorial />}
         <Container fluid style={{ padding: '0 32px' }}>
           <Menu secondary className='page-part'>
             {/*selectedRows.length > 0 ? (
