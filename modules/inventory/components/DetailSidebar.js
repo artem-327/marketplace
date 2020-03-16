4f69c8ef81b2098484ef586a2870928eb3ea971c
David Gomola <dave.gomola13@gmail.com>
2020-03-16 14:59:35 +0100

    sidebar dropdown keys fix

--- modules/inventory/components/DetailSidebar.js
+++ modules/inventory/components/DetailSidebar.js
@@ -1179,8 +1179,8 @@ class DetailSidebar extends Component {
                                     <GridColumn mobile={rightWidth} computer={rightWidth}>
                                       <Dropdown
                                         name='edit.product'
-                                        options={this.props.autocompleteData.map(el => ({
-                                          key: el.id,
+                                        options={this.props.autocompleteData.map((el, i) => ({
+                                          key: i,
                                           text: `${getSafe(() => el.intProductCode, '')} ${getSafe(
                                             () => el.intProductName,
                                             ''
