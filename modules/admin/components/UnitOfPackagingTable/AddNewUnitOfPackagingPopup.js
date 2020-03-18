eee09b580a1cf11e6aeeb0ba1812102631f3addf
Miroslav Veis <miroslav.veis@artio.cz>
2020-03-18 16:42:55 +0100

    added new fields to unit of packaging form

--- modules/admin/components/UnitOfPackagingTable/AddNewUnitOfPackagingPopup.js
+++ modules/admin/components/UnitOfPackagingTable/AddNewUnitOfPackagingPopup.js
@@ -10,15 +10,20 @@ import { FormattedMessage } from 'react-intl'
 
 const initialFormValues = {
   val0: '',
-  val1: ''
+  val1: '',
+  val2: '',
+  val3: '',
+  val4: ''
 }
 
 const formValidation = Yup.object().shape({
   val0: Yup.string()
     .trim()
-    .min(1, 'Too short')
-    .required('Required'),
-  val1: Yup.number().required('Required')
+    .min(1, 'Too short'),
+  val1: Yup.number().required('Required'),
+  val2: Yup.number().required('Required'),
+  val3: Yup.number().required('Required'),
+  val4: Yup.number().required('Required')
 })
 
 class AddNewUnitOfPackagingPopup extends React.Component {
@@ -38,13 +43,21 @@ class AddNewUnitOfPackagingPopup extends React.Component {
             onSubmit={async (values, { setSubmitting }) => {
               let data = {
                 [config.edit[0].name]: values.val0.trim(),
-                [config.edit[1].name]: values.val1
+                [config.edit[1].name]: values.val1,
+                [config.edit[2].name]: parseFloat(values.val2),
+                [config.edit[3].name]: parseFloat(values.val3),
+                [config.edit[4].name]: parseFloat(values.val4)
               }
-              await postNewRequest(config, data)
-              setSubmitting(false)
+              try {
+                await postNewRequest(config, data)
+              } catch (error) {
+                console.error(error)
+              } finally {
+                setSubmitting(false)
+              }
             }}>
             <FormGroup widths='equal' data-test='admin_add_unit_packaging_name_inp'>
-              <Input type={config.edit[0].type} label={config.edit[0].title} name='val0' />
+              <Input inputProps={{ type: config.edit[0].type }} label={config.edit[0].title} name='val0' />
             </FormGroup>
             <FormGroup widths='equal'>
               <Dropdown
@@ -52,6 +65,30 @@ class AddNewUnitOfPackagingPopup extends React.Component {
                 options={measureOptions}
                 name='val1'
                 inputProps={{ 'data-test': 'admin_add_unit_packaging_type_drpdn' }}
+              />
+            </FormGroup>
+            <FormGroup widths='equal' data-test='admin_add_unit_packaging_height_inp'>
+              <Input
+                inputProps={{ type: config.edit[2].type, step: config.edit[2].step }}
+                label={config.edit[2].title}
+                name='val2'
+                step={config.edit[2].step}
+              />
+            </FormGroup>
+            <FormGroup widths='equal' data-test='admin_add_unit_packaging_length_inp'>
+              <Input
+                inputProps={{ type: config.edit[3].type, step: config.edit[3].step }}
+                label={config.edit[3].title}
+                name='val3'
+                step={config.edit[3].step}
+              />
+            </FormGroup>
+            <FormGroup widths='equal' data-test='admin_add_unit_packaging_width_inp'>
+              <Input
+                inputProps={{ type: config.edit[4].type, step: config.edit[4].step }}
+                label={config.edit[4].title}
+                name='val4'
+                step={config.edit[4].step}
               />
             </FormGroup>
             <div style={{ textAlign: 'right' }}>
