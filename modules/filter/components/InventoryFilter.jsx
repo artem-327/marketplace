71e6e364982f215f05d85315e5412bd41aa642d2
Petr Staszko <petr.staszko@artio.cz>
2020-03-20 00:54:51 +0100

    #33172 Filters open by default, Chemical/CAS accordion open, rest collapsed.

--- modules/filter/components/InventoryFilter.jsx
+++ modules/filter/components/InventoryFilter.jsx
@@ -93,7 +93,7 @@ class InventoryFilter extends Component {
   state = {
     savedFiltersActive: false,
     openedSaveFilter: false,
-    inactiveAccordion: {},
+    activeAccordion: { chemicalType: true },
     dateDropdown: {
       expiration: dateDropdownOptions[0].value,
       mfg: dateDropdownOptions[0].value
@@ -414,9 +414,9 @@ class InventoryFilter extends Component {
   }
 
   toggleAccordion = name => {
-    let { inactiveAccordion } = this.state
-    let inactive = inactiveAccordion[name]
-    this.setState({ inactiveAccordion: { ...this.state.inactiveAccordion, [name]: !inactive } })
+    let { activeAccordion } = this.state
+    let active = activeAccordion[name]
+    this.setState({ activeAccordion: { ...this.state.activeAccordion, [name]: !active } })
   }
 
   handleSearch = debounce(({ searchQuery }) => {
@@ -469,7 +469,7 @@ class InventoryFilter extends Component {
     <AccordionTitle name={name} onClick={(e, { name }) => this.toggleAccordion(name)}>
       {text}
       <IconRight>
-        <Icon name={!this.state.inactiveAccordion[name] ? 'chevron down' : 'chevron right'} />
+        <Icon name={this.state.activeAccordion[name] ? 'chevron down' : 'chevron right'} />
       </IconRight>
     </AccordionTitle>
   )
@@ -858,14 +858,14 @@ class InventoryFilter extends Component {
       <FilterAccordion>
         <AccordionItem>
           {this.accordionTitle('chemicalType', <FormattedMessage id='filter.chemicalProductName' />)}
-          <AccordionContent active={!this.state.inactiveAccordion.chemicalType}>
+          <AccordionContent active={this.state.activeAccordion.chemicalType}>
             <BottomMargedDropdown {...dropdownProps} />
           </AccordionContent>
         </AccordionItem>
 
         <AccordionItem>
           {this.accordionTitle('warehouse', <FormattedMessage id='filter.warehouse' />)}
-          <AccordionContent active={!this.state.inactiveAccordion.warehouse}>
+          <AccordionContent active={this.state.activeAccordion.warehouse}>
             <BottomMargedDropdown {...dropdownWarehouseProps} />
           </AccordionContent>
         </AccordionItem>
@@ -875,7 +875,7 @@ class InventoryFilter extends Component {
             'expiration',
             <FormattedMessage id='filter.expiration' defaultMessage='Days Until Expiration' />
           )}
-          <AccordionContent active={!this.state.inactiveAccordion.expiration}>
+          <AccordionContent active={this.state.activeAccordion.expiration}>
             <FormGroup widths='equal'>
               {this.dateField('expiration', { values, setFieldValue, handleChange, min: 1 })}
             </FormGroup>
@@ -887,7 +887,7 @@ class InventoryFilter extends Component {
             'mfg',
             <FormattedMessage id='filter.mfg' defaultMessage='Days Since Manufacture Date' />
           )}
-          <AccordionContent active={!this.state.inactiveAccordion.mfg}>
+          <AccordionContent active={this.state.activeAccordion.mfg}>
             <FormGroup widths='equal'>
               {this.dateField('mfg', { values, setFieldValue, handleChange, min: 0 })}
             </FormGroup>
@@ -896,7 +896,7 @@ class InventoryFilter extends Component {
 
         <AccordionItem>
           {this.accordionTitle('quantity', <FormattedMessage id='filter.quantity' />)}
-          <AccordionContent active={!this.state.inactiveAccordion.quantity}>
+          <AccordionContent active={this.state.activeAccordion.quantity}>
             <FormGroup widths='equal' data-test='filter_quantity_inp'>
               <FormField width={8}>
                 {this.quantityWrapper('quantityFrom', {
@@ -920,7 +920,7 @@ class InventoryFilter extends Component {
 
         <AccordionItem>
           {this.accordionTitle('price', <FormattedMessage id='filter.price' />)}
-          <AccordionContent active={!this.state.inactiveAccordion.price}>
+          <AccordionContent active={this.state.activeAccordion.price}>
             <FormGroup>
               <FormField className='price-input' width={8} data-test='filter_price_inp'>
                 {this.inputWrapper(
@@ -955,7 +955,7 @@ class InventoryFilter extends Component {
         {false && (
           <AccordionItem>
             {this.accordionTitle('broadcasted', <FormattedMessage id='filter.broadcast' />)}
-            <AccordionContent active={!this.state.inactiveAccordion.broadcasted}>
+            <AccordionContent active={this.state.activeAccordion.broadcasted}>
               <BottomMargedDropdown {...dropdownBroadcastedProps} />
             </AccordionContent>
           </AccordionItem>
@@ -963,27 +963,27 @@ class InventoryFilter extends Component {
 
         <AccordionItem>
           {this.accordionTitle('packaging', <FormattedMessage id='filter.packaging' />)}
-          <AccordionContent active={!this.state.inactiveAccordion.packaging}>{packagingTypesRows}</AccordionContent>
+          <AccordionContent active={this.state.activeAccordion.packaging}>{packagingTypesRows}</AccordionContent>
         </AccordionItem>
 
         <AccordionItem>
           {this.accordionTitle('productGrades', <FormattedMessage id='filter.grade' defaultMessage='Grade' />)}
-          <AccordionContent active={!this.state.inactiveAccordion.productGrades}>{productGradeRows}</AccordionContent>
+          <AccordionContent active={this.state.activeAccordion.productGrades}>{productGradeRows}</AccordionContent>
         </AccordionItem>
 
         <AccordionItem>
           {this.accordionTitle('condition', <FormattedMessage id='filter.condition' defaultMessage='Condition' />)}
-          <AccordionContent active={!this.state.inactiveAccordion.condition}>{productConditionRows}</AccordionContent>
+          <AccordionContent active={this.state.activeAccordion.condition}>{productConditionRows}</AccordionContent>
         </AccordionItem>
 
         <AccordionItem>
           {this.accordionTitle('productForms', <FormattedMessage id='filter.form' defaultMessage='Form' />)}
-          <AccordionContent active={!this.state.inactiveAccordion.productForms}>{productFormsRows}</AccordionContent>
+          <AccordionContent active={this.state.activeAccordion.productForms}>{productFormsRows}</AccordionContent>
         </AccordionItem>
 
         <AccordionItem>
           {this.accordionTitle('assay', <FormattedMessage id='filter.percentage' />)}
-          <AccordionContent active={!this.state.inactiveAccordion.assay}>
+          <AccordionContent active={this.state.activeAccordion.assay}>
             <FormGroup data-test='filter_assay_inp'>
               <FormField width={8}>
                 {this.inputWrapper(
@@ -1016,7 +1016,7 @@ class InventoryFilter extends Component {
         {false && (
           <AccordionItem>
             {this.accordionTitle('manufacturer', <FormattedMessage id='filter.manufacturer' />)}
-            <AccordionContent active={!this.state.inactiveAccordion.manufacturer}>
+            <AccordionContent active={this.state.activeAccordion.manufacturer}>
               <BottomMargedDropdown {...dropdownManufacturerProps} />
             </AccordionContent>
           </AccordionItem>
@@ -1025,7 +1025,7 @@ class InventoryFilter extends Component {
         {false && (
           <AccordionItem>
             {this.accordionTitle('origin', <FormattedMessage id='filter.origin' />)}
-            <AccordionContent active={!this.state.inactiveAccordion.origin}>
+            <AccordionContent active={this.state.activeAccordion.origin}>
               <BottomMargedDropdown {...dropdownOriginProps} />
             </AccordionContent>
           </AccordionItem>
