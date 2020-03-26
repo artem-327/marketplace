19a4a2dabdce1e7ebe6013c2dcaffd78c37e8dfc
Miroslav Veis <miroslav.veis@artio.cz>
2020-03-26 20:57:44 +0100

    fixed request body, fixed api call, added new fields, added new action

--- modules/company-form/reducer.js
+++ modules/company-form/reducer.js
@@ -3,7 +3,8 @@ import * as AT from './action-types'
 export const initialState = {
   data: [],
   companyLogo: null,
-  loading: false
+  loading: false,
+  associations: []
 }
 
 export default function reducer(state = initialState, action) {
@@ -37,6 +38,29 @@ export default function reducer(state = initialState, action) {
       return {
         ...state,
         companyLogo: payload.data.size ? payload.data : null
+      }
+    }
+
+    case AT.GET_ASSOCIATIONS_PENDING: {
+      return {
+        ...state,
+        loading: true
+      }
+    }
+
+    case AT.GET_ASSOCIATIONS_REJECTED: {
+      return {
+        ...state,
+        associations: state.associations,
+        loading: false
+      }
+    }
+
+    case AT.GET_ASSOCIATIONS_FULFILLED: {
+      return {
+        ...state,
+        loading: false,
+        associations: payload
       }
     }
 
