c03e1cc79dc20d91a7c304e2b3249a41a1aa973c
David Gomola <dave.gomola13@gmail.com>
2020-04-16 12:38:53 +0200

    Checkout page redesign

--- modules/cart/components/StyledComponents.js
+++ modules/cart/components/StyledComponents.js
@@ -2,8 +2,48 @@ import styled from 'styled-components'
 
 import { Header, GridColumn, GridRow, Grid, Segment } from 'semantic-ui-react'
 
+
+export const GridContainer = styled(Grid)`
+  .row:not(.default-padded) {
+    padding-left: 30px;
+    padding-right: 30px;
+  }
+  > .column > .segment > .grid > .row > .column:first-child:not(.default) {
+    padding-left: 0px !important;
+  }
+  > .column > .segment > .grid > .row > .column:last-child:not(.default) {
+    padding-right: 0px !important;
+  }
+`
+
+export const SummaryGrid = styled(Grid)`
+  .column:first-child:not(.default) {
+    padding-left: 0px !important;
+  }
+  .column:first-child:last-child:not(.default) {
+    padding-right: 0px !important;
+  }
+  .row .column:nth-child(2):not(.default) {
+    padding-right: 0px !important;
+  }
+  .row > .column:last-child:not(:first-child) {
+    text-align: right;
+  }
+  .grid { 
+    padding-top: 0px !important;
+  }
+
+`
+
 export const CapitalizedText = styled.span`
   text-transform: capitalize;
+`
+export const TopUnpaddedRow = styled(GridRow)`
+  padding-top: 0px !important;
+`
+
+export const BottomUnpaddedRow = styled(GridRow)`
+  padding-bottom: 0px !important;
 `
 
 export const CustomHeader = styled(Header)`
@@ -29,16 +69,19 @@ export const VerticalUnpaddedColumn = styled(GridColumn)`
   padding-bottom: 0px !important;
 `
 
+export const VerticalUnpaddedRow = styled(GridRow)`
+  padding-top: 0px !important;
+  padding-bottom: 0px !important;
+`
+
 export const StyledRow = styled(GridRow)`
   box-shadow: ${props => props.bottomShadow ? '0 1px 0 0 #dee2e6' : '0 0 0 0'};
+  margin-top: ${props => props.topMarged ? '5px' : '0px'} !important;
+  ${props => props.verticallyUnpadded ? 'padding-bottom: 0px !important; padding-top: 0px !important;' : ''};
 `
 
-export const TopUnpaddedRow = styled(GridRow)`
-  padding-top: 0px !important;
-`
-
-export const BottomUnpaddedRow = styled(GridRow)`
-  padding-bottom; 0px !important;
+export const BottomUnpaddedColumn = styled(GridColumn)`
+  padding-bottom: 0px !important;
 `
 
 export const ItemDescriptionGrid = styled(Grid)`
@@ -55,7 +98,7 @@ export const ItemDescriptionGrid = styled(Grid)`
 `
 
 export const Item = styled.div`
-  box-shadow: ${props => props.bottomShadow ? '0 1px 0 0 #dee2e6' : '0 0 0 0'};
+  box-shadow: ${props => props.bottomShadow ? '0 1px 0 0 #dee2e6 !important' : '0 0 0 0'};
   padding-bottom: 30px;
 `
 
@@ -67,4 +110,26 @@ export const DescriptionValue = styled.span`
 export const TotalRow = styled(GridRow)`
   background-color: #f8f9fb;
   border-top: 1px solid #dee2e6;
+`
+
+export const TotalPriceRow = styled(GridRow)`
+  > .column:last-child {
+    color: #84c225 !important;
+  }  
+  background-color: #f8f9fb;
+  border-top: 1px solid #dee2e6;
+  
+ padding-right: 30px !important;
+`
+
+export const TopUnpaddedColumn = styled(GridColumn)`
+  padding-top: 0px !important;
+`
+
+export const UnpaddedColumn = styled(GridColumn)`
+  padding: 0 !important;
+`
+
+export const RightUnpaddedRow = styled(GridRow)`
+  padding-right: 0px !important;
 `
