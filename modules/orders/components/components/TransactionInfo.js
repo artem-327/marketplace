771670afb6a00461aea1090231d3db37e99b8d50
Marek Juroško <marek.jurosko@artio.cz>
2020-04-17 18:54:38 +0200

    Task #33564 - TransactionInfo module redesign

--- modules/orders/components/components/TransactionInfo.js
+++ modules/orders/components/components/TransactionInfo.js
@@ -6,7 +6,12 @@ import { FormattedMessage, injectIntl } from 'react-intl'
 import { getSafe } from '~/utils/functions'
 
 const SmallerTextColumn = styled(GridColumn)`
-  font-size: 12px !important;
+  .page-wrapper .segment .grid > .row > & {
+    padding: 5px 25px !important;
+    vertical-align: baseline;
+    font-size: 12px !important;
+    line-height: 20px !important;
+  }
 `
 
 const RightSpan = styled.span`
@@ -19,8 +24,30 @@ const RightSpan = styled.span`
 class TransactionInfo extends Component {
   render() {
     const StyledSegment = styled(OrderSegment)`
-      padding-top: 0px !important;
-      padding-bottom: 0px !important;
+      margin: 14px 32px !important;
+      padding: 0px !important;
+      
+      [class*="OrderSegment"] + & {
+        margin-top: -20px !important;
+      }
+      
+      + div.ui.hidden.divider {
+        margin: 14px 0 0 !important;
+        border: 0 none !important;
+      }
+      
+      > .grid {
+        margin: 0 !important;
+        padding: 10px 0 !important;
+        
+        > .row {
+          padding: 0 !important;
+          
+          + .row {
+            margin-top: 10px !important;
+          }
+        }
+      }
     `
 
     const { order, echoSupportPhone } = this.props
@@ -38,14 +65,14 @@ class TransactionInfo extends Component {
                   : getSafe(() => order.buyerCompanyName, '')}
               </RightSpan>
             </SmallerTextColumn>
-            <GridColumn>
+            <SmallerTextColumn>
               <FormattedMessage id='order.detail.echoSystemTransaction' defaultMessage='Echo System Transaction' />
               <RightSpan>{echoSystemTransaction}</RightSpan>
-            </GridColumn>
-            <GridColumn>
+            </SmallerTextColumn>
+            <SmallerTextColumn>
               <FormattedMessage id='order.detail.echoSupportPhone' defaultMessage='Echo Support phone number' />
               <RightSpan>{echoSupportPhone}</RightSpan>
-            </GridColumn>
+            </SmallerTextColumn>
           </GridRow>
         </Grid>
       </StyledSegment>
