c686c994c39cedcaecba1a0a02b7f09818378174
Petr Staszko <petr.staszko@artio.cz>
2020-05-04 08:08:16 +0200

    Resolve T-33754 "Expandableorderitemsonorderlist"

--- modules/orders/components/OrdersContainer.js
+++ modules/orders/components/OrdersContainer.js
@@ -16,6 +16,23 @@ import { downloadAttachment, downloadAttachmentPdf } from '~/modules/inventory/a
 import { getLocaleDateFormat } from '~/components/date-format'
 import { getSafe } from '~/utils/functions'
 
+const filterAttachments = (a, type) => {
+  if (!a) return []
+  let filtered = a.reduce((latest, a) => {
+    if (a.documentType && a.documentType.id === type) {
+      if (latest) {
+        if (latest.id < a.id) {
+          latest = a
+        }
+      } else {
+        latest = a
+      }
+    }
+    return latest
+  }, null)
+  return filtered ? [filtered] : []
+}
+
 function mapStateToProps(state, { router, datagrid }) {
   const { orders } = state
   const query = router ? router.query : { type: 'sales' }
@@ -33,44 +50,38 @@ function mapStateToProps(state, { router, datagrid }) {
     filterData: state.forms.filter,
     rows: datagrid.rows.map(r => ({
       id: r.id,
+      rawData: r,
+      clsName: 'tree-table root-row',
+      root: true,
+      treeRoot: true,
       globalStatus: r.cfGlobalStatus,
       date: r.orderDate && moment(r.orderDate).format(getLocaleDateFormat()),
       customerName: type === 'sales' ? r.buyerCompanyName : r.sellerCompanyName,
-      orderItems: r.orderItems,
       orderStatus: OrdersHelper.getOrderStatus(r.orderStatus),
       shippingStatus: OrdersHelper.getShippingStatus(r.shippingStatus),
       reviewStatus: OrdersHelper.getReviewStatus(r.reviewStatus),
       creditStatus: OrdersHelper.getCreditStatus(r.creditReviewStatus),
       paymentStatus: OrdersHelper.getPaymentStatus(r.paymentStatus),
-      bl:
-        r.attachments && r.attachments.length
-          ? r.attachments.reduce((cofAList, a) => {
-              if (a && a.documentType && a.documentType.id === 10) {
-                cofAList.push(a)
-              }
-              return cofAList
-            }, [])
-          : null,
-      sds:
-        r.attachments && r.attachments.length
-          ? r.attachments.reduce((cofAList, a) => {
-              if (a && a.documentType && a.documentType.id === 3) {
-                cofAList.push(a)
-              }
-              return cofAList
-            }, [])
-          : null,
-      cofA:
-        r.attachments && r.attachments.length
-          ? r.attachments.reduce((cofAList, a) => {
-              if (a && a.documentType && a.documentType.id === 1) {
-                cofAList.push(a)
-              }
-              return cofAList
-            }, [])
-          : null,
+      bl: '',
+      sds: '',
+      cofA: '',
       orderTotal: <FormattedNumber style='currency' currency={currency} value={r.cfPriceTotal} />,
-      accountingDocumentsCount: r.accountingDocumentsCount
+      accountingDocumentsCount: r.accountingDocumentsCount,
+      attachments: r.attachments,
+      orderItems: r.orderItems.map(item => {
+        let cofA = filterAttachments(item.attachments, 1)  // C of A
+        let sds = filterAttachments(item.attachments, 3)  // SDS
+        let bl = filterAttachments(item.attachments, 10)  // B/L
+        return ({
+          ...item,
+          rawData: item,
+          id: r.id + '_' + item.id,
+          clsName: 'tree-table nested-row',
+          cofA,
+          sds,
+          bl
+        })
+      }),
     })),
     activeStatus: orders.statusFilter,
     listDocumentTypes: orders.listDocumentTypes,
