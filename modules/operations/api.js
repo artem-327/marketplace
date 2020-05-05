15016389e24aeec2db196f44d6e23255f3f44841
Petr Staszko <petr.staszko@artio.cz>
2020-05-06 00:16:25 +0200

    #33595 Added Accounting documents popup to Operations / Order page.

--- modules/operations/api.js
+++ modules/operations/api.js
@@ -14,5 +14,6 @@ export default {
     api.get(`/prodex/api/companies/search?limit=${limit}&pattern=${companyText}`)
       .then(response => response.data),
   getDocumentTypes: () => api.get(`/prodex/api/document-types/`),
-  cancelOrder: orderId => api.patch(`/prodex/api/purchase-orders/${orderId}/cancel`)
+  cancelOrder: orderId => api.patch(`/prodex/api/purchase-orders/${orderId}/cancel`),
+  getAccountingDocuments: orderId => api.get(`/prodex/api/accounting-documents/order/${orderId}`)
 }
