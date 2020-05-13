2024287cc3c31bcd7a8b07b960088e47caaf8301
Miroslav Veis <miroslav.veis@artio.cz>
2020-05-13 13:38:27 +0200

    added alerts page

---
+++ pages/alerts/index.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import Layout from 'components/Layout'
+import securePage from '~/hocs/securePage'
+import Alerts from '~/modules/alerts'
+
+import { injectIntl } from 'react-intl'
+
+class Index extends Component {
+  render() {
+    const {
+      intl: { formatMessage }
+    } = this.props
+    return (
+      <Layout
+        title={formatMessage({
+          id: 'alerts.title',
+          defaultMessage: 'Notifications'
+        })}>
+        <Alerts />
+      </Layout>
+    )
+  }
+}
+
+export default securePage(injectIntl(Index))
