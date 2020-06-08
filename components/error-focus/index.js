ff546dd9177e3774d9ce3ce4c41a46387d57ff71
Miroslav Veis <miroslav.veis@artio.cz>
2020-06-08 15:49:02 +0200

    get deep keys from objects errors and values, focus on error field in company client form

--- components/error-focus/index.js
+++ components/error-focus/index.js
@@ -1,23 +1,23 @@
 import { Component } from 'react'
 import { connect } from 'formik'
+import deepKeys from 'deep-keys'
 
 class ErrorFocus extends Component {
   componentDidUpdate(prevProps) {
     const { isSubmitting, isValidating, errors, initialValues, values } = prevProps.formik
+    if (!Object.keys(errors).length || !Object.keys(values).length) return
 
-    const keyify = (obj, prefix = '') =>
-      Object.keys(obj).reduce((res, el) => {
-        if (Array.isArray(obj[el])) {
-          return res
-        } else if (typeof obj[el] === 'object' && obj[el] !== null) {
-          return [...res, ...keyify(obj[el], prefix + el + '.')]
-        } else {
-          return [...res, prefix + el]
-        }
-      }, [])
-    let errorsObj = {}
-    Object.keys(errors).forEach(err => (values[err] !== null ? (errorsObj[err] = values[err]) : null))
-    const keys = keyify(errorsObj)
+    const compareKeys = (errorKeys, valuesKeys) => {
+      let arrayKeys = []
+      valuesKeys.forEach(val => errorKeys.forEach(err => (err === val ? arrayKeys.push(val) : null)))
+      return arrayKeys
+    }
+
+    const errKeys = deepKeys(errors, true)
+    const valKeys = deepKeys(values)
+
+    const keys = compareKeys(errKeys, valKeys)
+
     if (keys.length > 0 && isSubmitting && !isValidating) {
       const selector = `[name="${keys[0]}"]`
       const errorElement = document.querySelector(selector)
