fe50ae949a51c6c9055bba673b5e64b7fbb8e7d1
Petr Staszko <petr.staszko@artio.cz>
2020-07-14 01:18:07 +0200

    #33821 Fixed Search by Company suggest input in Export Inventory sidebar.

--- modules/export-inventory/api.js
+++ modules/export-inventory/api.js
@@ -4,6 +4,6 @@ import { generateQueryString } from '~/utils/functions'
 export default {
   searchCompany: (companyText, limit = 30) =>
     api
-      .get(`/prodex/api/companies/search?limit=${limit}&pattern=${encodeURIComponent(companyText)}`)
+      .get(`/prodex/api/companies/client/search?limit=${limit}&pattern=${encodeURIComponent(companyText)}`)
       .then(response => response.data)
 }
