07d1b04bf7cea2ddd724d6906faf211ada6b5f17
Miroslav Veis <miroslav.veis@artio.cz>
2020-08-14 16:20:28 +0200

    new logic for mask date

--- components/custom-formik/DateInput.js
+++ components/custom-formik/DateInput.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import { Form } from 'semantic-ui-react'
 import { FastField, Field, getIn } from 'formik'
-import InputMask from 'react-input-mask'
 
 import { getFieldError, setFieldValue } from './helpers'
 
@@ -13,11 +12,62 @@ class FormikInput extends Component {
     super(props)
     const { id, name } = props
     this.id = id || `field_input_${name}`
+    this.state = {
+      value: null
+    }
   }
 
   handleRef = r => {
     r && r.inputNode.setAttribute('autocomplete', 'off')
     // r && r.inputNode.setAttribute('readonly', true)
+  }
+
+  //FIXME
+  checkMaskDate = value => {
+    if (value.length > 10 || isNaN(value.charAt(value.length - 1))) return
+    console.log('value', value)
+    console.log('this.state.value', this.state.value)
+    const dateFormat = 'MM/DD/YYYY' // getLocaleDateFormat()
+    const dash = getLocaleDateFormat().includes('-')
+    const dot = getLocaleDateFormat().includes('.')
+
+    let separator = '/'
+    if (dash) separator = '-'
+    if (dot) separator = '.'
+
+    if (value.length) {
+      if (dateFormat.charAt(0) === 'M') {
+        let monthValue = value.substring(0, 2)
+        let dayValue = value.substring(3, 5)
+        console.log('dayValue', isNaN(parseInt(dayValue)))
+        if (monthValue.length && (parseInt(monthValue) > 12 || isNaN(parseInt(monthValue)))) {
+          monthValue = 12
+          value = value.splice(0, 2, monthValue)
+        }
+        if (dayValue.length && (parseInt(dayValue) > 31 || isNaN(parseInt(dayValue)))) {
+          dayValue = 31
+          value = value.splice(3, 2, dayValue)
+        }
+      }
+    }
+    if (dateFormat.charAt(0) === 'M' || dateFormat.charAt(0) === 'D') {
+      if (
+        value.length >= 2 &&
+        value.indexOf(separator) !== 2 &&
+        ((this.state.value && this.state.value.indexOf(separator) !== 2) || !this.state.value)
+      ) {
+        value = value.splice(2, 0, separator)
+      }
+      if (
+        (value.length >= 5 && this.state.value && this.state.value.length < 5) ||
+        (this.state.value && this.state.value.length < 5 && value.length > 5)
+      ) {
+        value = value.splice(5, 0, separator)
+      }
+    }
+
+    this.setState({ value })
+    return value
   }
 
   render() {
@@ -44,37 +94,33 @@ class FormikInput extends Component {
           return (
             <Form.Field error={!!error} {...fieldProps}>
               {!!label && <label htmlFor={this.id}>{label}</label>}
-              <InputMask
+
+              {/* <InputRef inputRef={inputRef}> */}
+              <DateInput
+                {...safeInputProps}
                 name={name}
-                mask={mask}
-                onChange={(e, data) => {
-                  if (!e || !e.target || !e.target.name) return
-                  const name = data && data.name ? name : e.target.name
-                  const value = data && data.value ? value : e.target.value
+                value={field.value}
+                clearable
+                onChange={(e, { name, value }) => {
+                  const newValue = this.checkMaskDate(value)
+                  console.log('newValue====================================')
+                  console.log(newValue)
+                  console.log('====================================')
                   setFieldValue(form, name, value, true)
                   Promise.resolve().then(() => {
                     onChange && onChange(e, { name, value })
                   })
                 }}
-                value={field.value}
+                placeholder={getLocaleDateFormat()}
+                data-test={`FormikInput_${this.id}_DateInput`}
+                closable
+                id={this.id}
                 onBlur={form.handleBlur}
-                {...safeInputProps}>
-                {/* <InputRef inputRef={inputRef}> */}
-                <DateInput
-                  {...safeInputProps}
-                  name={name}
-                  value={field.value}
-                  clearable
-                  placeholder={getLocaleDateFormat()}
-                  data-test={`FormikInput_${this.id}_DateInput`}
-                  closable
-                  id={this.id}
-                  dateFormat={getLocaleDateFormat()}
-                  animation='none'
-                  ref={this.handleRef}
-                  localization={typeof navigator !== 'undefined' ? window.navigator.language.slice(0, 2) : 'en'}
-                />
-              </InputMask>
+                dateFormat={getLocaleDateFormat()}
+                animation='none'
+                ref={this.handleRef}
+                localization={typeof navigator !== 'undefined' ? window.navigator.language.slice(0, 2) : 'en'}
+              />
               {error && <span className='sui-error-message'>{getIn(form.errors, name)}</span>}
               {/* </InputRef> */}
             </Form.Field>
