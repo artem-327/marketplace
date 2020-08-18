fd850fc1a83a068b67a495f29d8149b06ae76857
Petr Staszko <petr.staszko@artio.cz>
2020-08-18 05:34:30 +0200

    #34242 Error-focus component.

--- components/error-focus/index.js
+++ components/error-focus/index.js
@@ -12,54 +12,49 @@ class ErrorFocus extends Component {
 
     if (!Object.keys(errors).length || !Object.keys(values).length || !isSubmitting) return
 
-    //compare errors with values because errors returns atributes in object like _owner, _store, props, etc.
-    const compareKeys = (errorKeys, valuesKeys) => {
-      let arrayKeys = []
-      valuesKeys.forEach(val => errorKeys.forEach(err => (err === val ? arrayKeys.push(val) : null)))
-      return arrayKeys
+    let errNames = []
+
+    const getErrNames = (errors, path = '') => {
+      const errKeys = Object.keys(errors)
+      errKeys.forEach(key => {
+        const error = errors[key]
+        if (Array.isArray(error)) {
+          error.forEach((err, index) => {
+            if (err) getErrNames(err, `${path}${key}[${index}].`)
+          })
+        } else {
+          if (typeof error === 'object' && error !== null) {
+            const nestedKeys = Object.keys(error)
+            if (nestedKeys && nestedKeys.includes('$$typeof')) {
+              errNames.push(`${path}${key}`)
+            } else {
+              getErrNames(error, `${path}${key}.`)
+            }
+          }
+        }
+      })
     }
 
-    let errKeys = []
-    let valKeys = []
-    let keys = ''
-    //TODO errors or values can be array (e.g. elements in Company Generate Product form) not only object
-    try {
-      /**
-       * @description
-       * Get an object, and return an array composed of it's properties names(nested too).
-       * With intermediate equals to true, we include also the intermediate parent keys into the result
-       * @param obj {Object}
-       * @returns {Array}
-       * @example
-       * deepKeys({ a:1, b: { c:2, d: { e: 3 } } }) ==> ["a", "b.c", "b.d.e"]
-       * @example
-       * deepKeys({ b: { c:2, d: { e: 3 } } }, true) ==> ["b", "b.c", "b.d", "b.d.e"]
-       */
-      errKeys = await deepKeys(errors, true)
-      valKeys = await deepKeys(values)
-    } catch (err) {
-      console.error(err)
-    } finally {
-      keys = compareKeys(errKeys, valKeys)
-    }
+    getErrNames(errors)
 
-    if (keys.length > 0 && isSubmitting && !isValidating) {
-      const selector = `[name="${keys[0]}"]`
+    if (errNames.length > 0 && isSubmitting && !isValidating) {
+      const selector = `[name="${errNames[0]}"]`
       const errorElement = document.querySelector(selector)
+
+      toastManager.add(
+        generateToastMarkup(
+          <FormattedMessage id='global.errorValidation.title' defaultMessage='Errors on form' />,
+          <FormattedMessage
+            id='global.errorValidation.content'
+            defaultMessage='Fields on form are incomplete/non-validating, please fix the issues first'
+          />
+        ),
+        {
+          appearance: 'warning'
+        }
+      )
       if (errorElement) {
         errorElement.focus()
-        toastManager.add(
-          generateToastMarkup(
-            <FormattedMessage id='global.errorValidation.title' defaultMessage='Errors on form' />,
-            <FormattedMessage
-              id='global.errorValidation.content'
-              defaultMessage='Fields on form are incomplete/non-validating, please fix the issues first'
-            />
-          ),
-          {
-            appearance: 'warning'
-          }
-        )
       }
     }
   }
