bbe7401bc48e09cce4f46b6eea23ed5a5024c7ee
Petr Staszko <petr.staszko@artio.cz>
2020-08-24 23:32:49 +0200

    #34644 Build fixes.

--- pages/inventory/edit.js
+++
@@ -1,9 +0,0 @@
-import securePage from '../../hocs/securePage'
-import Layout from 'components/Layout'
-import { AddInventory } from '~/modules/inventory'
-
-export default securePage(() => (
-  <Layout title='Edit Inventory'>
-    <AddInventory type='edit' />
-  </Layout>
-))
