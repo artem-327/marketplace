b255b59840ecc59d0c8d0bb5f4d402e8d5231bfa
Miroslav Veis <miroslav.veis@artio.cz>
2020-09-03 10:42:52 +0200

    added palletMinPkgs to minPkg input, fixed validation

--- modules/inventory/components/DetailSidebar.js
+++ modules/inventory/components/DetailSidebar.js
@@ -262,104 +262,109 @@ val.addMethod(val.object, 'uniqueProperty', function (propertyName, message) {
   })
 })
 
-const validationScheme = val.object().shape({
-  edit: val.object().shape({
-    product: val.number().typeError(errorMessages.requiredMessage).required(errorMessages.requiredMessage),
-    fobPrice: val
-      .number()
-      .min(0.001, errorMessages.minimum(0.001))
-      .typeError(errorMessages.mustBeNumber)
-      .test('maxdec', errorMessages.maxDecimals(3), val => {
-        return !val || val.toString().indexOf('.') === -1 || val.toString().split('.')[1].length <= 3
-      })
-      .required(errorMessages.requiredMessage),
-    costPerUOM: val
-      .string()
-      .test('v', errorMessages.mustBeNumber, function (v) {
-        return v === null || v === '' || !isNaN(v)
-      })
-      .test('v', errorMessages.minimum(0), function (v) {
-        return v === null || v === '' || isNaN(v) || Number(v) >= 0
+const validationScheme = val.lazy(values => {
+  return val.object().shape({
+    edit: val.object().shape({
+      product: val.number().typeError(errorMessages.requiredMessage).required(errorMessages.requiredMessage),
+      fobPrice: val
+        .number()
+        .min(0.001, errorMessages.minimum(0.001))
+        .typeError(errorMessages.mustBeNumber)
+        .test('maxdec', errorMessages.maxDecimals(3), val => {
+          return !val || val.toString().indexOf('.') === -1 || val.toString().split('.')[1].length <= 3
+        })
+        .required(errorMessages.requiredMessage),
+      costPerUOM: val
+        .string()
+        .test('v', errorMessages.mustBeNumber, function (v) {
+          return v === null || v === '' || !isNaN(v)
+        })
+        .test('v', errorMessages.minimum(0), function (v) {
+          return v === null || v === '' || isNaN(v) || Number(v) >= 0
+        }),
+      lotNumber: val.string().typeError(errorMessages.invalidString).nullable(),
+      lotManufacturedDate: val.lazy(_value => dateBefore()),
+      inStock: val.bool().required(errorMessages.requiredMessage),
+      minimum: val
+        .number()
+        .min(1, errorMessages.minimum(1))
+        .typeError(errorMessages.mustBeNumber)
+        .divisibleBy(
+          val.ref('splits'),
+          <FormattedMessage id='inventory.notDivisibleBySplits' defaultMessage='Value is not divisible by Splits' />
+        )
+        .required(errorMessages.requiredMessage),
+      pkgAvailable: val
+        .number()
+        .positive(errorMessages.positive)
+        .typeError(errorMessages.mustBeNumber)
+        .required(errorMessages.requiredMessage)
+        .test('match', errorMessages.greaterOrEqual(values.edit.minimum), function (pkgAvailable) {
+          return typeof values.edit.minimum === 'undefined' || pkgAvailable >= values.edit.minimum
+        }),
+      leadTime: val.number().min(1, errorMessages.minimum(1)).typeError(errorMessages.mustBeNumber),
+      splits: val
+        .number()
+        .min(1, errorMessages.minimum(1))
+        .typeError(errorMessages.mustBeNumber)
+        .required(errorMessages.requiredMessage),
+      warehouse: val
+        .number(errorMessages.requiredMessage)
+        .nullable(errorMessages.requiredMessage)
+        .moreThan(0, errorMessages.requiredMessage)
+        .required(errorMessages.requiredMessage),
+      conforming: val.boolean(),
+      conditionNotes: val.string().when('conforming', {
+        is: false,
+        then: val.string().required(errorMessages.requiredNonConforming)
       }),
-    lotNumber: val.string().typeError(errorMessages.invalidString).nullable(),
-    lotManufacturedDate: val.lazy(_value => dateBefore()),
-    inStock: val.bool().required(errorMessages.requiredMessage),
-    minimum: val
-      .number()
-      .min(1, errorMessages.minimum(1))
-      .typeError(errorMessages.mustBeNumber)
-      .divisibleBy(
-        val.ref('splits'),
-        <FormattedMessage id='inventory.notDivisibleBySplits' defaultMessage='Value is not divisible by Splits' />
-      )
-      .required(errorMessages.requiredMessage),
-    pkgAvailable: val
-      .number()
-      .positive(errorMessages.positive)
-      .typeError(errorMessages.mustBeNumber)
-      .required(errorMessages.requiredMessage),
-    leadTime: val.number().min(1, errorMessages.minimum(1)).typeError(errorMessages.mustBeNumber),
-    splits: val
-      .number()
-      .min(1, errorMessages.minimum(1))
-      .typeError(errorMessages.mustBeNumber)
-      .required(errorMessages.requiredMessage),
-    warehouse: val
-      .number(errorMessages.requiredMessage)
-      .nullable(errorMessages.requiredMessage)
-      .moreThan(0, errorMessages.requiredMessage)
-      .required(errorMessages.requiredMessage),
-    conforming: val.boolean(),
-    conditionNotes: val.string().when('conforming', {
-      is: false,
-      then: val.string().required(errorMessages.requiredNonConforming)
-    }),
-    expirationDate: dateValidation(false).concat(
-      val.string().when('doesExpire', {
-        is: true,
-        then: val
+      expirationDate: dateValidation(false).concat(
+        val.string().when('doesExpire', {
+          is: true,
+          then: val
+            .string()
+            .test(
+              'min-date',
+              errorMessages.mustBeInFuture,
+              val => moment('00:00:00', 'hh:mm:ss').diff(getStringISODate(val), 'days') <= -1
+            )
+        })
+      ),
+      lotExpirationDate: dateValidation(false),
+      lotManufacturedDate: dateValidation(false).concat(
+        val
           .string()
           .test(
-            'min-date',
-            errorMessages.mustBeInFuture,
-            val => moment('00:00:00', 'hh:mm:ss').diff(getStringISODate(val), 'days') <= -1
+            'max-date',
+            errorMessages.mustBeInPast,
+            val => moment('00:00:00', 'hh:mm:ss').diff(getStringISODate(val), 'days') > -1
           )
-      })
-    ),
-    lotExpirationDate: dateValidation(false),
-    lotManufacturedDate: dateValidation(false).concat(
-      val
-        .string()
-        .test(
-          'max-date',
-          errorMessages.mustBeInPast,
-          val => moment('00:00:00', 'hh:mm:ss').diff(getStringISODate(val), 'days') > -1
-        )
-    )
-  }),
-  priceTiers: val.object().shape({
-    priceTiers: val.number(),
-    pricingTiers: val.array().of(
-      val
-        .object()
-        .uniqueProperty('quantityFrom', 'Quantity has to be unique')
-        .shape({
-          quantityFrom: val
-            .number()
-            .typeError(errorMessages.mustBeNumber)
-            .required(errorMessages.requiredMessage)
-            .moreThan(0, errorMessages.greaterThan(0)),
-          price: val
-            .number()
-            .typeError(errorMessages.mustBeNumber)
-            .required(errorMessages.requiredMessage)
-            .moreThan(0, errorMessages.greaterThan(0))
-            .test('maxdec', errorMessages.maxDecimals(3), val => {
-              return !val || val.toString().indexOf('.') === -1 || val.toString().split('.')[1].length <= 3
-            }),
-          manuallyModified: val.number().min(0).max(1)
-        })
-    )
+      )
+    }),
+    priceTiers: val.object().shape({
+      priceTiers: val.number(),
+      pricingTiers: val.array().of(
+        val
+          .object()
+          .uniqueProperty('quantityFrom', 'Quantity has to be unique')
+          .shape({
+            quantityFrom: val
+              .number()
+              .typeError(errorMessages.mustBeNumber)
+              .required(errorMessages.requiredMessage)
+              .moreThan(0, errorMessages.greaterThan(0)),
+            price: val
+              .number()
+              .typeError(errorMessages.mustBeNumber)
+              .required(errorMessages.requiredMessage)
+              .moreThan(0, errorMessages.greaterThan(0))
+              .test('maxdec', errorMessages.maxDecimals(3), val => {
+                return !val || val.toString().indexOf('.') === -1 || val.toString().split('.')[1].length <= 3
+              }),
+            manuallyModified: val.number().min(0).max(1)
+          })
+      )
+    })
   })
 })
 
@@ -940,6 +945,18 @@ class DetailSidebar extends Component {
       }
     }
   }
+
+  handleChangeProduct = (e, value, setFieldValue) => {
+    if (!value) return
+    const { autocompleteData } = this.props
+    if (getSafe(() => autocompleteData.length, false)) {
+      const companyProduct = autocompleteData.find(product => product.id === value)
+      if (getSafe(() => companyProduct.palletMinPkgs, false))
+        setFieldValue('edit.minimum', companyProduct.palletMinPkgs)
+    }
+    this.setState({ edited: true })
+  }
+
   onChange = debounce(() => this.setState({ edited: true }), 200)
 
   inputWrapper = (name, inputProps, label, labelText) => {
@@ -1227,7 +1244,8 @@ class DetailSidebar extends Component {
                                             search: options => options,
                                             selection: true,
                                             clearable: true,
-                                            onChange: this.onChange,
+                                            onChange: (e, { value }) =>
+                                              this.handleChangeProduct(e, value, setFieldValue),
                                             onSearchChange: (e, { searchQuery }) =>
                                               searchQuery.length > 0 && this.searchProducts(searchQuery)
                                           }}
