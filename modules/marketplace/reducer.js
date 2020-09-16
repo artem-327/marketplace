bbab6ff8f02598341cc81b7d5cbc3e80f6689ce2
Petr Staszko <petr.staszko@artio.cz>
2020-09-16 06:15:48 +0200

    #34824 Fixed Marketplace / Holds type select.

--- modules/marketplace/reducer.js
+++ modules/marketplace/reducer.js
@@ -97,6 +97,153 @@ export default function reducer(state = initialState, action) {
       }
     }
 
+    case AT.CREATED_HOLD_PENDING: {
+      return {
+        ...state,
+        loading: true
+      }
+    }
+
+    case AT.CREATED_HOLD_FULFILLED: {
+      return {
+        ...state,
+        loading: false,
+        holds: payload
+      }
+    }
+
+    case AT.CREATED_HOLD_REJECTED: {
+      return {
+        ...state,
+        loading: false
+      }
+    }
+
+    case AT.GET_COUNT_HOLDS_PENDING: {
+      return {
+        ...state,
+        loading: true
+      }
+    }
+
+    case AT.GET_COUNT_HOLDS_FULFILLED: {
+      return {
+        ...state,
+        loading: false,
+        countHolds: payload.data
+      }
+    }
+
+    case AT.GET_COUNT_HOLDS_REJECTED: {
+      return {
+        ...state,
+        loading: false
+      }
+    }
+
+    case AT.REJECT_HOLD_PENDING: {
+      return {
+        ...state,
+        loading: true
+      }
+    }
+
+    case AT.REJECT_HOLD_FULFILLED: {
+      return {
+        ...state,
+        loading: false,
+        holds: payload.data
+      }
+    }
+
+    case AT.REJECT_HOLD_REJECTED: {
+      return {
+        ...state,
+        loading: false
+      }
+    }
+
+    case AT.CANCEL_HOLD_PENDING: {
+      return {
+        ...state,
+        loading: true
+      }
+    }
+
+    case AT.CANCEL_HOLD_FULFILLED: {
+      return {
+        ...state,
+        loading: false,
+        holds: payload.data
+      }
+    }
+
+    case AT.CANCEL_HOLD_REJECTED: {
+      return {
+        ...state,
+        loading: false
+      }
+    }
+
+    case AT.APPROVE_HOLD_PENDING: {
+      return {
+        ...state,
+        loading: true
+      }
+    }
+
+    case AT.APPROVE_HOLD_FULFILLED: {
+      return {
+        ...state,
+        loading: false,
+        holds: payload.data
+      }
+    }
+
+    case AT.APPROVE_HOLD_REJECTED: {
+      return {
+        ...state,
+        loading: false
+      }
+    }
+
+    case AT.TO_CART_HOLD_PENDING: {
+      return {
+        ...state,
+        loading: true
+      }
+    }
+
+    case AT.TO_CART_HOLD_FULFILLED: {
+      return {
+        ...state,
+        loading: false,
+        holds: payload.data
+      }
+    }
+
+    case AT.TO_CART_HOLD_REJECTED: {
+      return {
+        ...state,
+        loading: false
+      }
+    }
+
+    case AT.HOLD_APPLY_FILTER: {
+      return {
+        ...state,
+        datagridFilter: payload,
+        datagridFilterUpdate: !state.datagridFilterUpdate
+      }
+    }
+
+    case AT.TOGGLE_HOLDS: {
+      return {
+        ...state,
+        typeHolds: payload
+      }
+    }
+
     default: {
       return state
     }
