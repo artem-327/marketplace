d27aac449ec725d0a8d306f3432d0abf0fdbb223
Miroslav Veis <miroslav.veis@artio.cz>
2020-10-30 12:28:59 +0100

    adjusted available date, fixed total number

--- modules/wanted-board/listings/components/SubmitOffer/SubmitOfferPopup.js
+++ modules/wanted-board/listings/components/SubmitOffer/SubmitOfferPopup.js
@@ -726,6 +726,8 @@ class SubmitOfferPopup extends React.Component {
         total = Number(value) * Number(this.values.items[index].pricePerUOM)
       } else if (name.includes('pricePerUOM')) {
         total = Number(value) * Number(this.values.items[index].pkgAmount)
+      } else {
+        total = Number(this.values.items[index].pkgAmount) * Number(this.values.items[index].pricePerUOM)
       }
       this.setFieldValue(`items[${index}].total`, total)
     }
@@ -819,6 +821,7 @@ class SubmitOfferPopup extends React.Component {
         <DateInput
           name={`items[${index}].fulfilledAt`}
           inputProps={{
+            minDate: moment().add(1, 'days'),
             onChange: (e, { name, value }) => this.handleChange(e, { name, value: getStringISODate(value) }),
             clearable: true,
             fluid: this.state.nextSubmit && this.values.fulfillmentType !== 'COMPLETE_SCHEDULE'
