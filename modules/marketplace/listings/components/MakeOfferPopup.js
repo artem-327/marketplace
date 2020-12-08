19647b5773148cc87b62f09a661970e4cc398dfa
Petr Staszko <petr.staszko@artio.cz>
2020-12-09 00:27:29 +0100

    #35511 Added popup contents, styles.

--- modules/marketplace/listings/components/MakeOfferPopup.js
+++ modules/marketplace/listings/components/MakeOfferPopup.js
@@ -1,20 +1,23 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import * as Actions from '../../actions'
-import { Form, Input, Button, Dropdown } from 'formik-semantic-ui-fixed-validation'
+import { Input, Button, TextArea } from 'formik-semantic-ui-fixed-validation'
 import {
+  Form,
   Modal,
   Dimmer,
   Loader,
   Grid,
   GridRow,
   GridColumn,
+  Label,
   Checkbox,
   FormField,
   FormGroup,
   Segment
 } from 'semantic-ui-react'
 
+import { Formik } from 'formik'
 import { Field as FormikField } from 'formik'
 import * as Yup from 'yup'
 import { FormattedMessage, injectIntl } from 'react-intl'
@@ -29,7 +32,7 @@ import { uniqueArrayByKey } from '~/utils/functions'
 import get from 'lodash/get'
 import { getSafe } from '~/utils/functions'
 import ErrorFocus from '~/components/error-focus'
-import { X as XIcon } from 'react-feather'
+import { Schedule } from '@material-ui/icons'
 
 const CustomForm = styled(Form)`
   flex-grow: 0 !important;
@@ -62,6 +65,20 @@ const HighSegment = styled.div`
     width: 18px;
     height: 18px;
     cursor: pointer;
+  }
+`
+
+const StyledModal = styled(Modal)`  
+  > i.close.icon {
+    font-size: 18px;
+  }
+  
+  &.ui.large.modal > .header {
+    font-size: 18px;
+  }
+  
+  &.ui.large.modal > .scrolling.content {
+    padding: 30px;
   }
 `
 
@@ -112,6 +129,67 @@ const BottomButtons = styled.div`
     margin: 30px -1.5rem -1.5rem;
     border-top: 1px solid #dee2e6;
     box-shadow: 0 0 0 0 transparent;
+  }
+`
+
+const StyledRectangle = styled.div`
+  padding: 11px 15px;
+  border-radius: 4px;
+  border: solid 1px #dee2e6;
+  background-color: #f8f9fb;
+  
+  .header {
+    font-size: 12px;
+    color: #848893;
+  }
+  
+  .name {
+    font-size: 14px;
+    color: #20273a;
+    font-weight: bold;
+  }
+`
+
+const FieldRectangle = styled.div`
+  padding: 10px 15px;
+  border-radius: 3px;
+  border: solid 1px #dee2e6;
+  background-color: #f8f9fb;
+  font-size: 14px;
+  font-weight: bold;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.29;
+  letter-spacing: normal;
+  color: #20273a;
+`
+
+const SmallText = styled.div`
+  font-size: 12px;
+  color: #848893;
+  display: flex;
+  
+  > svg.title-icon {
+    font-size: 14px;
+    margin-right: 8px;
+  }
+`
+
+const StyledGrid = styled(Grid)`
+  &.ui.grid {
+    margin: -7.5px -10px;
+  
+    .row {
+      padding: 7.5px 0;
+      
+      .column {
+        padding: 0 10px;  
+      }  
+    }
+    
+    .ui.input {
+      height: 40px;
+    }
   }
 `
 
@@ -169,8 +247,10 @@ class MakeOfferPopup extends React.Component {
       isSending
     } = this.props
 
+    console.log('!!!!!!!!!! render popupValues', popupValues)
+
     return (
-      <CustomForm
+      <Formik
         autoComplete='off'
         enableReinitialize
         initialValues={this.getInitialFormValues()}
@@ -178,51 +258,136 @@ class MakeOfferPopup extends React.Component {
         onSubmit={this.submitOffer}>
         {formikProps => {
           let { values, setFieldValue, setFieldTouched, errors, touched, isSubmitting } = formikProps
-
           return (
-            <>
-              <Modal closeIcon onClose={closePopup} open={true} size='large'>
-                <Dimmer active={isSending} inverted>
-                  <Loader />
-                </Dimmer>
-                <Modal.Header>
-                  <FormattedMessage id='marketplace.makeAnOffer' defaultMessage='Make an Offer' />
-                </Modal.Header>
+            <StyledModal closeIcon onClose={closePopup} open={true} size='large'>
+              <Dimmer active={isSending} inverted>
+                <Loader />
+              </Dimmer>
+              <Modal.Header>
+                <FormattedMessage id='marketplace.makeAnOffer' defaultMessage='Make an Offer' />
+              </Modal.Header>
+              <Modal.Content scrolling>
+                <Form>
+                  <>
+                    <StyledGrid>
+                      <GridRow>
+                        <GridColumn>
+                          <StyledRectangle>
+                            <div className='header'>
+                              <FormattedMessage id='marketplace.productName' defaultMessage='Product Name' />
+                            </div>
+                            <div className='name'>
+                              {this.props.productName}
+                            </div>
+                          </StyledRectangle>
+                        </GridColumn>
+                      </GridRow>
 
-                <Modal.Content scrolling>
+                      <GridRow>
+                        <GridColumn width={5}>
+                          <Input
+                            name='priceOffer'
+                            inputProps={{
+                              placeholder: '0',
+                              min: 0,
+                              type: 'number'
+                            }}
+                            label={
+                              <FormattedMessage
+                                id='marketplace.yourFobPriceOffer'
+                                defaultMessage='Your FOB price offer'>
+                                {text => text}
+                              </FormattedMessage>}
+                          />
+                        </GridColumn>
+                        <GridColumn width={3}>
+                          <Form.Field>
+                            <label>
+                              <FormattedMessage
+                                id='marketplace.YourTotalBid'
+                                defaultMessage='Your Total Bid'>
+                                {text => text}
+                              </FormattedMessage>
+                            </label>
+                            <FieldRectangle>
+                              value
+                            </FieldRectangle>
+                          </Form.Field>
+                        </GridColumn>
+                      </GridRow>
 
-                  bla bla
+                      <GridRow>
+                        <GridColumn>
+                          <TextArea
+                            name='message'
+                            label={
+                              <FormattedMessage id='marketplace.messageToSeller' defaultMessage='Message to Seller' />
+                            }
+                            inputProps={{
+                              'data-test': 'wanted_board_sidebar_specialNotes_inp',
+                              placeholder: formatMessage({
+                                id: 'wantedBoard.writeNotesHere',
+                                defaultMessage: 'Write notes here'
+                              })
+                            }}
+                          />
+                          <SmallText style={{ marginTop: '-14px' }}>
+                            <FormattedMessage id='marketplace.optional' defaultMessage='Optional' />
+                          </SmallText>
+                        </GridColumn>
+                      </GridRow>
 
-                </Modal.Content>
-                <Modal.Actions>
-                  <Button
-                    size='large'
-                    inputProps={{ type: 'button' }}
-                    onClick={closePopup}
-                    data-test='inventory_quick_edit_pricing_popup_cancel_btn'>
-                    {formatMessage({ id: 'global.cancel', defaultMessage: 'Cancel' })}
-                  </Button>
-                  <Button.Submit
-                    primary
-                    size='large'
-                    type='button'
-                    data-test='inventory_quick_edit_pricing_popup_save_btn'>
-                    {formatMessage({ id: 'global.save', defaultMessage: 'Save' })}
-                  </Button.Submit>
-                </Modal.Actions>
-              </Modal>
-              <ErrorFocus />
-            </>
+                      <GridRow>
+                        <GridColumn>
+                          <SmallText>
+                            <Schedule className='title-icon' />
+                            <div>
+                              <FormattedMessage
+                                id='marketplace.sellerHas24HoursToReply.'
+                                defaultMessage='Seller has 24 hours to reply.'>
+                                {text => text}
+                              </FormattedMessage>
+                            </div>
+                          </SmallText>
+                        </GridColumn>
+                      </GridRow>
+                    </StyledGrid>
+                    <ErrorFocus />
+                  </>
+                </Form>
+              </Modal.Content>
+              <Modal.Actions>
+                <Button
+                  size='large'
+                  inputProps={{ type: 'button' }}
+                  onClick={closePopup}
+                  data-test='inventory_quick_edit_pricing_popup_cancel_btn'>
+                  {formatMessage({ id: 'global.cancel', defaultMessage: 'Cancel' })}
+                </Button>
+                <Button.Submit
+                  primary
+                  size='large'
+                  onclick={() => console.log('!!!!!!!!!! onclick')}
+                  type='button'
+                  data-test='inventory_quick_edit_pricing_popup_save_btn'>
+                  {formatMessage({ id: 'global.save', defaultMessage: 'Save' })}
+                </Button.Submit>
+              </Modal.Actions>
+            </StyledModal>
           )
         }}
-      </CustomForm>
+      </Formik>
     )
   }
 }
 
 function mapStateToProps(store) {
+  const { popupValues } = store.marketplace
   return {
-    ...store.marketplace
+    popupValues,
+    isSending: store.marketplace.isSending,
+    productName: getSafe(() => popupValues.companyProduct.intProductName, ''),
+
   }
 }
 
