6be30322129f4a017393f87574733e885d3ec960
Petr Staszko <petr.staszko@artio.cz>
2021-01-20 03:19:37 +0100

    #35735 Fixed merge and libraries dependency problems.

--- components/custom-formik/DateInput.js
+++ components/custom-formik/DateInput.js
@@ -2,22 +2,44 @@ import React, { Component } from 'react'
 import { Form } from 'semantic-ui-react'
 import { FastField, Field, getIn } from 'formik'
 import moment from 'moment'
-
 import { getFieldError, setFieldValue } from './helpers'
 
-import { DateInput } from 'semantic-ui-calendar-react'
+import { DateInput } from 'semantic-ui-calendar-react'    //! ! to be deleted
+
+import DatePicker from 'react-datepicker'
+import './styles/dateInput.scss'
 import { getLocaleDateFormat } from '../date-format'
 import { Input } from 'formik-semantic-ui-fixed-validation'
+import { Input as SemanticInput } from 'semantic-ui-react'
+import { registerLocale } from  "react-datepicker"
+import * as locales from 'date-fns/locale'
+import { Calendar } from 'react-feather'
 
 class FormikInput extends Component {
   constructor(props) {
     super(props)
     const { id, name } = props
     this.id = id || `field_input_${name}`
+
+    this.state = {
+      datepickerMode: 0
+    }
   }
 
   handleRef = r => {
-    r && r.inputNode.setAttribute('autocomplete', 'off')
+    //! !r && r.inputNode.setAttribute('autocomplete', 'off')
+  }
+
+  //https://react-day-picker.js.org/docs/localization/
+  //https://www.positronx.io/react-datepicker-tutorial-with-react-datepicker-examples/
+  //https://app.zeplin.io/project/5dccc59b839aa2bcf6509370/screen/5e006f3e2149329a03d737e3
+
+  componentDidMount() {
+    const localization = typeof navigator !== 'undefined' ? window.navigator.language.slice(0, 2) : 'en'
+    const locale = locales[localization]
+    if (locale) {
+      registerLocale(localization, locale)
+    }
   }
 
   render() {
@@ -32,6 +54,7 @@ class FormikInput extends Component {
       inputOnly,
       addSeparator
     } = this.props
+    const { datepickerMode } = this.state
     const { onChange, placeholder, ...safeInputProps } = inputProps
     const DesiredField = fast === true ? FastField : Field
 
@@ -94,37 +117,97 @@ class FormikInput extends Component {
                   }}
                 />
               ) : (
-                <DateInput
-                  {...safeInputProps}
-                  name={name}
-                  value={field.value}
-                  clearable
-                  onChange={(e, { name, value }) => {
-                    //automatic adjust date in input based on format date
-                    const formatedValue = value.replace(/[/.]/g, '-').replace(/ /g, '').split('-')
+                <>
+                  <DatePicker
+                    {...safeInputProps}
+                    name={name}
+                    value={field.value}
+                    showMonthYearPicker={datepickerMode === 2}
+                    showYearPicker={datepickerMode === 1}
+                    showPopperArrow={false}
+                    placeholderText={placeholder || getLocaleDateFormat()}
+                    isClearable={true}
+                    locale={typeof navigator !== 'undefined' ? window.navigator.language.slice(0, 2) : 'en'}
+                    customInput={
+                      <SemanticInput
+                        icon='calendar'
+                      />
+                    }
 
-                    const canAutomaticallyAdjustDateFormat =
-                      formatedValue.some(d => d.length >= 4) &&
-                      formatedValue.length === 3 &&
-                      moment(value, getLocaleDateFormat()).isValid()
-                    const val = canAutomaticallyAdjustDateFormat
-                      ? moment(value, getLocaleDateFormat()).format(getLocaleDateFormat())
-                      : value
-                    setFieldValue(form, name, val, true)
-                    Promise.resolve().then(() => {
-                      onChange && onChange(e, { name, value: val })
-                    })
-                  }}
-                  placeholder={placeholder || getLocaleDateFormat()}
-                  data-test={`FormikInput_${this.id}_DateInput`}
-                  closable
-                  id={this.id}
-                  onBlur={form.handleBlur}
-                  dateFormat={getLocaleDateFormat()}
-                  animation='none'
-                  ref={this.handleRef}
-                  localization={typeof navigator !== 'undefined' ? window.navigator.language.slice(0, 2) : 'en'}
-                />
+                    onChangeRaw={e => {
+                      const { name, value } = e.target
+                      let val = ''
+                      if (value) {
+                        //console.log('!!!!!!!!!! onChangeRaw value', value)
+                        const formatedValue = value.replace(/[/.]/g, '-').replace(/ /g, '').split('-')
+                        const canAutomaticallyAdjustDateFormat =
+                          formatedValue.some(d => d.length >= 4) &&
+                          formatedValue.length === 3 &&
+                          moment(value, getLocaleDateFormat()).isValid()
+                        val = canAutomaticallyAdjustDateFormat
+                          ? moment(value, getLocaleDateFormat()).format(getLocaleDateFormat())
+                          : value
+                      }
+                      setFieldValue(form, name, val, true)
+                      Promise.resolve().then(() => {
+                        onChange && onChange(e, { name, value: val })
+                      })
+                    }}
+                    onSelect={value => {
+                      //console.log('!!!!!!!!!! onSelect value', value)
+                      const val = moment(value, 'en').format(getLocaleDateFormat())
+                      //const val = moment(value, getLocaleDateFormat()).format(getLocaleDateFormat())
+                      setFieldValue(form, name, val, true)
+                      Promise.resolve().then(() => {
+                        onChange && onChange(e, { name, value: val })
+                      })
+                    }}
+                    dateFormat={
+                      getLocaleDateFormat()
+                        .replace(/D/g, 'd')
+                        .replace(/m/g, 'M')
+                        .replace(/Y/g, 'y')
+                    }
+                  />
+
+
+
+                  <DateInput
+                    {...safeInputProps}
+                    name={name}
+                    value={field.value}
+                    clearable
+                    onChange={(e, { name, value }) => {
+                      //automatic adjust date in input based on format date
+                      const formatedValue = value.replace(/[/.]/g, '-').replace(/ /g, '').split('-')
+
+                      console.log('!!!!!!!!!! onChange value', value)
+
+                      const canAutomaticallyAdjustDateFormat =
+                        formatedValue.some(d => d.length >= 4) &&
+                        formatedValue.length === 3 &&
+                        moment(value, getLocaleDateFormat()).isValid()
+                      const val = canAutomaticallyAdjustDateFormat
+                        ? moment(value, getLocaleDateFormat()).format(getLocaleDateFormat())
+                        : value
+                      setFieldValue(form, name, val, true)
+                      Promise.resolve().then(() => {
+                        onChange && onChange(e, { name, value: val })
+                      })
+
+                      console.log('!!!!!!!!!! onChange old val', val)
+                    }}
+                    placeholder={placeholder || getLocaleDateFormat()}
+                    data-test={`FormikInput_${this.id}_DateInput`}
+                    closable
+                    id={this.id}
+                    onBlur={form.handleBlur}
+                    dateFormat={getLocaleDateFormat()}
+                    animation='none'
+                    ref={this.handleRef}
+                    localization={typeof navigator !== 'undefined' ? window.navigator.language.slice(0, 2) : 'en'}
+                  />
+                </>
               )}
               {!inputOnly && error && <span className='sui-error-message'>{getIn(form.errors, name)}</span>}
               {/* </InputRef> */}
