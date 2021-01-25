34f2535b70788971cd52cad17ab7284ee8097acb
Miroslav Veis <miroslav.veis@artio.cz>
2021-01-25 16:27:20 +0100

    T-35772
    adjusted error message

--- modules/add-bank-accounts/api.js
+++ modules/add-bank-accounts/api.js
@@ -5,17 +5,17 @@ export default {
     api
       .post(`/prodex/api/payments/bank-accounts/velloci/add/log/magic-token?eventName=${eventName}`, metadata)
       .then(response => response.data)
-      .catch(err => console.log(err)),
+      .catch(err => console.error(err.message)),
 
   getVellociToken: magicToken =>
     api
       .get(`/prodex/api/payments/bank-accounts/velloci/add/token/magic-token?token=${magicToken}`)
       .then(response => response.data)
-      .catch(err => console.log(err)),
+      .catch(err => console.error(err.message)),
 
   addVellociAcount: (publicToken, metadata) =>
     api
       .post(`/prodex/api/payments/bank-accounts/velloci/add/magic-token?publicToken=${publicToken}`, metadata)
       .then(response => response.data)
-      .catch(err => console.log(err))
+      .catch(err => console.error(err.message))
 }
