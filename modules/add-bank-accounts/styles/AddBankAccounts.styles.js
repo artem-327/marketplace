2866369da3e916e51c6f9b2439334c8bb2c65409
Miroslav Veis <miroslav.veis@artio.cz>
2021-01-26 16:58:42 +0100

    T-35772
    added comments

--- modules/add-bank-accounts/styles/AddBankAccounts.styles.js
+++ modules/add-bank-accounts/styles/AddBankAccounts.styles.js
@@ -2,6 +2,22 @@ import { PlaidLink } from 'react-plaid-link'
 
 import styled from 'styled-components'
 
+/**
+ * @param {boolean} disabled Props change cursor, pointer and background in button
+ * @css cursor: ${props => (props.disabled ? 'not-allowed !important' : 'pointer !important')};
+ * @css pointer-events: ${props => (props.disabled ? 'none !important' : 'auto !important')};
+ * @css box-shadow: none !important;
+ * @css border: solid 1px #dee2e6 !important;
+ * @css color: #20273a !important;
+ * @css background-color: ${props => (props.disabled ? '#adaeaf !important' : '#ffffff !important')};
+ * @css border-radius: 3px !important;
+ * @css font-weight: 500 !important;
+ * @css align-items: center !important;
+ * @css display: flex !important;
+ * @css justify-content: center !important;
+ * @css font-size: 20px !important;
+ * @css padding: 15px !important;
+ */
 export const PlaidButton = styled(PlaidLink)`
   cursor: ${props => (props.disabled ? 'not-allowed !important' : 'pointer !important')};
   pointer-events: ${props => (props.disabled ? 'none !important' : 'auto !important')};
