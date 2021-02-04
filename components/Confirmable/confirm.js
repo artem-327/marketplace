1a3a4cbfd1bba57665016fd02daa65a3dad3217b
Marek Juroško <marek.jurosko@artio.cz>
2021-02-05 00:18:03 +0100

    Bug #35853 - quick fix for Confirm

--- components/Confirmable/confirm.js
+++ components/Confirmable/confirm.js
@@ -1,7 +1,6 @@
 //import React from 'react'
 import { createConfirmation } from 'react-confirm'
 import ConfirmDialog from './ConfirmDialog'
-import { FormattedMessage } from 'react-intl'
 
 // create confirm function
 const confirm = createConfirmation(ConfirmDialog)
@@ -11,8 +10,8 @@ export default function ConfirmComponent(
   title,
   confirmation,
   options = {
-    cancelText: <FormattedMessage id='global.no' defaultMessage='No' />,
-    proceedText: <FormattedMessage id='global.yes' defaultMessage='Yes' />
+    cancelText: 'No',
+    proceedText: 'Yes'
   }
 ) {
   return confirm({ title, confirmation, options })
