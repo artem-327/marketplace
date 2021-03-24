b7799a1507ab31971aba4432e3b152f235bde8d7
Miroslav Veis <miroslav.veis@artio.cz>
2021-03-24 16:32:12 +0100

    DT-387
    • fixed error in Price controler for broadcast
    • added injectIntl for modalPriceBook (fixed unhandled error)
    • fixed displaying broadcast icon in table
    • removed global broaadcast

--- modules/inventory/shared-listings/components/SharedListings.constants.js
+++ modules/inventory/shared-listings/components/SharedListings.constants.js
@@ -1,5 +1,10 @@
 import { FormattedMessage, injectIntl } from 'react-intl'
-
+//Styles
+import { DivIconOptions } from '../../constants/layout'
+/**
+ * @category Shared Listings
+ * @constant {array}
+ */
 export const COLUMNS = [
   { name: 'groupProductName', disabled: true },
   {
@@ -88,5 +93,51 @@ export const COLUMNS = [
     title: ' ',
     width: 81,
     allowReordering: false
+  }
+]
+
+/**
+ * @category
+ * @type {'NO_BROADCAST'}
+ */
+const NO_BROADCAST = 'NO_BROADCAST'
+
+/**
+ * @category Shared Listings
+ * @type {array}
+ */
+export const BROADCAST_OPTIONS = [
+  {
+    icon: (
+      <DivIconOptions>
+        <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'>
+          <g fill='none' fill-rule='evenodd'>
+            <g>
+              <path
+                d='M0 0L24 0 24 24 0 24z'
+                transform='translate(-1125 -567) translate(1105 295) translate(0 29) translate(20 243)'
+              />
+              <path
+                d='M0 0L24 0 24 24 0 24z'
+                opacity='.87'
+                transform='translate(-1125 -567) translate(1105 295) translate(0 29) translate(20 243)'
+              />
+            </g>
+            <path
+              fill='#848893'
+              fill-rule='nonzero'
+              d='M18 8h-1V6c0-2.76-2.24-5-5-5S7 3.24 7 6v2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V10c0-1.1-.9-2-2-2zm-6 9c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2zM9 8V6c0-1.66 1.34-3 3-3s3 1.34 3 3v2H9z'
+              transform='translate(-1125 -567) translate(1105 295) translate(0 29) translate(20 243)'
+            />
+          </g>
+        </svg>
+      </DivIconOptions>
+    ),
+    title: <FormattedMessage id='myInventory.justMe' defaultMessage='Just Me' />,
+    subtitleId: 'myInventory.justMeSubtitle',
+    subtitleText: 'Only my Company',
+    value: NO_BROADCAST,
+    id: null,
+    tmp: null
   }
 ]
