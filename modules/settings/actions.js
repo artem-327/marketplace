d6e60dd63843301a53613d58a4e6313c2c20b12b
Marek Juroško <marek.jurosko@artio.cz>
2021-03-30 01:19:00 +0200

    DT-151 - Disabled Certifications for Add Warehouse popup

--- modules/settings/actions.js
+++ modules/settings/actions.js
@@ -372,7 +372,7 @@ export function postNewWarehouseRequest(createWarehouse, payload, attachmentFile
       })
     }
     //dispatch(getWarehousesDataRequest())
-    dispatch(closeSidebar())
+    //dispatch(closeSidebar())
     Datagrid.loadData()
   }
 }
