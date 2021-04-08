969fe4b93a1b51f991b907ad540c9ef0a19c766a
Miroslav Veis <miroslav.veis@artio.cz>
2021-04-08 16:06:42 +0200

    DT-502
    • fixed submitHandler
    • added check function to the removeEmpty
    • added catch in api call

--- modules/inventory/shared-listings/components/ListingDetail/Header.services.js
+++ modules/inventory/shared-listings/components/ListingDetail/Header.services.js
@@ -1,16 +1,12 @@
 import { removeEmpty } from '../../../../../utils/functions'
 
 export const submitHandler = async (values, props) => {
-  const body = {
+  let body = {
     priceAddition: values.priceAddition,
     priceMultiplier: parseFloat(values.priceMultiplier),
     priceOverride: values.priceOverride
   }
 
-  removeEmpty(body)
-  try {
-    props.updateMarkUp(values.id, body)
-  } catch (e) {
-    console.error(e)
-  }
+  removeEmpty(body, val => val === 0)
+  await props.updateMarkUp(values.id, body)
 }
