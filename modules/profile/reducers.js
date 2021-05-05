a563ce89d355db66ce6d918a4ac5aca108dc242d
Kiet <72509022+wwwKiet@users.noreply.github.com>
2021-05-05 16:53:13 +0300

    Optimize action and reducer

--- modules/profile/reducers.js
+++ modules/profile/reducers.js
@@ -1,4 +1,19 @@
-import * as AT from './action-types'
+import typeToReducer from 'type-to-reducer'
+
+import {
+  openProfilePopup,
+  closePopup,
+  openChangePasswordPopup,
+  closeChangePasswordPopup,
+  getUserMeData,
+  getCurrencies,
+  updateMyProfile,
+  changePassword,
+  setPreferredLanguage,
+  loadFile,
+  saveAvatarPicture,
+  deleteAvatarPicture
+} from './actions'
 
 export const initialState = {
   usersMe: null,
@@ -9,104 +24,170 @@ export const initialState = {
   savingAvatarPicture: false
 }
 
-export default function reducer(state = initialState, action) {
-  const { payload } = action
-
-  switch (action.type) {
-    case AT.PROFILE_OPEN_POPUP: {
-      return {
-        ...state,
-        profilePopup: true
-      }
-    }
-
-    case AT.PROFILE_CLOSE_POPUP: {
-      return {
-        ...state,
-        profilePopup: false
-      }
-    }
-
-    case AT.PROFILE_PREFERRED_LANGUAGE_PENDING:
-    case AT.PROFILE_CHANGE_PASSWORD_PENDING:
-    case AT.PROFILE_GET_CURRENCIES_PENDING:
-    case AT.PROFILE_GET_USERS_ME_PENDING: {
+export default typeToReducer(
+  {
+    [openProfilePopup]: state => ({
+      ...state,
+      profilePopup: true
+    }),
+    [closePopup]: state => ({
+      ...state,
+      profilePopup: false
+    }),
+    [openChangePasswordPopup]: state => ({
+      ...state,
+      changePasswordPopup: true
+    }),
+    [closeChangePasswordPopup]: state => ({
+      ...state,
+      changePasswordPopup: false,
+      loading: false
+    }),
+    [getUserMeData.pending]: state => {
       return {
         ...state,
         loading: true
       }
-    }
-
-    case AT.PROFILE_PREFERRED_LANGUAGE_FULFILLED:
-    case AT.PROFILE_PREFERRED_LANGUAGE_REJECTED:
-    case AT.PROFILE_GET_CURRENCIES_REJECTED:
-    case AT.PROFILE_GET_USERS_ME_REJECTED: {
-      return {
-        ...state,
-        loading: false
-      }
-    }
-
-    case AT.PROFILE_OPEN_CHANGE_PASSWORD: {
-      return {
-        ...state,
-        changePasswordPopup: true
-      }
-    }
-
-    case AT.PROFILE_CHANGE_PASSWORD: // ! ! smazat
-    case AT.PROFILE_CHANGE_PASSWORD_FULFILLED:
-    case AT.PROFILE_CLOSE_CHANGE_PASSWORD: {
-      return {
-        ...state,
-        changePasswordPopup: false,
-        loading: false
-      }
-    }
-
-    case AT.PROFILE_GET_USERS_ME_FULFILLED: {
+    },
+    [getUserMeData.fulfilled]: (state, { payload }) => {
       return {
         ...state,
         usersMe: payload,
         loading: false
       }
-    }
-
-    case AT.PROFILE_GET_CURRENCIES_FULFILLED: {
+    },
+    [getUserMeData.rejected]: state => {
+      return {
+        ...state,
+        loading: false
+      }
+    },
+    [getCurrencies.pending]: state => {
+      return {
+        ...state,
+        loading: true
+      }
+    },
+    [getCurrencies.fulfilled]: (state, { payload }) => {
       return {
         ...state,
         currency: payload,
         loading: false
       }
-    }
-
-    case AT.PROFILE_UPDATE_MY_PROFILE_FULFILLED: {
+    },
+    [getCurrencies.rejected]: state => {
+      return {
+        ...state,
+        loading: false
+      }
+    },
+    [updateMyProfile.pending]: state => {
+      return {
+        ...state
+      }
+    },
+    [updateMyProfile.fulfilled]: state => {
       return {
         ...state,
         profilePopup: false
       }
-    }
-
-    case AT.PROFILE_DELETE_AVATAR_PICTURE_PENDING:
-    case AT.PROFILE_SAVE_AVATAR_PICTURE_PENDING: {
+    },
+    [updateMyProfile.rejected]: state => {
+      return {
+        ...state
+      }
+    },
+    [changePassword.pending]: state => {
+      return {
+        ...state,
+        loading: true
+      }
+    },
+    [changePassword.fulfilled]: state => {
+      return {
+        ...state,
+        changePasswordPopup: false,
+        loading: false
+      }
+    },
+    [changePassword.rejected]: state => {
+      return {
+        ...state,
+        changePasswordPopup: false,
+        loading: false
+      }
+    },
+    [setPreferredLanguage.pending]: state => {
+      return {
+        ...state,
+        loading: true
+      }
+    },
+    [setPreferredLanguage.fulfilled]: state => {
+      return {
+        ...state,
+        loading: false
+      }
+    },
+    [setPreferredLanguage.rejected]: state => {
+      return {
+        ...state,
+        loading: false
+      }
+    },
+    [loadFile.pending]: state => {
+      return {
+        ...state
+      }
+    },
+    [loadFile.fulfilled]: state => {
+      return {
+        ...state
+      }
+    },
+    [loadFile.rejected]: state => {
+      return {
+        ...state
+      }
+    },
+    [saveAvatarPicture.pending]: state => {
       return {
         ...state,
         savingAvatarPicture: true
       }
-    }
-
-    case AT.PROFILE_DELETE_AVATAR_PICTURE_REJECTED:
-    case AT.PROFILE_DELETE_AVATAR_PICTURE_FULFILLED:
-    case AT.PROFILE_SAVE_AVATAR_PICTURE_REJECTED:
-    case AT.PROFILE_SAVE_AVATAR_PICTURE_FULFILLED: {
+    },
+    [saveAvatarPicture.fulfilled]: state => {
+      return {
+        ...state,
+        savingAvatarPicture: false
+      }
+    },
+    [saveAvatarPicture.rejected]: state => {
+      return {
+        ...state,
+        savingAvatarPicture: false
+      }
+    },
+    [deleteAvatarPicture.pending]: state => {
+      return {
+        ...state,
+        savingAvatarPicture: true
+      }
+    },
+    [deleteAvatarPicture.fulfilled]: state => {
+      return {
+        ...state,
+        savingAvatarPicture: false
+      }
+    },
+    [deleteAvatarPicture.rejected]: state => {
       return {
         ...state,
         savingAvatarPicture: false
       }
     }
+    
+  },
 
-    default: {
-      return state
-    }
-  }
-}
+  initialState
+)
