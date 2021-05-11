5cbc2b1de13b87ce9c0ebf3e36c7f2319325ea61
Petr Staszko <petr.staszko@artio.cz>
2021-05-12 01:05:10 +0200

    DT-571 Velloci registration pages fix.

--- modules/velloci-register/components/VellociRegister.jsx
+++ modules/velloci-register/components/VellociRegister.jsx
@@ -74,6 +74,7 @@ const VellociRegister = props => {
           <SetupIndicator activeStep={props.activeStep} />
           <Formik
             onSubmit={values => handleSubmit(values, props, selfFormikProps)}
+            enableReinitialize
             validateOnChange={true}
             initialValues={props.initialValues}
             validationSchema={getValidationSchema()}
