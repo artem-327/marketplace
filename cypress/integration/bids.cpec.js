6896f7ef532e295a5aae3e95e0028d5ffc846e4a
Tomáš Drlíček <toms.drl@gmail.com>
2021-05-18 13:19:51 +0200

    New counter side user

--- cypress/integration/bids.cpec.js
+++ cypress/integration/bids.cpec.js
@@ -1,6 +1,6 @@
 context("Bids Tests", () => {
     const userJSON = require('../fixtures/user.json')
-    const userJSON2 = require('../fixtures/user3.json')
+    const userJSON2 = require('../fixtures/user2.json')
     let offerId
     let productName
     let bidId
