bb15ffabadefc8593ec5afe1354e05363e7fffe8
Kiet <72509022+wwwKiet@users.noreply.github.com>
2021-05-24 16:01:23 +0300

    DT-753 Error handler on Velloci document upload

--- modules/velloci-register/api.js
+++ modules/velloci-register/api.js
@@ -11,7 +11,7 @@ export default {
       formData.append('files', files[i])
     }
     const id = companyId ? `/${companyId}` : ''
-    api.post(`/prodex/api/payments/velloci/documents/upload${id}`, formData, {
+    return api.post(`/prodex/api/payments/velloci/documents/upload${id}`, formData, {
       headers: {
         accept: 'application/json',
         'Accept-Language': 'en-US,en;q=0.8',
