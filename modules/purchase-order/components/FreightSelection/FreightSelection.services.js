a4118dc9a5cb470158534f10b7ede0489747a80c
Petr Staszko <petr.staszko@artio.cz>
2021-06-23 05:53:12 +0200

    DT-737 Shipping Quote Request Delivery Date added to Checkout.

--- modules/purchase-order/components/FreightSelection/FreightSelection.services.js
+++ modules/purchase-order/components/FreightSelection/FreightSelection.services.js
@@ -1,19 +1,28 @@
-import { generateToastMarkup } from '~/utils/functions'
+import moment from 'moment'
+import { getStringISODate } from '../../../../components/date-format'
 
-export const handleManualShipment = async (setIsOpenConfirmPopup, props) => {
-  let {
-    requestManualShipment,
-    shippingAddress
-  } = props
+export const handleManualShipment = async (onSubmit, values, props) => {
+  const { requestManualShipment, shippingAddress } = props
+  const { date } = values
 
   if (shippingAddress) {
-    const payload = shippingAddress.warehouse
+    let payload = shippingAddress.warehouse
       ? { shippingWarehouseId: shippingAddress.id }
       : { shippingDeliveryAddressId: shippingAddress.id }
 
+    if (date) {
+      let dateTime = moment(getStringISODate(date))
+      const current = moment()
+      dateTime
+        .add(current.hour(), 'hours')
+        .add(current.minute() + 1, 'minutes')
+        .add(current.second(), 'seconds')
+      payload['preferredDeliveryDate'] = encodeURIComponent(dateTime.format())
+    }
+
     try {
       await requestManualShipment(payload)
-      setIsOpenConfirmPopup(true)
+      onSubmit()
     } catch (e) {
       console.error(e)
     }
