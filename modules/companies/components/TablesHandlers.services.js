19fc1b49d8db90836af91f4cc669989a0b6322d6
Petr Staszko <petr.staszko@artio.cz>
2021-07-22 03:47:02 +0200

    DT-849 Fixed Companies / Users - company filter.

--- modules/companies/components/TablesHandlers.services.js
+++ modules/companies/components/TablesHandlers.services.js
@@ -73,17 +73,21 @@ export const handleFilterChangeInputSearch = (e, data, state, setState, props) =
  * @category Companies
  * @method
  * @param {object} data filter name/value
+ * @param {array} allCompanyOptions company options name/value
  * @param {object} state object with state values
  * @param {function} setState object with set state Hook functions
  * @param {object} props object with all props (actions, init data, ...)
  * @return {none}
  */
-export const handleFilterChangeCompany = (e, data, state, setState, props) => {
+export const handleFilterChangeCompany = (e, data, allCompanyOptions, state, setState, props) => {
   const { currentTab } = props
   if (currentTab === '') return
 
+  const selectedCompanyOption = allCompanyOptions.find(el => el.value === data.value)
+
   setState({
     ...state,
+    selectedCompanyOption,
     [currentTab]: {
       ...state[currentTab],
       [data.name]: data.value
@@ -97,6 +101,7 @@ export const handleFilterChangeCompany = (e, data, state, setState, props) => {
 
   props.saveFilters({
     ...state,
+    selectedCompanyOption,
     [currentTab]: {
       ...state[currentTab],
       [data.name]: data.value
