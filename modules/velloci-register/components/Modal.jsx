8fec008895b7970d8c7c4879bc19fc0c50f060cd
Tomáš Drlíček <tomas.drlicek@artio.cz>
2021-09-21 10:00:56 +0200

    Onboarding to 211

---
+++ modules/velloci-register/components/Modal.jsx
@@ -0,0 +1,41 @@
+import { Modal as SemanticModal } from 'semantic-ui-react';
+import { Button } from 'formik-semantic-ui-fixed-validation';
+import { ButtonPrimary, HeadingContainer, HVCenteredContentContainer, StyledModal } from './styles';
+import { FormattedMessage } from 'react-intl';
+import TradePassLogo from '~/assets/images/blue-pallet/trade-pass-logo-only.svg';
+
+export const Modal = props => {
+    const { buttonText, children, handleClick, title } = props
+    const subTitle = props?.subTitle
+
+    return (
+        <StyledModal open centered={true} size='small'>
+            <SemanticModal.Content>
+                <Button style={{ border: 'none', position: 'absolute', top: '0', right: '0' }} onClick={() => handleClick()}>X</Button>
+                <HVCenteredContentContainer style={{ margin: '2rem 0 2rem 0' }}>
+                    <img style={{ width: '4rem', height: '4rem' }} src={TradePassLogo} alt="TradePass Logo" />
+                </HVCenteredContentContainer>
+                {title &&
+                    <HVCenteredContentContainer style={{ marginBlockEnd: '.5rem' }}>
+                        <HeadingContainer style={{ fontSize: '1.5rem' }}>
+                            <FormattedMessage id={title} />
+                        </HeadingContainer>
+                    </HVCenteredContentContainer>
+                }
+                {subTitle &&
+                    <HVCenteredContentContainer style={{ marginBlockEnd: '2rem' }}>
+                        <HeadingContainer style={{ color: '#989898', fontSize: '1.15rem', fontWeight: 'lighter' }}>
+                            <FormattedMessage id={subTitle} />
+                        </HeadingContainer>
+                    </HVCenteredContentContainer>
+                }
+                {children}
+                <HVCenteredContentContainer>
+                    <ButtonPrimary onClick={() => handleClick()}>
+                        <FormattedMessage id={buttonText} />
+                    </ButtonPrimary>
+                </HVCenteredContentContainer>
+            </SemanticModal.Content>
+        </StyledModal>
+    )
+}
